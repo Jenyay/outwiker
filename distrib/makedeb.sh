16cfc1d831668b7e07a3778945e29cc6db394ba3
jenyay <jenyay@jenyay-ubuntu-10>
2010-10-22 09:03:01 +0400

    Добавил скрипт для создания deb-пакета

---
+++ distrib/makedeb.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DIR_NAME="outwiker-0.99.3-1"
+
+outwiker_dir="$DIR_NAME/opt/outwiker/"
+
+if [ ! -d $outwiker_dir ] ; 
+then
+	mkdir $outwiker_dir
+fi
+
+# Скопируем нужные файлы из исходников
+cp -r "../src/core" $outwiker_dir
+cp -r "../src/gui" $outwiker_dir
+cp -r "../src/help" $outwiker_dir
+cp -r "../src/images" $outwiker_dir
+cp -r "../src/libs" $outwiker_dir
+cp -r "../src/pages" $outwiker_dir
+cp "../src/outwiker.py" $outwiker_dir
+cp "../src/copyright" $outwiker_dir
+cp "../src/README" $outwiker_dir
+
+# Удалить файлы *.pyc, *.wxg, *.py~, *.wxg~ 
+find . -name *.pyc -type f -print | xargs rm 
+find . -name *.py~ -type f -print | xargs rm 
+find . -name *.wxg -type f -print | xargs rm 
+find . -name *.wxg~ -type f -print | xargs rm 
+
+# Создание файла с контрольными суммами 
+md5deep -r "$DIR_NAME/opt" > "$DIR_NAME/DEBIAN/md5sums"
+md5deep -r "$DIR_NAME/usr" >> "$DIR_NAME/DEBIAN/md5sums"
+
+# Сборка пакета 
+fakeroot dpkg-deb --build "$DIR_NAME"
