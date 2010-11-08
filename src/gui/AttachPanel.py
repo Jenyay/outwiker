91bdd2a0025781b53d9c2adcd3866a293b85d470
Jenyay <jenyay.ilin@gmail.com>
2010-11-08 11:17:05 +0300

    Создал файлы перевода

--- src/gui/AttachPanel.py
+++ src/gui/AttachPanel.py
@@ -112,7 +112,7 @@ class AttachPanel(wx.Panel):
 	def onRemove(self, event): # wxGlade: AttachPanel.<event_handler>
 		if self.currentPage != None:
 			if wx.MessageBox (_(u"Remove selected files?"), 
-					_(u"Error)",
+					_(u"Error"),
 					wx.YES_NO  | wx.ICON_QUESTION ) == wx.YES:
 				files = self.getSelectedFiles ()
 				try:
