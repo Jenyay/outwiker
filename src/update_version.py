65a3e11d3017d97d75fe7c665750d4af3b2baa8a
Jenyay <jenyay.ilin@gmail.com>
2010-12-28 22:52:59 +0300

    Дополнение в Todo и обновление номера версии

--- src/update_version.py
+++ src/update_version.py
@@ -4,8 +4,8 @@
 import subprocess
 
 fname = "version.txt"
-version = "1.0.0"
-status = "release"
+version = "1.1.0"
+status = "dev"
 
 p = subprocess.Popen(["bzr", "revno"], stdout=subprocess.PIPE)
 stdout, stderr = p.communicate()
