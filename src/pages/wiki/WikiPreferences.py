7661400bbf416fafe870221e4c4d613f113c9bf7
Jenyay <jenyay.ilin@gmail.com>
2011-02-21 16:43:20 +0300

    Исправление в интерфейсе настроек вики-страницы

--- src/pages/wiki/WikiPreferences.py
+++ src/pages/wiki/WikiPreferences.py
@@ -19,9 +19,9 @@ class WikiPrefGeneralPanel(wx.Panel):
 		kwds["style"] = wx.TAB_TRAVERSAL
 		wx.Panel.__init__(self, *args, **kwds)
 		self.htmlCodeCheckbox = wx.CheckBox(self, -1, _("Show HTML Code Tab"))
+		self.showAttachInsteadBlank = wx.CheckBox(self, -1, _("Show Attachments Instead Blank page"))
 		self.thumbSizeLabel = wx.StaticText(self, -1, _("Thumbnail Size"))
 		self.thumbSize = wx.SpinCtrl(self, -1, "250", min=1, max=10000)
-		self.showAttachInsteadBlank = wx.CheckBox(self, -1, _("Show Attachments Instead Blank page"))
 
 		self.__set_properties()
 		self.__do_layout()
@@ -37,9 +37,9 @@ class WikiPrefGeneralPanel(wx.Panel):
 		mainSizer = wx.FlexGridSizer(2, 1, 0, 0)
 		grid_sizer_1 = wx.FlexGridSizer(1, 2, 0, 0)
 		mainSizer.Add(self.htmlCodeCheckbox, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+		mainSizer.Add(self.showAttachInsteadBlank, 0, wx.ALL, 2)
 		grid_sizer_1.Add(self.thumbSizeLabel, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
 		grid_sizer_1.Add(self.thumbSize, 0, wx.ALL|wx.EXPAND, 2)
-		grid_sizer_1.Add(self.showAttachInsteadBlank, 0, 0, 0)
 		grid_sizer_1.AddGrowableRow(0)
 		grid_sizer_1.AddGrowableCol(0)
 		grid_sizer_1.AddGrowableCol(1)
