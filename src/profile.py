a3f6b6312510b36c44d7e85ea79bc8bca37a7abd
Jenyay <jenyay.ilin@gmail.com>
2011-03-04 22:24:08 +0300

    Профилирование

--- src/profile.py
+++ src/profile.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import profiles.pro_parser
 import cProfile
+import pstats
 
-fname = "profiles/test2.wiki"
-pparser = profiles.pro_parser.ParseSample (fname)
-#pparser.run()
+import wx
 
-cProfile.run('pparser.run()')
+from core.application import Application
 
+def wikiparserProfile ():
+	import profiles.pro_parser
+
+	fname = "profiles/test2.wiki"
+	profile_fname = "../test/wikiparser.profile"
+
+	global pparser
+	pparser = profiles.pro_parser.ParseSample (fname)
+
+	#pparser.run()
+	cProfile.run('pparser.run()', profile_fname)
+
+	stats = pstats.Stats(profile_fname)
+	stats.strip_dirs().sort_stats('cumulative').print_stats()
+
+
+if __name__ == "__main__":
+	Application.init ("../test/testconfig.ini")
+
+	class testApp(wx.App):
+		def __init__(self, *args, **kwds):
+			wx.App.__init__ (self, *args, **kwds)
+
+	app = testApp(redirect=False)
+
+	wikiparserProfile()
--- src/profiles/pro_parser.py
+++ src/profiles/pro_parser.py
@@ -12,13 +12,13 @@ from test.utils import removeWiki
 
 class ParseSample (object):
 	def __init__ (self, fname):
-		self.filesPath = u"../samples/samplefiles/"
+		self.filesPath = u"../test/samplefiles/"
 
 		self.pagelinks = [u"Страница 1", u"/Страница 1", u"/Страница 2/Страница 3"]
 		self.pageComments = [u"Страницо 1", u"Страницо 1", u"Страницо 3"]
 
 		with open (fname) as fp:
-			self.content = unicode (fp.read (), "utf-8")
+			self.content = unicode (fp.read (), "utf8")
 
 		self.__createWiki()
 
@@ -27,7 +27,7 @@ class ParseSample (object):
 
 	def __createWiki (self):
 		# Здесь будет создаваться вики
-		self.path = u"../samples/testwiki"
+		self.path = u"../test/testwiki"
 		removeWiki (self.path)
 
 		self.rootwiki = WikiDocument.create (self.path)
@@ -49,3 +49,5 @@ class ParseSample (object):
 	
 	def run (self):
 		result = self.parser.toHtml (self.testPage.content)
+
+		removeWiki (self.path)
