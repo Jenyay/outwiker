004d8942b404b1cbcb57eea9025344dfeff276fe
Jenyay <jenyay.ilin@gmail.com>
2011-07-15 09:32:24 +0400

    Добавил тесты по изменению заголовка окна при открытии вики

--- src/test/guitests/mainwnd.py
+++ src/test/guitests/mainwnd.py
@@ -7,11 +7,36 @@ import wx
 
 from core.application import Application
 from gui.MainWindow import MainWindow
-from gui.guiconfig import GeneralGuiConfig
+from gui.guiconfig import MainWindowConfig
+from core.tree import RootWikiPage, WikiDocument
+from pages.text.textpage import TextPageFactory
 
 from .basemainwnd import BaseMainWndTest
+from test.utils import removeWiki
+
 
 class MainWndTest(BaseMainWndTest):
+	def setUp (self):
+		BaseMainWndTest.setUp (self)
+
+		self.path = u"../test/testwiki"
+		removeWiki (self.path)
+
+		self.rootwiki = WikiDocument.create (self.path)
+
+		TextPageFactory.create (self.rootwiki, u"Страница 1", [])
+		TextPageFactory.create (self.rootwiki, u"Страница 2", [])
+		TextPageFactory.create (self.rootwiki[u"Страница 2"], u"Страница 3", [])
+		TextPageFactory.create (self.rootwiki[u"Страница 2/Страница 3"], u"Страница 4", [])
+		TextPageFactory.create (self.rootwiki[u"Страница 1"], u"Страница 5", [])
+
+
+	def tearDown (self):
+		BaseMainWndTest.tearDown (self)
+		Application.wikiroot = None
+		removeWiki (self.path)
+
+
 	def testProperties (self):
 		self.assertNotEqual (None, self.wnd.tree)
 		self.assertNotEqual (None, self.wnd.pagePanel)
@@ -25,4 +50,18 @@ class MainWndTest(BaseMainWndTest):
 		self.assertNotEqual (None, self.wnd.treeConfig)
 		self.assertNotEqual (None, self.wnd.attachConfig)
 		self.assertNotEqual (None, self.wnd.generalConfig)
+
+
+	def testTitle (self):
+		conf = MainWindowConfig (Application.config)
+		conf.titleFormatOption.value = u"OutWiker - {page} - {file}"
+
+		Application.wikiroot = self.rootwiki
+		self.assertEqual (self.wnd.GetTitle(), u"OutWiker -  - testwiki")
+
+		self.rootwiki.selectedPage = self.rootwiki[u"Страница 1"]
+		self.assertEqual (self.wnd.GetTitle(), u"OutWiker - Страница 1 - testwiki")
+
+		self.rootwiki.selectedPage = self.rootwiki[u"Страница 2/Страница 3"]
+		self.assertEqual (self.wnd.GetTitle(), u"OutWiker - Страница 3 - testwiki")
 
