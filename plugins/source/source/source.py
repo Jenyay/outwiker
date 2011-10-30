8cf06f529bc5ed9cb046528f8e066b2860c9432a
Jenyay <jenyay.ilin@gmail.com>
2011-10-30 21:46:00 +0400

    Добавил локализацию плагина Source

--- plugins/source/source/source.py
+++ plugins/source/source/source.py
@@ -34,7 +34,26 @@ class PluginSourceCommand (Plugin):
 	
 	@property
 	def description (self):
-		return u"Add command (:source:) in wiki parser"
+		return _(u"""Add command (:source:) in wiki parser. This command highlight your source code.
+
+<B>Usage:</B>:
+(:source params... :)
+source code
+(:sourceend:)
+
+<B>Params:</B>
+<I>lang</I> - programming language
+<I>tabwidth</I> - tab size
+
+<B>Example:</B>
+<PRE>(:source lang="python" tabwidth=4:)
+import os
+
+if __name__ == "__main__":
+    print "Hello World!"
+(:sourceend:)
+</PRE>
+""")
 
 
 	@property
@@ -42,7 +61,22 @@ class PluginSourceCommand (Plugin):
 		return u"1.0"
 
 
+	def __initlocale (self):
+		domain = u"source"
+
+		langdir = os.path.join (os.path.dirname (__file__), "locale")
+		global _
+
+		try:
+			_ = self._init_i18n (domain, langdir)
+		except BaseException as e:
+			print e
+
+
+
 	def initialize(self):
+		self.__initlocale()
+
 		cmd_folder = os.path.dirname(os.path.abspath(__file__))
 		if cmd_folder not in sys.path:
 			sys.path.insert(0, cmd_folder)
