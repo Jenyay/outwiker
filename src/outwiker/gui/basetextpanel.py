7c2179e7666d35dfa33e7b0dc2af48e98b7bdc80
Jenyay <jenyay.ilin@gmail.com>
2011-11-22 08:51:32 +0300

    Убрал отладочную информацию

--- src/outwiker/gui/basetextpanel.py
+++ src/outwiker/gui/basetextpanel.py
@@ -107,10 +107,6 @@ class BaseTextPanel (BasePagePanel):
         if self._oldContent != None and self._oldContent != self.page.content:
             # Старое содержимое не совпадает с содержимым страницы.
             # Значит содержимое страницы кто-то изменил
-
-            import traceback
-            traceback.print_stack()
-
             self.__externalEdit()
         else:
             self._savePageContent(self.page)
