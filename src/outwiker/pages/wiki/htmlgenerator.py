75d267310bbc980649f551a71d42fd4d93d97ef1
Jenyay <jenyay.ilin@gmail.com>
2012-05-04 21:54:52 +0400

    Рефакторинг расчета контрольной суммы викистраниц

--- src/outwiker/pages/wiki/htmlgenerator.py
+++ src/outwiker/pages/wiki/htmlgenerator.py
@@ -8,14 +8,13 @@ from outwiker.core.config import Config, StringOption
 from outwiker.core.htmlimprover import HtmlImprover
 from outwiker.core.htmltemplate import HtmlTemplate
 from outwiker.core.application import Application
-from outwiker.core.attachment import Attachment
 from outwiker.core.tree import RootWikiPage
-from outwiker.core.style import Style
-from parserfactory import ParserFactory
-from wikiconfig import WikiConfig
-from emptycontent import EmptyContent
 from outwiker.core.system import getTemplatesDir
-from outwiker.gui.guiconfig import HtmlRenderConfig
+
+from .parserfactory import ParserFactory
+from .wikiconfig import WikiConfig
+from .emptycontent import EmptyContent
+from .wikihashcalculator import WikiHashCalculator
 
 
 class HtmlGenerator (object):
@@ -25,12 +24,10 @@ class HtmlGenerator (object):
     def __init__ (self, page):
         self.page = page
         self.config = WikiConfig (Application.config)
-        self.htmlrenderconfig = HtmlRenderConfig (Application.config)
 
         self.resultName = u"__content.html"
         self._configSection = u"wiki"
         self._hashKey = u"md5_hash"
-        self._unicodeEncoding = "unicode_escape"
 
 
     def makeHtml (self, stylepath):
@@ -74,7 +71,8 @@ class HtmlGenerator (object):
 
 
     def getHash (self):
-        return hashlib.md5(self.__getFullContent () ).hexdigest()
+        hashcalculator = WikiHashCalculator (Application)
+        return hashcalculator.getHash (self.page)
 
 
     def getResultPath (self):
@@ -95,102 +93,3 @@ class HtmlGenerator (object):
             return True
 
         return False
-
-
-
-    def __getFullContent (self):
-        """
-        Получить контент для расчета контрольной суммы, по которой определяется, нужно ли обновлять страницу
-        """
-        # Здесь накапливаем список интересующих строк (по которым определяем изменилась страница или нет)
-        content = []
-
-        # Заголовок страницы
-        content.append (self.page.title.encode (self._unicodeEncoding))
-
-        # Содержимое
-        pagecontent = self.page.content.encode (self._unicodeEncoding)
-        content.append (pagecontent)
-
-        self.__getDirContent (self.page, content)
-        content.append (self.__getPluginsList())
-        content.append (self.__getStyleContent (self.page))
-
-        # Настройки, касающиеся вида вики-страницы
-        content.append (str (self.config.showAttachInsteadBlankOptions.value))
-        content.append (str (self.config.thumbSizeOptions.value))
-
-        # Настройки отображения HTML-страницы
-        content.append (str (self.htmlrenderconfig.fontSize.value) )
-        content.append (self.htmlrenderconfig.fontName.value.encode(self._unicodeEncoding) )
-        content.append (str (self.htmlrenderconfig.userStyle.value) )
-
-        # Список подстраниц
-        for child in self.page.children:
-            content.append (child.title.encode (self._unicodeEncoding) + "\n")
-
-        if len (self.page.content) == 0:
-            # Если страница пустая, то проверим настройку, отвечающую за шаблон пустой страницы
-            emptycontent = EmptyContent (Application.config)
-            content.append (emptycontent.content.encode (self._unicodeEncoding))
-
-        return u"".join (content)
-
-
-    def __getStyleContent (self, page):
-        """
-        Возвращает содержимое шаблона
-        """
-        style = Style ()
-
-        try:
-            with open (style.getPageStyle (page)) as fp:
-                stylecontent = unicode (fp.read(), "utf8")
-        except IOError:
-            stylecontent = u""
-        except UnicodeDecodeError:
-            stylecontent = u""
-
-        return stylecontent.encode (self._unicodeEncoding)
-
-
-    def __getPluginsList (self):
-        """
-        Создать список плагинов с учетом номеров версий
-        Возвращает строку
-        """
-        if len (Application.plugins) == 0:
-            return u""
-
-        plugins = [plugin.name + plugin.version for plugin in Application.plugins]
-        plugins.sort()
-        result = reduce (lambda x, y: x + y, plugins)
-        return result
-
-
-    def __getDirContent (self, page, filescontent, dirname="."):
-        """
-        Сформировать строку для расчета хеша по данным вложенной поддиректории dirname (путь относительно __attach)
-        page - страница, для которой собираем список вложений
-        filescontent - список, содержащий строки, описывающие вложенные файлы
-        """
-        attach = Attachment (page)
-        attachroot = attach.getAttachPath()
-
-        attachlist = attach.getAttachRelative (dirname)
-        attachlist.sort (Attachment.sortByName)
-
-        for fname in attachlist:
-            fullpath = os.path.join (attachroot, dirname, fname)
-
-            # Пропустим директории, которые начинаются с __
-            if not os.path.isdir (fname) or not fname.startswith ("__"):
-                try:
-                    filescontent.append (fname.encode (self._unicodeEncoding))
-                    filescontent.append (unicode (os.stat (fullpath).st_mtime))
-
-                    if os.path.isdir (fullpath):
-                        self.__getDirContent (page, filescontent, os.path.join (dirname, fname))
-                except OSError:
-                    # Если есть проблемы с доступом к файлу, то здесь на это не будем обращать внимания
-                    pass
---
+++ src/outwiker/pages/wiki/wikihashcalculator.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os.path
+import hashlib
+
+from outwiker.core.attachment import Attachment
+from outwiker.core.style import Style
+from outwiker.gui.guiconfig import HtmlRenderConfig
+
+from .wikiconfig import WikiConfig
+from .emptycontent import EmptyContent
+
+
+class WikiHashCalculator (object):
+    """
+    Класс для расчета контрольной суммы викистраницы
+    """
+    def __init__ (self, application):
+        self._unicodeEncoding = "unicode_escape"
+
+        self._application = application
+        self._mainConfig = self._application.config
+        self._wikiConfig = WikiConfig (self._mainConfig)
+        self.__htmlConfig = HtmlRenderConfig (self._mainConfig)
+
+
+    def getHash (self, page):
+        return hashlib.md5(self.__getFullContent (page) ).hexdigest()
+
+
+    def __getFullContent (self, page):
+        """
+        Получить контент для расчета контрольной суммы, по которой определяется, нужно ли обновлять страницу
+        """
+        # Здесь накапливаем список интересующих строк (по которым определяем изменилась страница или нет)
+        content = []
+
+        # Заголовок страницы
+        content.append (page.title.encode (self._unicodeEncoding))
+
+        # Содержимое
+        pagecontent = page.content.encode (self._unicodeEncoding)
+        content.append (pagecontent)
+
+        self.__getDirContent (page, content)
+        content.append (self.__getPluginsList())
+        content.append (self.__getStyleContent (page))
+
+        # Настройки, касающиеся вида вики-страницы
+        content.append (str (self._wikiConfig.showAttachInsteadBlankOptions.value))
+        content.append (str (self._wikiConfig.thumbSizeOptions.value))
+
+        # Настройки отображения HTML-страницы
+        content.append (str (self.__htmlConfig.fontSize.value) )
+        content.append (self.__htmlConfig.fontName.value.encode(self._unicodeEncoding) )
+        content.append (str (self.__htmlConfig.userStyle.value) )
+
+        # Список подстраниц
+        for child in page.children:
+            content.append (child.title.encode (self._unicodeEncoding) + "\n")
+
+        if len (page.content) == 0:
+            # Если страница пустая, то проверим настройку, отвечающую за шаблон пустой страницы
+            emptycontent = EmptyContent (self._mainConfig)
+            content.append (emptycontent.content.encode (self._unicodeEncoding))
+
+        return u"".join (content)
+
+
+    def __getStyleContent (self, page):
+        """
+        Возвращает содержимое шаблона
+        """
+        style = Style ()
+
+        try:
+            with open (style.getPageStyle (page)) as fp:
+                stylecontent = unicode (fp.read(), "utf8")
+        except IOError:
+            stylecontent = u""
+        except UnicodeDecodeError:
+            stylecontent = u""
+
+        return stylecontent.encode (self._unicodeEncoding)
+
+
+    def __getPluginsList (self):
+        """
+        Создать список плагинов с учетом номеров версий
+        Возвращает строку
+        """
+        if len (self._application.plugins) == 0:
+            return u""
+
+        plugins = [plugin.name + plugin.version for plugin in self._application.plugins]
+        plugins.sort()
+        result = reduce (lambda x, y: x + y, plugins)
+        return result
+
+
+    def __getDirContent (self, page, filescontent, dirname="."):
+        """
+        Сформировать строку для расчета хеша по данным вложенной поддиректории dirname (путь относительно __attach)
+        page - страница, для которой собираем список вложений
+        filescontent - список, содержащий строки, описывающие вложенные файлы
+        """
+        attach = Attachment (page)
+        attachroot = attach.getAttachPath()
+
+        attachlist = attach.getAttachRelative (dirname)
+        attachlist.sort (Attachment.sortByName)
+
+        for fname in attachlist:
+            fullpath = os.path.join (attachroot, dirname, fname)
+
+            # Пропустим директории, которые начинаются с __
+            if not os.path.isdir (fname) or not fname.startswith ("__"):
+                try:
+                    filescontent.append (fname.encode (self._unicodeEncoding))
+                    filescontent.append (unicode (os.stat (fullpath).st_mtime))
+
+                    if os.path.isdir (fullpath):
+                        self.__getDirContent (page, filescontent, os.path.join (dirname, fname))
+                except OSError:
+                    # Если есть проблемы с доступом к файлу, то здесь на это не будем обращать внимания
+                    pass
