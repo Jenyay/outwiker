7b2a6c6337aab9b8ad29c311ce224b59edab2d1c
Jenyay <jenyay.ilin@gmail.com>
2013-01-02 15:46:38 +0400

    Рефакторинг плагина Source. Временно сломал локализацию

---
+++ plugins/source/source/controller.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from outwiker.gui.preferences.preferencepanelinfo import PreferencePanelInfo
+
+
+class Controller (object):
+    """
+    Класс отвечает за основную работу интерфейса плагина
+    """
+    def __init__ (self, plugin, application):
+        """
+        plugin - Владелец контроллера (экземпляр класса PluginSource)
+        application - экземпляр класса ApplicationParams
+        """
+        self.SOURCE_TOOL_ID = u"PLUGIN_SOURCE_TOOL_ID"
+        self._plugin = plugin
+        self._application = application
+
+
+    def initialize (self):
+        """
+        Инициализация контроллера при активации плагина. Подпись на нужные события
+        """
+        self._application.onWikiParserPrepare += self.__onWikiParserPrepare
+        self._application.onPageViewCreate += self.__onPageViewCreate
+        self._application.onPreferencesDialogCreate += self.__onPreferencesDialogCreate
+
+        if self._isCurrentWikiPage:
+            self.__onPageViewCreate (self._application.selectedPage)
+
+
+    def destroy (self):
+        """
+        Вызывается при отключении плагина
+        """
+        self._application.onWikiParserPrepare -= self.__onWikiParserPrepare
+        self._application.onPageViewCreate -= self.__onPageViewCreate
+        self._application.onPreferencesDialogCreate -= self.__onPreferencesDialogCreate
+
+        if self._isCurrentWikiPage:
+            self._getPageView().removeTool (self.SOURCE_TOOL_ID)
+
+
+    def __onWikiParserPrepare (self, parser):
+        from .commandsource import CommandSource
+        parser.addCommand (CommandSource (parser, self._application.config))
+
+
+    def __onPreferencesDialogCreate (self, dialog):
+        from .preferencepanel import PreferencePanel
+        prefPanel = PreferencePanel (dialog.treeBook, self._application.config, _)
+
+        panelName = _(u"Source [Plugin]")
+        panelsList = [PreferencePanelInfo (prefPanel, panelName)]
+        dialog.appendPreferenceGroup (panelName, panelsList)
+
+
+    @property
+    def _isCurrentWikiPage (self):
+        return (self._application.selectedPage != None and
+                self._application.selectedPage.getTypeString() == u"wiki")
+
+
+    def __onPageViewCreate(self, page):
+        """Обработка события после создания представления страницы"""
+        assert self._application.mainWindow != None
+
+        if page.getTypeString() != u"wiki":
+            return
+
+        pageView = self._getPageView()
+
+        helpString = _(u"Source Code (:source ...:)")
+        pageView.addTool (pageView.commandsMenu, 
+                self.SOURCE_TOOL_ID, 
+                self.__onInsertCommand, 
+                helpString, 
+                helpString, 
+                None)
+
+
+    def __onInsertCommand (self, event):
+        config = self._plugin.config
+
+        startCommand = u'(:source lang="{language}" tabwidth={tabwidth}:)\n'.format (
+                language=config.defaultLanguage.value,
+                tabwidth=config.tabWidth.value
+                )
+
+        endCommand = u'\n(:sourceend:)'
+
+        pageView = self._getPageView()
+        pageView.codeEditor.turnText (startCommand, endCommand)
+
+
+    def _getPageView (self):
+        """
+        Получить указатель на панель представления страницы
+        """
+        return self._application.mainWindow.pagePanel.pageView
--- plugins/source/source/source.py
+++ plugins/source/source/source.py
@@ -8,10 +8,9 @@ from outwiker.core.pluginbase import Plugin
 from outwiker.core.system import getOS
 from outwiker.core.commands import getCurrentVersion
 from outwiker.core.version import Version, StatusSet
-from outwiker.pages.wiki.wikipanel import WikiPagePanel
-from outwiker.gui.preferences.preferencepanelinfo import PreferencePanelInfo
 
 from .sourceconfig import SourceConfig
+from .controller import Controller
 
 
 # Для работы этого плагина требуется OutWiker 1.6.0.632
@@ -27,14 +26,8 @@ else:
             application - экземпляр класса core.application.ApplicationParams
             """
             Plugin.__init__ (self, application)
-            self.__version = u"1.6"
-            self.SOURCE_TOOL_ID = u"PLUGIN_SOURCE_TOOL_ID"
-
-
-        @property
-        def _isCurrentWikiPage (self):
-            return (self._application.selectedPage != None and
-                    self._application.selectedPage.getTypeString() == u"wiki")
+            self.__version = u"1.7"
+            self.__controler = Controller(self, application)
 
 
         def initialize(self):
@@ -42,12 +35,7 @@ else:
 
             self.__correctSysPath()
 
-            self._application.onWikiParserPrepare += self.__onWikiParserPrepare
-            self._application.onPageViewCreate += self.__onPageViewCreate
-            self._application.onPreferencesDialogCreate += self.__onPreferencesDialogCreate
-
-            if self._isCurrentWikiPage:
-                self.__onPageViewCreate (self._application.selectedPage)
+            self.__controler.initialize()
 
 
         def __correctSysPath (self):
@@ -57,63 +45,10 @@ else:
             if cmd_folder not in syspath:
                 sys.path.insert(0, cmd_folder)
 
-        
-        def __onWikiParserPrepare (self, parser):
-            from .commandsource import CommandSource
-            parser.addCommand (CommandSource (parser, self._application.config))
-
-
-        def __onPreferencesDialogCreate (self, dialog):
-            from .preferencepanel import PreferencePanel
-            prefPanel = PreferencePanel (dialog.treeBook, self._application.config, _)
-
-            panelName = _(u"Source [Plugin]")
-            panelsList = [PreferencePanelInfo (prefPanel, panelName)]
-            dialog.appendPreferenceGroup (panelName, panelsList)
-
-
-        def __onPageViewCreate(self, page):
-            """Обработка события после создания представления страницы"""
-            assert self._application.mainWindow != None
-
-            if page.getTypeString() != u"wiki":
-                return
-
-            pageView = self._getPageView()
-
-            helpString = _(u"Source Code (:source ...:)")
-            pageView.addTool (pageView.commandsMenu, 
-                    self.SOURCE_TOOL_ID, 
-                    self.__onInsertCommand, 
-                    helpString, 
-                    helpString, 
-                    None)
-
-
-        def _getPageView (self):
-            """
-            Получить указатель на панель представления страницы
-            """
-            return self._application.mainWindow.pagePanel.pageView
-
 
         @property
         def config (self):
             return SourceConfig (self._application.config)
-
-
-        def __onInsertCommand (self, event):
-            config = self.config
-
-            startCommand = u'(:source lang="{language}" tabwidth={tabwidth}:)\n'.format (
-                    language=config.defaultLanguage.value,
-                    tabwidth=config.tabWidth.value
-                    )
-
-            endCommand = u'\n(:sourceend:)'
-
-            pageView = self._getPageView()
-            pageView.codeEditor.turnText (startCommand, endCommand)
 
 
         @property
@@ -164,9 +99,4 @@ if __name__ == "__main__":
             """
             Уничтожение (выгрузка) плагина. Здесь плагин должен отписаться от всех событий
             """
-            self._application.onWikiParserPrepare -= self.__onWikiParserPrepare
-            self._application.onPageViewCreate -= self.__onPageViewCreate
-            self._application.onPreferencesDialogCreate -= self.__onPreferencesDialogCreate
-
-            if self._isCurrentWikiPage:
-                self._getPageView().removeTool (self.SOURCE_TOOL_ID)
+            self.__controler.destroy()
