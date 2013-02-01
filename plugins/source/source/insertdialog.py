94aae66d4e752cb4143ac4fab53c7166cf6c57d4
Jenyay <jenyay.ilin@gmail.com>
2013-02-01 22:07:25 +0400

    Изменение оформления диалога плагина Source

--- plugins/source/source/insertdialog.py
+++ plugins/source/source/insertdialog.py
@@ -66,19 +66,55 @@ class InsertDialog (wx.Dialog):
         """
         Создать элементы управления
         """
+        self.notebook = wx.Notebook(self, -1)
+
         mainSizer = wx.FlexGridSizer (0, 1)
         mainSizer.AddGrowableCol(0)
-        mainSizer.AddGrowableRow(3)
+        mainSizer.AddGrowableRow(0)
 
-        self.__createFileGui (mainSizer)
-        self.__createLanguageGui (mainSizer)
-        self.__createStyleGui (mainSizer)
-        self.__createTabWidthGui (mainSizer)
+        mainSizer.Add (
+                self.notebook,
+                proportion=1,
+                flag=wx.ALL | wx.EXPAND,
+                border=2
+                )
+
+        self.generalPanel = self.__createGeneralPanel()
+        self.appearancePanel = self.__createAppearancePanel()
         self.__createOkCancelButtons (mainSizer)
+
+        self.notebook.AddPage (self.generalPanel, _(u"General"))
+        self.notebook.AddPage (self.appearancePanel, _(u"Appearance"))
 
         self.SetSizer(mainSizer)
         self.Layout()
         self.Fit()
+
+
+    def __createGeneralPanel (self):
+        generalPanel = wx.Panel (self.notebook)
+
+        generalSizer = wx.FlexGridSizer (0, 1)
+        generalSizer.AddGrowableCol(0)
+        generalPanel.SetSizer (generalSizer)
+
+        self.__createFileGui (generalSizer, generalPanel)
+        self.__createLanguageGui (generalSizer, generalPanel)
+
+        return generalPanel
+
+
+    def __createAppearancePanel (self):
+        appearancePanel = wx.Panel (self.notebook)
+
+        appearanceSizer = wx.FlexGridSizer (0, 1)
+        appearanceSizer.AddGrowableCol(0)
+        appearancePanel.SetSizer (appearanceSizer)
+
+        self.__createStyleGui (appearanceSizer, appearancePanel)
+        self.__createTabWidthGui (appearanceSizer, appearancePanel)
+
+        return appearancePanel
 
 
     def __createOkCancelButtons (self, mainSizer):
@@ -92,15 +128,15 @@ class InsertDialog (wx.Dialog):
                 )
 
 
-    def __createLanguageGui (self, mainSizer):
+    def __createLanguageGui (self, mainSizer, parent):
         """
         Создать интерфейс, связанный с языком программирования
         """
         langSizer = wx.FlexGridSizer (0, 2)
         langSizer.AddGrowableCol(1)
 
-        languageLabel = wx.StaticText(self, -1, _(u"Language"))
-        self.languageComboBox = wx.ComboBox (self, 
+        languageLabel = wx.StaticText(parent, -1, _(u"Language"))
+        self.languageComboBox = wx.ComboBox (parent, 
                 style=wx.CB_DROPDOWN | wx.CB_READONLY)
 
         self.languageComboBox.SetMinSize (wx.Size (self._fieldsWidth, -1))
@@ -128,16 +164,16 @@ class InsertDialog (wx.Dialog):
                 )
 
 
-    def __createTabWidthGui (self, mainSizer):
+    def __createTabWidthGui (self, mainSizer, parent):
         """
         Создать интерфейс, связанный с размером табуляции
         """
         tabSizer = wx.FlexGridSizer (0, 2)
         tabSizer.AddGrowableCol(1)
 
-        tabWidthLabel = wx.StaticText(self, -1, _(u"Tab Width (0 - Default Value)"))
+        tabWidthLabel = wx.StaticText(parent, -1, _(u"Tab Width (0 - Default Value)"))
         self.tabWidthSpin = wx.SpinCtrl (
-                self, 
+                parent, 
                 style=wx.SP_ARROW_KEYS|wx.TE_AUTO_URL
                 )
         self.tabWidthSpin.SetMinSize (wx.Size (self._fieldsWidth, -1))
@@ -165,15 +201,15 @@ class InsertDialog (wx.Dialog):
                 )
 
 
-    def __createStyleGui (self, mainSizer):
+    def __createStyleGui (self, mainSizer, parent):
         """
         Создать интерфейс, связанный с выбором стиля оформления
         """
         styleSizer = wx.FlexGridSizer (0, 2)
         styleSizer.AddGrowableCol(1)
 
-        styleLabel = wx.StaticText(self, -1, _(u"Style"))
-        self.styleComboBox = wx.ComboBox (self, 
+        styleLabel = wx.StaticText(parent, -1, _(u"Style"))
+        self.styleComboBox = wx.ComboBox (parent, 
                 style=wx.CB_DROPDOWN | wx.CB_READONLY)
 
         self.styleComboBox.SetMinSize (wx.Size (self._fieldsWidth, -1))
@@ -201,12 +237,12 @@ class InsertDialog (wx.Dialog):
                 )
 
 
-    def __createFileGui (self, mainSizer):
+    def __createFileGui (self, mainSizer, parent):
         """
         Создать интерфейс, связанный со вставкой исходников из вложенных файлов
         """
         self.fileCheckBox = wx.CheckBox (
-                self,
+                parent,
                 label = _(u"Insert source from file"))
 
         mainSizer.Add (
@@ -220,8 +256,8 @@ class InsertDialog (wx.Dialog):
         fileSizer.AddGrowableCol (2)
 
         # Список для выбора прикрепленных файлов
-        self.attachmentLabel = wx.StaticText(self, -1, _(u"Attached file"))
-        self.attachmentComboBox = wx.ComboBox (self,
+        self.attachmentLabel = wx.StaticText(parent, -1, _(u"Attached file"))
+        self.attachmentComboBox = wx.ComboBox (parent,
                 style=wx.CB_DROPDOWN | wx.CB_READONLY)
 
         self.attachmentComboBox.SetMinSize ((self._fieldsWidth, -1))
@@ -243,8 +279,8 @@ class InsertDialog (wx.Dialog):
                 )
 
         # Выбор кодировки файла
-        self.encodingLabel = wx.StaticText(self, -1, _(u"File encoding"))
-        self.encodingComboBox = wx.ComboBox (self,
+        self.encodingLabel = wx.StaticText(parent, -1, _(u"File encoding"))
+        self.encodingComboBox = wx.ComboBox (parent,
                 style=wx.CB_DROPDOWN)
 
         self.encodingComboBox.SetMinSize ((self._fieldsWidth, -1))
