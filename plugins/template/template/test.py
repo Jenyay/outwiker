689c7520c482083a10ed4d7b5f3fb0ea9c329f3b
jsoft <jsoft@front.ru>
2013-04-11 12:04:00 +0400

    Добавил шаблон пустого плагина

---
+++ plugins/template/template/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+import os.path
+
+from outwiker.core.pluginsloader import PluginsLoader
+from outwiker.core.tree import WikiDocument
+from outwiker.core.application import Application
+from outwiker.pages.wiki.wikipage import WikiPageFactory
+from test.utils import removeWiki
+
+
+class PluginNameTest (unittest.TestCase):
+    """Тесты плагина PluginName"""
+    def setUp (self):
+        self.__pluginname = u"PluginName"
+
+        self.__createWiki()
+
+        dirlist = [u"../plugins/pluginname"]
+
+        self.loader = PluginsLoader(Application)
+        self.loader.load (dirlist)
+
+
+    def tearDown (self):
+        removeWiki (self.path)
+        self.loader.clear()
+
+
+    def __createWiki (self):
+        # Здесь будет создаваться вики
+        self.path = u"../test/testwiki"
+        removeWiki (self.path)
+
+        self.rootwiki = WikiDocument.create (self.path)
+
+        WikiPageFactory.create (self.rootwiki, u"Страница 1", [])
+        self.testPage = self.rootwiki[u"Страница 1"]
+
+    def testPluginLoad (self):
+        self.assertEqual ( len (self.loader), 1)
