9a04729489356ff0b4cf961cb8c2033f6023c18a
Jenyay <jenyay.ilin@gmail.com>
2013-09-30 21:21:56 +0400

    Сделал парсер горячих клавиш из строки

--- src/outwiker/gui/hotkeyparser.py
+++ src/outwiker/gui/hotkeyparser.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import re
+
 from outwiker.gui.hotkey import HotKey
+
 
 class HotKeyParser (object):
     """Класс для создания экземпляра класса HotKey из строки и создания строки по HotKey"""
     def __init__(self):
-        pass        
+        self._regex = re.compile ("((?P<ctrl>\s*ctrl\s*\+)|(?P<shift>\s*shift\s*\+)|(?P<alt>\s*alt\s*\+))*(?P<key>.*)",
+                re.I | re.U)
 
 
     def toString (self, hotkey):
@@ -22,3 +26,25 @@ class HotKeyParser (object):
                 shift=shiftStr,
                 alt=altStr,
                 key=hotkey.key)
+
+
+    def fromString (self, hotkeyStr):
+        """
+        Создание экземпляра класса HotKey по строке
+        """
+        match = self._regex.match (hotkeyStr)
+        if match == None:
+            raise ValueError ("Invalid hot key string")
+
+        elements = match.groupdict()
+        key = elements["key"].strip()
+
+        ctrl = elements["ctrl"] != None
+        shift = elements["shift"] != None
+        alt = elements["alt"] != None
+
+        if (len (key) == 0 and
+                (ctrl or shift or alt) ):
+            raise ValueError ("Invalid hot key string")
+
+        return HotKey (key, ctrl=ctrl, shift=shift, alt=alt)
--- src/test/hotkeyparser.py
+++ src/test/hotkeyparser.py
@@ -40,3 +40,141 @@ class HotKeyParserTest (unittest.TestCase):
 
         self.assertEqual (self.parser.toString (HotKey (u"A", ctrl=True, alt=True, shift=True)), 
                 u"Ctrl+Shift+Alt+A")
+
+
+    def testParse1 (self):
+        hotkey = self.parser.fromString (u"A")
+
+        self.assertEqual (hotkey.key, "A")
+        self.assertFalse (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse2 (self):
+        hotkey = self.parser.fromString (u"F1")
+
+        self.assertEqual (hotkey.key, "F1")
+        self.assertFalse (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse2 (self):
+        hotkey = self.parser.fromString (u"Ctrl+F1")
+
+        self.assertEqual (hotkey.key, "F1")
+        self.assertTrue (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse3 (self):
+        hotkey = self.parser.fromString (u"Shift+A")
+
+        self.assertEqual (hotkey.key, "A")
+        self.assertFalse (hotkey.ctrl)
+        self.assertTrue (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse4 (self):
+        hotkey = self.parser.fromString (u"Alt+A")
+
+        self.assertEqual (hotkey.key, "A")
+        self.assertFalse (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertTrue (hotkey.alt)
+
+
+    def testParse5 (self):
+        hotkey = self.parser.fromString (u"Shift+Alt+Ctrl+F1")
+
+        self.assertEqual (hotkey.key, "F1")
+        self.assertTrue (hotkey.ctrl)
+        self.assertTrue (hotkey.shift)
+        self.assertTrue (hotkey.alt)
+
+
+    def testParse6 (self):
+        hotkey = self.parser.fromString (u"")
+
+        self.assertEqual (hotkey.key, u"")
+        self.assertFalse (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse7 (self):
+        hotkey = self.parser.fromString (u"+")
+
+        self.assertEqual (hotkey.key, u"+")
+        self.assertFalse (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse8 (self):
+        hotkey = self.parser.fromString (u"Ctrl++")
+
+        self.assertEqual (hotkey.key, u"+")
+        self.assertTrue (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse9 (self):
+        hotkey = self.parser.fromString (u"ShIfT+ALT+ctrl+F1")
+
+        self.assertEqual (hotkey.key, "F1")
+        self.assertTrue (hotkey.ctrl)
+        self.assertTrue (hotkey.shift)
+        self.assertTrue (hotkey.alt)
+
+
+    def testParse10 (self):
+        hotkey = self.parser.fromString (u"Ctrl + F1")
+
+        self.assertEqual (hotkey.key, u"F1")
+        self.assertTrue (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse11 (self):
+        hotkey = self.parser.fromString (u"Ctrl + Shift + F1")
+
+        self.assertEqual (hotkey.key, u"F1")
+        self.assertTrue (hotkey.ctrl)
+        self.assertTrue (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParse12 (self):
+        hotkey = self.parser.fromString (u" Alt + F1")
+
+        self.assertEqual (hotkey.key, u"F1")
+        self.assertFalse (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertTrue (hotkey.alt)
+
+
+    def testParse13 (self):
+        hotkey = self.parser.fromString (u" Ctrl + F1 ")
+
+        self.assertEqual (hotkey.key, u"F1")
+        self.assertTrue (hotkey.ctrl)
+        self.assertFalse (hotkey.shift)
+        self.assertFalse (hotkey.alt)
+
+
+    def testParseInvalid1 (self):
+        self.assertRaises (ValueError, self.parser.fromString, u"Ctrl+")
+
+
+    def testParseInvalid2 (self):
+        self.assertRaises (ValueError, self.parser.fromString, u"Ctrl+Shift+")
+
+
+    def testParseInvalid2 (self):
+        self.assertRaises (ValueError, self.parser.fromString, u"Ctrl+Shift+Alt+")
