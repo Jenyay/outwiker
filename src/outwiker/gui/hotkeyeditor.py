5c1ac490c6faba84703658c6c41b2ad4f54ad55b
Jenyay <jenyay.ilin@gmail.com>
2013-12-03 20:57:08 +0400

    Исправление в списке возможных горячих клавиш

--- src/outwiker/gui/hotkeyeditor.py
+++ src/outwiker/gui/hotkeyeditor.py
@@ -18,10 +18,10 @@ class HotkeyEditor (wx.Panel):
 
         hotkeys = r"""F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12
 0 1 2 3 4 5 6 7 8 9
-A B C D E F G H I J K L M N O P Q R S T U V W X Y Z,
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 Insert Enter Delete Home End Pageup Pagedown 
 Up Down Left Right
-/ \ * - + . _ = ` ,"""
+/ \ * - + . _ = ` , ?"""
 
         self._hotkeysList = [_("None")] + [item for item in hotkeys.split() if len (item) != 0]
 
