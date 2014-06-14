b68301093554dd3a0ac5e4eda9af0724214d51d2
Jenyay <jenyay.ilin@gmail.com>
2014-06-14 10:53:32 +0400

    Добавлены тесты на понимание википарсером ссылок вида page://

--- src/test/parsertests/parserurl.py
+++ src/test/parsertests/parserurl.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import os
 import unittest
-import hashlib
 
 from test.utils import removeWiki
 
 from outwiker.core.tree import WikiDocument
-from outwiker.core.attachment import Attachment
 from outwiker.core.application import Application
 
-from outwiker.pages.wiki.parser.wikiparser import Parser
 from outwiker.pages.wiki.wikipage import WikiPageFactory
 from outwiker.pages.wiki.parserfactory import ParserFactory
 
@@ -23,10 +19,10 @@ class ParserUrlTest (unittest.TestCase):
         self.filesPath = u"../test/samplefiles/"
 
         self.__createWiki()
-        
+
         factory = ParserFactory()
         self.parser = factory.make (self.testPage, Application.config)
-    
+
 
     def __createWiki (self):
         # Здесь будет создаваться вики
@@ -36,18 +32,18 @@ class ParserUrlTest (unittest.TestCase):
         self.rootwiki = WikiDocument.create (self.path)
         WikiPageFactory.create (self.rootwiki, u"Страница 2", [])
         self.testPage = self.rootwiki[u"Страница 2"]
-        
+
 
     def tearDown(self):
         removeWiki (self.path)
-    
+
 
     def testUrlParse1 (self):
         text = u"http://example.com/,"
         result = u'<a href="http://example.com/">http://example.com/</a>,'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
-    
+
 
     def testUrlParse2 (self):
         text = u"http://example.com/."
@@ -61,14 +57,14 @@ class ParserUrlTest (unittest.TestCase):
         result = u'<a href="http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)">http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)</a>,'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
-    
+
 
     def testUrlParse4 (self):
         text = u"http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)."
         result = u'<a href="http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)">http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)</a>.'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
-    
+
 
     def testUrlParse5 (self):
         text = u"http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/,"
@@ -76,7 +72,7 @@ class ParserUrlTest (unittest.TestCase):
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
-    
+
     def testUrlParse6 (self):
         text = u"http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/."
         result = u'<a href="http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/">http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/</a>.'
@@ -90,20 +86,20 @@ class ParserUrlTest (unittest.TestCase):
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
-    
+
     def testUrlParse8 (self):
         text = u"http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/"
         result = u'<a href="http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/">http://ru.wikipedia.org/wiki/xxx,_yyy_(zzz)/</a>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
-    
+
 
     def testUrlParse9 (self):
         text = u"www.jenyay.net"
         result = u'<a href="http://www.jenyay.net">www.jenyay.net</a>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
-    
+
 
     def testUrlParse10 (self):
         text = u"www.jenyay.net,"
@@ -132,7 +128,7 @@ class ParserUrlTest (unittest.TestCase):
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
-    
+
     def testUrlParse14 (self):
         text = u"http://rapidshare.com/#!download|514l34|373912473|ansys_hfss_12.1_with_fix.part1.rar|100431"
         result = u'<a href="http://rapidshare.com/#!download|514l34|373912473|ansys_hfss_12.1_with_fix.part1.rar|100431">http://rapidshare.com/#!download|514l34|373912473|ansys_hfss_12.1_with_fix.part1.rar|100431</a>'
@@ -227,3 +223,31 @@ class ParserUrlTest (unittest.TestCase):
         result = u'0.168.100.100'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
+
+
+    def testUrlParse28 (self):
+        text = u"page://__adsfasdfaf"
+        result = u'<a href="page://__adsfasdfaf">page://__adsfasdfaf</a>'
+
+        self.assertEqual (self.parser.toHtml (text), result)
+
+
+    def testUrlParse29 (self):
+        text = u"page://__абырвалг-ффф"
+        result = u'<a href="page://__абырвалг-ффф">page://__абырвалг-ффф</a>'
+
+        self.assertEqual (self.parser.toHtml (text), result)
+
+
+    def testUrlParse30 (self):
+        text = u"page://__adsfasdfaf/"
+        result = u'<a href="page://__adsfasdfaf/">page://__adsfasdfaf/</a>'
+
+        self.assertEqual (self.parser.toHtml (text), result)
+
+
+    def testUrlParse31 (self):
+        text = u"page://__абырвалг-ффф/"
+        result = u'<a href="page://__абырвалг-ффф/">page://__абырвалг-ффф/</a>'
+
+        self.assertEqual (self.parser.toHtml (text), result)
