bf7093d4b5b0188bbd8ad7596ed9cbca531852be
Jenyay <jenyay.ilin@gmail.com>
2014-08-26 12:16:55 +0400

    Исправлена прокрутка при перемещении страниц вверх/вниз в дереве

--- src/outwiker/actions/movepagedown.py
+++ src/outwiker/actions/movepagedown.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
 import wx
@@ -25,7 +24,7 @@ class MovePageDownAction (BaseAction):
     @property
     def description (self):
         return _(u"Move page down")
-    
+
 
     def run (self, params):
         self.moveCurrentPageDown ()
@@ -36,11 +35,16 @@ class MovePageDownAction (BaseAction):
         """
         Переместить текущую страницу на одну позицию вниз
         """
-        if self._application.wikiroot == None:
-            MessageBox (_(u"Wiki is not open"), 
-                    _(u"Error"), 
-                    wx.ICON_ERROR | wx.OK)
+        if self._application.wikiroot is None:
+            MessageBox (_(u"Wiki is not open"),
+                        _(u"Error"),
+                        wx.ICON_ERROR | wx.OK)
             return
 
-        if self._application.wikiroot.selectedPage != None:
+        if self._application.wikiroot.selectedPage is not None:
+            tree = self._application.mainWindow.treePanel.panel.treeCtrl
+            tree.Freeze()
+            scrollPos = tree.GetScrollPos (wx.VERTICAL)
             self._application.wikiroot.selectedPage.order += 1
+            tree.SetScrollPos (wx.VERTICAL, scrollPos)
+            tree.Thaw()
--- src/outwiker/actions/movepageup.py
+++ src/outwiker/actions/movepageup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
 import wx
@@ -25,7 +24,7 @@ class MovePageUpAction (BaseAction):
     @property
     def description (self):
         return _(u"Move page up")
-    
+
 
     def run (self, params):
         self.moveCurrentPageUp ()
@@ -36,11 +35,16 @@ class MovePageUpAction (BaseAction):
         """
         Переместить текущую страницу на одну позицию вверх
         """
-        if self._application.wikiroot == None:
-            MessageBox (_(u"Wiki is not open"), 
-                    _(u"Error"), 
-                    wx.ICON_ERROR | wx.OK)
+        if self._application.wikiroot is None:
+            MessageBox (_(u"Wiki is not open"),
+                        _(u"Error"),
+                        wx.ICON_ERROR | wx.OK)
             return
 
-        if self._application.wikiroot.selectedPage != None:
+        if self._application.wikiroot.selectedPage is not None:
+            tree = self._application.mainWindow.treePanel.panel.treeCtrl
+            tree.Freeze()
+            scrollPos = tree.GetScrollPos (wx.VERTICAL)
             self._application.wikiroot.selectedPage.order -= 1
+            tree.SetScrollPos (wx.VERTICAL, scrollPos)
+            tree.Thaw()
