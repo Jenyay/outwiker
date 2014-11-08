9dbda7d8598279eaccae742112806e299288393f
Jenyay <jenyay.ilin@gmail.com>
2014-11-08 10:10:43 +0300

    Names for localization was moved in separate file (iconslocales.py)

--- src/outwiker/core/iconscollection.py
+++ src/outwiker/core/iconscollection.py
@@ -21,23 +21,6 @@ class IconsCollection (object):
         """
         self._iconsDirList = iconsDirList
 
-        # Folder names for localization
-        _(u"awards")
-        _(u"battery")
-        _(u"books")
-        _(u"computer")
-        _(u"emotions")
-        _(u"flags")
-        _(u"folders")
-        _(u"food")
-        _(u"internet")
-        _(u"money")
-        _(u"people")
-        _(u"signs")
-        _(u"software")
-        _(u"tags")
-        _(u"weather")
-
         # Key - group name, value - list of the files (full paths)
         self._groups = {}
 
