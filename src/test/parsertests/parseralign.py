6519b33ebf8e012b0d4a53bfc12db63a26758c8a
Jenyay <jenyay.ilin@gmail.com>
2015-03-20 09:02:06 +0300

    Now for alignment wiki tags is used styles instead align attribute

--- src/test/parsertests/parseralign.py
+++ src/test/parsertests/parseralign.py
@@ -55,112 +55,112 @@ class ParserAlignTest (unittest.TestCase):
 
     def testCenter1 (self):
         text = u"бла-бла-бла \n%center%кхм бла-бла-бла\nбла-бла-бла"
-        result = u'бла-бла-бла \n<div align="center">кхм бла-бла-бла\nбла-бла-бла</div>'
+        result = u'бла-бла-бла \n<div style="text-align:center">кхм бла-бла-бла\nбла-бла-бла</div>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCenter2 (self):
         text = u"бла-бла-бла \n%center%кхм бла-бла-бла\n\nбла-бла-бла"
-        result = u'бла-бла-бла \n<div align="center">кхм бла-бла-бла</div>\n\nбла-бла-бла'
+        result = u'бла-бла-бла \n<div style="text-align:center">кхм бла-бла-бла</div>\n\nбла-бла-бла'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCenter3 (self):
         text = u"%center%бла-бла-бла \nкхм бла-бла-бла\n\nбла-бла-бла"
-        result = u'<div align="center">бла-бла-бла \nкхм бла-бла-бла</div>\n\nбла-бла-бла'
+        result = u'<div style="text-align:center">бла-бла-бла \nкхм бла-бла-бла</div>\n\nбла-бла-бла'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCenter4 (self):
         text = u"%center%бла-бла-бла \n'''кхм''' бла-бла-бла\n\nбла-бла-бла"
-        result = u'<div align="center">бла-бла-бла \n<b>кхм</b> бла-бла-бла</div>\n\nбла-бла-бла'
+        result = u'<div style="text-align:center">бла-бла-бла \n<b>кхм</b> бла-бла-бла</div>\n\nбла-бла-бла'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCenter5 (self):
         text = u"бла-бла-бла \n\n% center %Attach:accept.png\n\nбла-бла-бла\nбла-бла-бла"
-        result = u'бла-бла-бла \n\n<div align="center"><img src="__attach/accept.png"/></div>\n\nбла-бла-бла\nбла-бла-бла'
+        result = u'бла-бла-бла \n\n<div style="text-align:center"><img src="__attach/accept.png"/></div>\n\nбла-бла-бла\nбла-бла-бла'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testRight1 (self):
         text = u"бла-бла-бла \n% right %кхм бла-бла-бла\nбла-бла-бла"
-        result = u'бла-бла-бла \n<div align="right">кхм бла-бла-бла\nбла-бла-бла</div>'
+        result = u'бла-бла-бла \n<div style="text-align:right">кхм бла-бла-бла\nбла-бла-бла</div>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand1 (self):
         text = u"бла-бла-бла \n% right %(:command:)"
-        result = u'бла-бла-бла \n<div align="right">(:command:)</div>'
+        result = u'бла-бла-бла \n<div style="text-align:right">(:command:)</div>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand2 (self):
         text = u"бла-бла-бла \n% right %(:command:)\nАбырвалг"
-        result = u'бла-бла-бла \n<div align="right">(:command:)\nАбырвалг</div>'
+        result = u'бла-бла-бла \n<div style="text-align:right">(:command:)\nАбырвалг</div>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand3 (self):
         text = u"бла-бла-бла \n% right %Абырвалг (:command:)\nАбырвалг"
-        result = u'бла-бла-бла \n<div align="right">Абырвалг (:command:)\nАбырвалг</div>'
+        result = u'бла-бла-бла \n<div style="text-align:right">Абырвалг (:command:)\nАбырвалг</div>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand4 (self):
         text = u"бла-бла-бла \n% right %(:command:) Абырвалг"
-        result = u'бла-бла-бла \n<div align="right">(:command:) Абырвалг</div>'
+        result = u'бла-бла-бла \n<div style="text-align:right">(:command:) Абырвалг</div>'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand5 (self):
         text = u"бла-бла-бла \n% right %(:command:) Абырвалг\n\n111"
-        result = u'бла-бла-бла \n<div align="right">(:command:) Абырвалг</div>\n\n111'
+        result = u'бла-бла-бла \n<div style="text-align:right">(:command:) Абырвалг</div>\n\n111'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand6 (self):
         text = u"бла-бла-бла \n% right %(:command:)qqq\n\nwww\n(:commandend:) Абырвалг\n\n111"
-        result = u'бла-бла-бла \n<div align="right">(:command:)qqq\n\nwww\n(:commandend:) Абырвалг</div>\n\n111'
+        result = u'бла-бла-бла \n<div style="text-align:right">(:command:)qqq\n\nwww\n(:commandend:) Абырвалг</div>\n\n111'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand7 (self):
         text = u"бла-бла-бла \n% right %1111 (:command:)qqq\n\nwww\n(:commandend:) Абырвалг\n\n111"
-        result = u'бла-бла-бла \n<div align="right">1111 (:command:)qqq\n\nwww\n(:commandend:) Абырвалг</div>\n\n111'
+        result = u'бла-бла-бла \n<div style="text-align:right">1111 (:command:)qqq\n\nwww\n(:commandend:) Абырвалг</div>\n\n111'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testCommand8 (self):
         text = u"бла-бла-бла \n%right%1111 (:command:)111\n\n222(:commandend:) Абырвалг (:command2:)333\n\n\n444(:command2end:)\n\n777"
-        result = u'бла-бла-бла \n<div align="right">1111 (:command:)111\n\n222(:commandend:) Абырвалг (:command2:)333\n\n\n444(:command2end:)</div>\n\n777'
+        result = u'бла-бла-бла \n<div style="text-align:right">1111 (:command:)111\n\n222(:commandend:) Абырвалг (:command2:)333\n\n\n444(:command2end:)</div>\n\n777'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testLeft1 (self):
         text = u"%left%бла-бла-бла \n'''кхм''' бла-бла-бла\n\nбла-бла-бла"
-        result = u'<div align="left">бла-бла-бла \n<b>кхм</b> бла-бла-бла</div>\n\nбла-бла-бла'
+        result = u'<div style="text-align:left">бла-бла-бла \n<b>кхм</b> бла-бла-бла</div>\n\nбла-бла-бла'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
 
     def testJustify1 (self):
         text = u"%justify%бла-бла-бла \n'''кхм''' бла-бла-бла\n\nбла-бла-бла"
-        result = u'<div align="justify">бла-бла-бла \n<b>кхм</b> бла-бла-бла</div>\n\nбла-бла-бла'
+        result = u'<div style="text-align:justify">бла-бла-бла \n<b>кхм</b> бла-бла-бла</div>\n\nбла-бла-бла'
 
         self.assertEqual (self.parser.toHtml (text), result, self.parser.toHtml (text).encode (self.encoding))
 
