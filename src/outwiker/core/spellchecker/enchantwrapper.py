f366f7559030309eaf8650354f080a8c559f36de
Jenyay <jenyay.ilin@gmail.com>
2015-07-23 09:44:21 +0300

    Added cache of the spell dictionaries

--- src/outwiker/core/spellchecker/enchantwrapper.py
+++ src/outwiker/core/spellchecker/enchantwrapper.py
@@ -9,6 +9,9 @@ class EnchantWrapper (object):
     """
     Wrapper around pyenchant (http://pythonhosted.org/pyenchant/)
     """
+    # Key - tuple (lang, path), value - Dict instance
+    _dictCache = {}
+
     def __init__ (self, langlist, folders):
         """
         langlist - list of the languages ("ru_RU", "en_US", etc)
@@ -18,9 +21,20 @@ class EnchantWrapper (object):
 
         for lang in langlist:
             for path in dictsFinder.getFoldersForLang (lang):
-                broker = Broker ()
-                broker.set_param ('enchant.myspell.dictionary.path', path)
-                self._checkers.append (Dict (lang, broker))
+                self._checkers.append (self._getDict (lang, path))
+
+
+    def _getDict (self, lang, path):
+        key = (lang, path)
+        if key not in self._dictCache:
+            broker = Broker ()
+            broker.set_param ('enchant.myspell.dictionary.path', path)
+            currentDict = Dict (lang, broker)
+            self._dictCache[key] = currentDict
+        else:
+            currentDict = self._dictCache[key]
+
+        return currentDict
 
 
     def check (self, word):
