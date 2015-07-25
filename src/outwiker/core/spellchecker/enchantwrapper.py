af9d608c90bda75a000928ba426dff558b42f46e
Jenyay <jenyay.ilin@gmail.com>
2015-07-25 21:01:04 +0300

    Added the custom dictionary for spell checking

--- src/outwiker/core/spellchecker/enchantwrapper.py
+++ src/outwiker/core/spellchecker/enchantwrapper.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-from enchant import Dict, Broker
+import os.path
+
+from enchant import Dict, DictWithPWL, Broker
 import enchant.errors
 
 from dictsfinder import DictsFinder
@@ -19,6 +21,8 @@ class EnchantWrapper (object):
         """
         dictsFinder = DictsFinder (folders)
         self._checkers = []
+        self._customDictPath = os.path.join (folders[-1], u'custom.dic')
+        self._customTag = '__custom'
 
         for lang in langlist:
             for path in dictsFinder.getFoldersForLang (lang):
@@ -26,6 +30,31 @@ class EnchantWrapper (object):
                     self._checkers.append (self._getDict (lang, path))
                 except enchant.errors.Error:
                     pass
+
+        try:
+            self._checkers.append (self._getCustomDict ())
+        except enchant.errors.Error:
+            pass
+        except IOError:
+            pass
+
+
+    def _getCustomDict (self):
+        key = (self._customTag, self._customDictPath)
+
+        if key not in self._dictCache:
+            broker = Broker ()
+            broker.set_param ('enchant.myspell.dictionary.path',
+                              self._customDictPath)
+
+            currentDict = DictWithPWL (None,
+                                       self._customDictPath,
+                                       broker=broker)
+            self._dictCache[key] = currentDict
+        else:
+            currentDict = self._dictCache[key]
+
+        return currentDict
 
 
     def _getDict (self, lang, path):
