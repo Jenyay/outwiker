c866c5e7a7ce677f6f50a63eb233f055e2749196
Jenyay <jenyay.ilin@gmail.com>
2015-10-13 13:48:31 +0300

    Changed logging level

--- src/outwiker/core/logredirector.py
+++ src/outwiker/core/logredirector.py
@@ -20,7 +20,7 @@ class LogRedirector(object):
 
         self._runTime = time.strftime (u'%Y-%m-%d %H:%M:%S')
         logging.basicConfig (format = '%(asctime)s - %(levelname)s - %(message)s',
-                             level = logging.INFO)
+                             level = logging.WARNING)
 
 
     def write(self, message):
