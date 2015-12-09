3e908e686b65b518672bf19c27b5cbb825f32225
Jenyay <jenyay.ilin@gmail.com>
2015-12-09 18:01:04 +0300

    Added test fot Downloader class

--- src/test/plugins/webpage/test_downloader.py
+++ src/test/plugins/webpage/test_downloader.py
@@ -1,5 +1,7 @@
 # -*- coding: UTF-8 -*-
 
+import os.path
+import urllib
 import unittest
 
 from outwiker.core.application import Application
@@ -8,7 +10,8 @@ from outwiker.core.pluginsloader import PluginsLoader
 
 class DownloaderTest (unittest.TestCase):
     def setUp (self):
-        self.plugindirlist = [u"../plugins/webpage"]
+        self.plugindirlist = [u'../plugins/webpage']
+        self._staticDirName = u'download'
 
         self.loader = PluginsLoader(Application)
         self.loader.load (self.plugindirlist)
@@ -16,3 +19,18 @@ class DownloaderTest (unittest.TestCase):
 
     def tearDown (self):
         self.loader.clear()
+
+
+    def testEmpty (self):
+        from webpage.downloader import Downloader
+        downloader = Downloader (self._staticDirName)
+
+        fname = self._path2url (u'../test/webpage/empty.html')
+        downloader.start (fname)
+
+
+    @staticmethod
+    def _path2url (path):
+        path = os.path.abspath(path)
+        path = path.encode('utf8')
+        return 'file:' + urllib.pathname2url(path)
