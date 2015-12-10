b01f4eb9cffd5d6e2a3cf42a755a1a85636e9314
Jenyay <jenyay.ilin@gmail.com>
2015-12-10 17:22:14 +0300

    WebPage plugin.

--- plugins/webpage/webpage/downloader.py
+++ plugins/webpage/webpage/downloader.py
@@ -2,32 +2,43 @@
 
 from abc import ABCMeta, abstractmethod
 import urllib2
+from urlparse import urljoin
 
 from bs4 import BeautifulSoup
 
 
 class Downloader (object):
-    def __init__ (self, staticDirName, controller, timeout=20):
+    def __init__ (self, staticDirName, timeout=20):
         self._staticDir = staticDirName
-        self._controller = controller
         self._timeout = 20
 
         self._content_src = None
+        self._pageTitle = None
+        self._soup = None
 
 
-    def start (self, url):
+    def start (self, url, controller):
         obj = urllib2.urlopen (url, timeout=self._timeout)
-        soup = BeautifulSoup(obj.read(), 'html.parser')
-        self._content_src = soup.prettify()
+        self._soup = BeautifulSoup(obj.read(), 'html.parser')
+        self._content_src = self._soup.prettify()
 
+        if self._soup.title is not None:
+            self._pageTitle = self._soup.title.string
 
-    def nextFile (self):
-        pass
+        images = self._soup.find_all (u'img')
+        for image in images:
+            src = urljoin (url, image['src'])
+            controller.process (src, image)
 
 
     @property
     def content_src (self):
         return self._content_src
+
+
+    @property
+    def pageTitle (self):
+        return self._pageTitle
 
 
 
--- src/test/plugins/webpage/test_downloader.py
+++ src/test/plugins/webpage/test_downloader.py
@@ -23,20 +23,51 @@ class DownloaderTest (unittest.TestCase):
 
     def testEmpty (self):
         from webpage.downloader import Downloader
-        downloader = Downloader (self._staticDirName,
-                                 self._getTestController())
+        controller = self._getTestController()
+        downloader = Downloader (self._staticDirName)
 
         fname = self._path2url (u'../test/webpage/example0/example0.html')
-        downloader.start (fname)
+        downloader.start (fname, controller)
         self.assertIn (u'<html>', downloader.content_src)
+        self.assertEqual (downloader.pageTitle, None)
+
+
+    def testExample1 (self):
+        from webpage.downloader import Downloader
+        controller = self._getTestController()
+        downloader = Downloader (self._staticDirName)
+
+        examplePath = u'../test/webpage/example1/'
+        exampleHtmlPath = os.path.join (examplePath, u'example1.html')
+
+        downloader.start (self._path2url (exampleHtmlPath), controller)
+        self.assertEqual (downloader.pageTitle, u'Заголовок страницы')
+
+        imagePath_01 = os.path.join (examplePath, u'image_01.png')
+        self.assertEqual (controller.files[0][0], self._path2url (imagePath_01))
+
+        imagePath_02 = u'file:///images/image_02.png'
+        self.assertEqual (controller.files[1][0], imagePath_02)
+
+        imagePath_03 = os.path.join (examplePath, u'images', u'image_03.png')
+        self.assertEqual (controller.files[2][0], self._path2url (imagePath_03))
+
+        imagePath_04 = os.path.join (examplePath,
+                                     u'images',
+                                     u'subfolder',
+                                     u'image_04.png')
+        self.assertEqual (controller.files[3][0], self._path2url (imagePath_04))
 
 
     def _getTestController (self):
         from webpage.downloader import BaseDownloadController
 
         class TestController (BaseDownloadController):
+            def __init__ (self):
+                self.files = []
+
             def process (self, url, node):
-                pass
+                self.files.append ((url, node))
 
         return TestController()
 
