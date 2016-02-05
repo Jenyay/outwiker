347eb4633b44d036b175f07d31bb488b13b14285
Jenyay <jenyay.ilin@gmail.com>
2016-02-05 14:31:06 +0300

    WebPage. Added tests

---
+++ src/test/plugins/webpage/test_loading.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+from test.plugins.baseloading import BasePluginLoadingTest
+
+
+class WebPageLoadingTest (BasePluginLoadingTest):
+    def getPluginDir (self):
+        """
+        Должен возвращать путь до папки с тестируемым плагином
+        """
+        return u"../plugins/webpage"
+
+
+    def getPluginName (self):
+        """
+        Должен возвращать имя плагина, по которому его можно найти в PluginsLoader
+        """
+        return "WebPage"
