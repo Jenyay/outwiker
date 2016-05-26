2ed2cd73a708d07a26b5c99503e2eaf21ea8da00
Eugeniy Ilin <jenyay.ilin@gmail.com>
2016-05-26 17:23:50 +0300

    Markdown plug-in. Added Markdown page to page dialog

--- plugins/markdown/markdown/controller.py
+++ plugins/markdown/markdown/controller.py
@@ -21,10 +21,16 @@ class Controller (object):
         Инициализация контроллера при активации плагина. Подписка на нужные события
         """
         FactorySelector.addFactory(MarkdownPageFactory())
+        self._application.onPageDialogPageFactoriesNeeded += self.__onPageDialogPageFactoriesNeeded
 
 
-    def destroy (self):
+    def clear (self):
         """
         Вызывается при отключении плагина
         """
         FactorySelector.removeFactory (MarkdownPageFactory().getTypeString())
+        self._application.onPageDialogPageFactoriesNeeded -= self.__onPageDialogPageFactoriesNeeded
+
+
+    def __onPageDialogPageFactoriesNeeded (self, page, params):
+        params.addPageFactory (MarkdownPageFactory())
