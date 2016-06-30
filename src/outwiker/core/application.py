474bc371e74bc52cffae739e5fa9d4ad54a60c59
Eugeniy Ilin <jenyay.ilin@gmail.com>
2016-06-30 17:19:11 +0300

    Code cleanup

--- src/outwiker/core/application.py
+++ src/outwiker/core/application.py
@@ -8,8 +8,8 @@ from outwiker.core.pluginsloader import PluginsLoader
 from outwiker.core.pageuiddepot import PageUidDepot
 
 
-class ApplicationParams (object):
-    def __init__ (self):
+class ApplicationParams(object):
+    def __init__(self):
         # Current at given time wiki
         self.__wikiroot = None
 
@@ -20,7 +20,7 @@ class ApplicationParams (object):
         self.config = None
         self.recentWiki = None
         self.actionController = None
-        self.plugins = PluginsLoader (self)
+        self.plugins = PluginsLoader(self)
         self.pageUidDepot = PageUidDepot()
 
         # Anchor for transition during the opening other page
@@ -32,12 +32,12 @@ class ApplicationParams (object):
 
         # Opening wiki event
         # Parameters:
-        #     root - opened wiki root (it may be None)
+        #     root - opened wiki root(it may be None)
         self.onWikiOpen = Event()
 
         # Closing wiki event
         # Parameters:
-        #     root - closed wiki root (it may be None)
+        #     root - closed wiki root(it may be None)
         self.onWikiClose = Event()
 
         # Updating page wiki event
@@ -64,7 +64,7 @@ class ApplicationParams (object):
 
         # User want insert link to selected attached files to page
         # Parameters:
-        #     fnames - selected file names (names only, without full paths)
+        #     fnames - selected file names(names only, without full paths)
         self.onAttachmentPaste = Event()
 
         # Changings in the bookmarks list event
@@ -83,12 +83,12 @@ class ApplicationParams (object):
         #     oldSubpath - previous relative path to page
         self.onPageRename = Event()
 
-        # Beginning complex tree updating (updating of several steps) event
+        # Beginning complex tree updating(updating of several steps) event
         # Parameters:
         #     root - wiki tree root
         self.onStartTreeUpdate = Event()
 
-        # Finishing complex tree updating (updating of several steps) event
+        # Finishing complex tree updating(updating of several steps) event
         # Parameters:
         #     root - wiki tree root
         self.onEndTreeUpdate = Event()
@@ -110,64 +110,71 @@ class ApplicationParams (object):
         #     page - page with changed order
         self.onPageOrderChange = Event()
 
-        # Evont for forced saving page state (e.g. by the loss the focus or by timer)
+        # Evont for forced saving page state
+        # (e.g. by the loss the focus or by timer)
         # Parameters:
         #     --
         self.onForceSave = Event()
 
-        # The event occurs after wiki parser (Parser cllass) creation, but before it using
+        # The event occurs after wiki parser(Parser cllass) creation,
+        # but before it using
         # Parameter:
         #     parser - Parser class instance
-        self.onWikiParserPrepare = Event ()
+        self.onWikiParserPrepare = Event()
 
         # Event occurs during preferences dialog creation
         # Parameters:
-        #     dialog - outwiker.gui.preferences.prefdialog.PrefDialog class instance
+        #     dialog - outwiker.gui.preferences.prefdialog.PrefDialog
+        #              class instance
         self.onPreferencesDialogCreate = Event()
 
         # Event occurs after preferences dialog closing.
         # Parameters:
-        #     dialog - outwiker.gui.preferences.prefdialog.PrefDialog class instance
+        #     dialog - outwiker.gui.preferences.prefdialog.PrefDialog
+        #              class instance
         self.onPreferencesDialogClose = Event()
 
-        # Event occurs after (!) the page view creation (inside CurrentPagePanel instance)
+        # Event occurs after(!) the page view creation
+        # (inside CurrentPagePanel instance)
         # Parameters:
         #     page - new selected page
         self.onPageViewCreate = Event()
 
-        # Event occurs before (!) the page view removing  (inside CurrentPagePanel instance)
+        # Event occurs before(!) the page view removing
+        # (inside CurrentPagePanel instance)
         # Parameters:
         #     page - Current selected page
         self.onPageViewDestroy = Event()
 
-        # Event occurs after the popup menu creation by right mouse click on the notes tree
+        # Event occurs after the popup menu creation by right mouse click
+        # on the notes tree
         # Parameters:
         #     menu - created popup menu,
         #     page - the page on which was right clicked in the notes tree
         self.onTreePopupMenu = Event()
 
-        # Event occurs after the popup menu creation by right mouse click on the tree
+        # Event occurs after the popup menu creation by right mouse click
+        # on the tree
         # Parameters:
         #     menu - created popup menu,
         #     tray - the OutwikerTrayIcon class instance
         self.onTrayPopupMenu = Event()
 
-
-        # Event occurs before HTML generation (for wiki and HTML pages)
+        # Event occurs before HTML generation(for wiki and HTML pages)
         # Order of the calling preprocessing events is not regulated
         # Parameters:
         #    page - page for which HTML is generated
-        #    params - instance of the outwiker.core.events.PreprocessingParams class
+        #    params - instance of the outwiker.core.events.PreprocessingParams
+        #             class
         self.onPreprocessing = Event()
 
-
-        # Event occurs after HTML generation (for wiki and HTML pages)
+        # Event occurs after HTML generation(for wiki and HTML pages)
         # Order of the calling preprocessing events is not regulated
         # Parameters:
         #    page - page for which HTML is generated
-        #    params - instance of the outwiker.core.events.PostprocessingParams class
+        #    params - instance of the outwiker.core.events.PostprocessingParams
+        #             class
         self.onPostprocessing = Event()
-
 
         # Event occurs during HtmlImproverFactory instance creation
         # Parameters:
@@ -178,177 +185,163 @@ class ApplicationParams (object):
         # Event occurs after wiki parsing but before HTML improveing
         # Parameters:
         #     page - page for which HTML is generated
-        #     params - instance of the outwiker.core.events.PreHtmlImprovingParams class
+        #     params - instance of the
+        #              outwiker.core.events.PreHtmlImprovingParams class
         self.onPreHtmlImproving = Event()
-
 
         # Event occurs when cursor hovers under link on preview tab
         # Parameters:
         #     page - current page
-        #     params - instance of the outwiker.core.events.HoverLinkParams class
+        #     params - instance of the outwiker.core.events.HoverLinkParams
+        #              class
         self.onHoverLink = Event()
-
 
         # Event occurs when user click to link on a page
         # Parameters:
         #     page - current page
-        #     params - instance of the outwiker.core.events.LinkClickParams class
+        #     params - instance of the outwiker.core.events.LinkClickParams
+        #              class
         self.onLinkClick = Event()
-
 
         # Event occurs when user click with right button in text editor
         # Parameters:
         #     page - current page
-        #     params - instance of the the outwiker.core.events.EditorPopupMenuParams class
+        #     params - instance of the the
+        #              outwiker.core.events.EditorPopupMenuParams class
         self.onEditorPopupMenu = Event()
-
 
         # Event occurs after page dialog creation
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogInitParams class
         self.onPageDialogInit = Event()
 
-
         # Event occurs before page dialog will be destroyed
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogDestroyParams class
         self.onPageDialogDestroy = Event()
 
-
         # Event occurs after page type changing
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogPageTypeChangedParams class
         self.onPageDialogPageTypeChanged = Event()
 
-
         # Event occurs after page type changing
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogPageTitleChangedParams class
         self.onPageDialogPageTitleChanged = Event()
 
-
         # Event occurs after page style changing
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogPageStyleChangedParams class
         self.onPageDialogPageStyleChanged = Event()
 
-
         # Event occurs after page icon changing
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogPageIconChangedParams class
         self.onPageDialogPageIconChanged = Event()
 
-
         # Event occurs after page tag list changing
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageDialogPageTagsChangedParams class
         self.onPageDialogPageTagsChanged = Event()
 
-
         # Event occurs during page dialog initialization,
         # during general panel creation. Evens sender expect what event
-        # handlers will fill the page factories list with addPageFactory method.
+        # handlers will fill the page factories list with addPageFactory method
         # Parameters:
-        #     page - current (selected) page
-        #     params - instance of the PageDialogPageFactoriesNeededParams class
+        #     page - current(selected) page
+        #     params - instance of the PageDialogPageFactoriesNeededParams
+        #              class
         self.onPageDialogPageFactoriesNeeded = Event()
-
 
         # Event occurs by TextEditor when it needs styles
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the EditorStyleNeededParams class
         self.onEditorStyleNeeded = Event()
 
         # Event forces update and render current page
         # Parameters:
-        #     page - current (selected) page
+        #     page - current(selected) page
         #     params - instance of the PageUpdateNeededParams class
         self.onPageUpdateNeeded = Event()
 
         # Event occurs before wiki opening
         # Parameters:
-        #    page - current (selected) page
+        #    page - current(selected) page
         #    params - instance of the PreWikiOpenParams class
         self.onPreWikiOpen = Event()
 
         # Event occurs before wiki opening
         # Parameters:
-        #    page - current (selected) page
+        #    page - current(selected) page
         #    params - instance of the PostWikiOpenParams class
         self.onPostWikiOpen = Event()
 
-
-    def init (self, configFilename):
+    def init(self, configFilename):
         """
         Initialize config and locale
         """
-        self.config = Config (configFilename)
-        self.recentWiki = RecentWiki (self.config)
+        self.config = Config(configFilename)
+        self.recentWiki = RecentWiki(self.config)
         self.__initLocale()
 
-
     @property
-    def anchor (self):
+    def anchor(self):
         return self._anchor
 
-
     @anchor.setter
-    def anchor (self, value):
+    def anchor(self, value):
         self._anchor = value
 
-
     @property
-    def wikiroot (self):
+    def wikiroot(self):
         """
-        Возвращает корень открытой в данный момент вики или None, если нет открытой вики
+        Возвращает корень открытой в данный момент вики или None,
+        если нет открытой вики
         """
         return self.__wikiroot
 
-
     @wikiroot.setter
-    def wikiroot (self, value):
+    def wikiroot(self, value):
         """
         Установить текущую вики
         """
-        self.onWikiClose (self.__wikiroot)
+        self.onWikiClose(self.__wikiroot)
 
         if self.__wikiroot is not None:
-            self.__unbindWikiEvents (self.__wikiroot)
+            self.__unbindWikiEvents(self.__wikiroot)
 
         self.__wikiroot = value
 
         if self.__wikiroot is not None:
-            self.__bindWikiEvents (self.__wikiroot)
+            self.__bindWikiEvents(self.__wikiroot)
 
         self.pageUidDepot = PageUidDepot(self.__wikiroot)
-        self.onWikiOpen (self.__wikiroot)
-
+        self.onWikiOpen(self.__wikiroot)
 
     @property
-    def mainWindow (self):
+    def mainWindow(self):
         """
         Возвращает главное окно программы или None, если оно еще не создано
         """
         return self.__mainWindow
 
-
     @mainWindow.setter
-    def mainWindow (self, value):
+    def mainWindow(self, value):
         """
         Установить главное окно программы
         """
         self.__mainWindow = value
 
-
-    def __bindWikiEvents (self, wiki):
+    def __bindWikiEvents(self, wiki):
         """
         Подписка на события, связанные с открытой вики для передачи их дальше
         """
@@ -363,8 +356,7 @@ class ApplicationParams (object):
         wiki.onPageRemove += self.onPageRemove
         wiki.bookmarks.onBookmarksChanged += self.onBookmarksChanged
 
-
-    def __unbindWikiEvents (self, wiki):
+    def __unbindWikiEvents(self, wiki):
         """
         Отписаться от события, связанных с открытой вики
         """
@@ -379,35 +371,34 @@ class ApplicationParams (object):
         wiki.onPageRemove -= self.onPageRemove
         wiki.bookmarks.onBookmarksChanged -= self.onBookmarksChanged
 
-
     @property
-    def selectedPage (self):
+    def selectedPage(self):
         """
-        Вернуть текущую страницу или None, если страница не выбрана или вики не открыта
+        Вернуть текущую страницу или None,
+        если страница не выбрана или вики не открыта
         """
         if self.__wikiroot is None:
             return None
 
         return self.__wikiroot.selectedPage
 
-
     @selectedPage.setter
-    def selectedPage (self, page):
+    def selectedPage(self, page):
         """
         Установить текущую страницу
         """
-        if self.__wikiroot is not None and self.__wikiroot.selectedPage != page:
+        if (self.__wikiroot is not None and
+                self.__wikiroot.selectedPage != page):
             self.__wikiroot.selectedPage = page
 
-
-    def __initLocale (self):
+    def __initLocale(self):
         """
         Инициализации локализаций интерфейса
         """
-        language = getLanguageFromConfig (self.config)
+        language = getLanguageFromConfig(self.config)
 
         try:
-            init_i18n (language)
+            init_i18n(language)
         except IOError:
             print u"Can't load language: %s" % language
 
