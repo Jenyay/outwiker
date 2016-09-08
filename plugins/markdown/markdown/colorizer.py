5a7a886b29e99a4f0533263e72f63f01256cf9df
Jenyay <jenyay.ilin@gmail.com>
2016-09-08 09:16:15 +0300

    Markdown. Code cleaning.

--- plugins/markdown/markdown/colorizer.py
+++ plugins/markdown/markdown/colorizer.py
@@ -1,12 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-from parser.tokenfonts import (FontsFactory,
-                               BoldToken,
-                               ItalicToken,
-                               BoldItalicToken)
-from parser.tokentext import TextFactory
-from parser.tokenheading import HeadingFactory
-from parser.tokenlink import LinkFactory
+from markdownparser.tokens.tokenfonts import (FontsFactory,
+                                              BoldToken,
+                                              ItalicToken,
+                                              BoldItalicToken)
+from markdownparser.tokens.tokentext import TextFactory
+from markdownparser.tokens.tokenheading import HeadingFactory
+from markdownparser.tokens.tokenlink import LinkFactory
 
 from outwiker.gui.texteditorhelper import TextEditorHelper
 
@@ -24,39 +24,23 @@ class MarkdownColorizer(object):
         self.bold_italic = FontsFactory.makeBoldItalic()
         self.heading = HeadingFactory.make()
         self.link = LinkFactory.make()
-        # self.underline = FontsFactory.makeUnderline(None)
-        # self.command = CommandFactory.make (None).setParseAction(lambda s, l, t: None)
-        # self.url = UrlFactory.make (None).setParseAction(lambda s, l, t: None)
-        # self.linebreak = LineBreakFactory.make (None).setParseAction(lambda s, l, t: None)
-        # self.noformat = NoFormatFactory.make (None).setParseAction(lambda s, l, t: None)
-        # self.preformat = PreFormatFactory.make (None).setParseAction(lambda s, l, t: None)
 
         if colorizeSyntax:
             self.colorParser = (
-                # self.url |
                 self.heading |
                 self.text |
-                # self.linebreak |
                 self.link |
-                # self.noformat |
-                # self.preformat |
                 self.bold_italic |
                 self.bold |
                 self.italic
-                # self.underline |
             )
 
             self.insideBlockParser = (
-                # self.url |
                 self.text |
-                # self.linebreak |
                 self.link |
-                # self.noformat |
-                # self.preformat |
                 self.bold_italic |
                 self.bold |
                 self.italic
-                # self.underline
             )
         else:
             self.colorParser = self.text
@@ -97,8 +81,8 @@ class MarkdownColorizer(object):
                 continue
 
             # Нас интересует позиция в байтах, а не в символах
-            bytepos_start = self._helper.calcBytePos (text, pos_start)
-            bytepos_end = self._helper.calcBytePos (text, pos_end)
+            bytepos_start = self._helper.calcBytePos(text, pos_start)
+            bytepos_end = self._helper.calcBytePos(text, pos_end)
 
             # Применим стиль
             if tokenname == "bold":
@@ -108,8 +92,8 @@ class MarkdownColorizer(object):
                                       bytepos_end)
                 self._colorizeText(fullText,
                                    text,
-                                   pos_start + len (BoldToken.start_1),
-                                   pos_end - len (BoldToken.end_1),
+                                   pos_start + len(BoldToken.start_1),
+                                   pos_end - len(BoldToken.end_1),
                                    self.insideBlockParser, stylelist)
 
             elif tokenname == "italic":
@@ -119,8 +103,8 @@ class MarkdownColorizer(object):
                                       bytepos_end)
                 self._colorizeText(fullText,
                                    text,
-                                   pos_start + len (ItalicToken.start_1),
-                                   pos_end - len (ItalicToken.end_1),
+                                   pos_start + len(ItalicToken.start_1),
+                                   pos_end - len(ItalicToken.end_1),
                                    self.insideBlockParser, stylelist)
 
             elif tokenname == "bold_italic":
@@ -130,22 +114,10 @@ class MarkdownColorizer(object):
                                       bytepos_end)
                 self._colorizeText(fullText,
                                    text,
-                                   pos_start + len (BoldItalicToken.start),
-                                   pos_end - len (BoldItalicToken.end),
+                                   pos_start + len(BoldItalicToken.start),
+                                   pos_end - len(BoldItalicToken.end),
                                    self.insideBlockParser, stylelist)
 
-            # elif tokenname == "underline":
-            #     self._helper.addStyle(stylelist,
-            #                           self._editor.STYLE_UNDERLINE_ID,
-            #                           bytepos_start,
-            #                           bytepos_end)
-            #     self._colorizeText(fullText,
-            #                        text,
-            #                        pos_start + len (UnderlineToken.start),
-            #                        pos_end - len (UnderlineToken.end),
-            #                        self.insideBlockParser,
-            #                        stylelist)
-            #
             elif tokenname == "heading":
                 self._helper.setStyle(stylelist,
                                       self._editor.STYLE_HEADING_ID,
@@ -170,13 +142,8 @@ class MarkdownColorizer(object):
                                             pos_end,
                                             token)
 
-            # elif tokenname == "url":
-            #     self._helper.addStyle(stylelist,
-            #                           self._editor.STYLE_LINK_ID,
-            #                           bytepos_start,
-            #                           bytepos_end)
-
-    def _linkSpellChecking (self, fullText, text, stylelist, pos_start, pos_end, token):
+    def _linkSpellChecking(self, fullText, text, stylelist,
+                           pos_start, pos_end, token):
         self._editor.runSpellChecking(stylelist,
                                       fullText,
                                       pos_start + 1,
@@ -184,7 +151,9 @@ class MarkdownColorizer(object):
         link = token[0][1]
         space_pos = link.find(u' ')
         if space_pos != -1:
-            self._editor.runSpellChecking(stylelist,
-                                          fullText,
-                                          pos_start + 1 + len(token[0][0]) + 2 + space_pos,
-                                          pos_start + 1 + len(token[0][0]) + 2 + space_pos + len(token[0][1]))
+            self._editor.runSpellChecking(
+                stylelist,
+                fullText,
+                pos_start + 1 + len(token[0][0]) + 2 + space_pos,
+                pos_start + 1 + len(token[0][0]) + 2 + space_pos +
+                len(token[0][1]))
