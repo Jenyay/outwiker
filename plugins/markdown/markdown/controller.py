48b654c245a336beb70f6c41cc635b4355cd69c4
Jenyay <jenyay.ilin@gmail.com>
2016-09-18 19:17:20 +0300

    Markdown. Added the styles support.

--- plugins/markdown/markdown/controller.py
+++ plugins/markdown/markdown/controller.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 from outwiker.core.factoryselector import FactorySelector
+from outwiker.gui.pagedialogpanels.appearancepanel import(AppearancePanel,
+                                                          AppearanceController)
 
 from .markdownpage import MarkdownPageFactory, MarkdownPage
 from .colorizercontroller import ColorizerController
@@ -15,6 +17,10 @@ class Controller (object):
         """
         self._plugin = plugin
         self._application = application
+
+        self._appearancePanel = None
+        self._appearanceController = None
+
         self._colorizerController = ColorizerController(
             self._application,
             MarkdownPage.getTypeString())
@@ -27,6 +33,8 @@ class Controller (object):
         self._application.onPageDialogPageFactoriesNeeded += self.__onPageDialogPageFactoriesNeeded
         self._application.onPageViewCreate += self.__onPageViewCreate
         self._application.onPageViewDestroy += self.__onPageViewDestroy
+        self._application.onPageDialogPageTypeChanged += self.__onPageDialogPageTypeChanged
+        self._application.onPageDialogDestroy += self.__onPageDialogDestroy
 
     def clear (self):
         """
@@ -36,6 +44,8 @@ class Controller (object):
         self._application.onPageDialogPageFactoriesNeeded -= self.__onPageDialogPageFactoriesNeeded
         self._application.onPageViewCreate -= self.__onPageViewCreate
         self._application.onPageViewDestroy -= self.__onPageViewDestroy
+        self._application.onPageDialogPageTypeChanged -= self.__onPageDialogPageTypeChanged
+        self._application.onPageDialogDestroy -= self.__onPageDialogDestroy
 
     def __onPageDialogPageFactoriesNeeded (self, page, params):
         params.addPageFactory (MarkdownPageFactory())
@@ -49,3 +59,28 @@ class Controller (object):
         assert page is not None
         if page.getTypeString() == MarkdownPage.getTypeString():
             self._colorizerController.clear()
+
+    def __onPageDialogPageTypeChanged(self, page, params):
+        if params.pageType == MarkdownPage.getTypeString():
+            self._addTab(params.dialog)
+        elif self._appearancePanel is not None:
+            params.dialog.removeController(self._appearanceController)
+            params.dialog.removePanel(self._appearancePanel)
+            self._appearancePanel = None
+            self._appearanceController = None
+
+    def _addTab(self, dialog):
+        if self._appearancePanel is None:
+            self._appearancePanel = AppearancePanel(dialog.getPanelsParent())
+            dialog.addPanel(self._appearancePanel, _(u'Appearance'))
+
+            self._appearanceController = AppearanceController(
+                self._appearancePanel,
+                self._application,
+                dialog)
+
+            dialog.addController(self._appearanceController)
+
+    def __onPageDialogDestroy(self, page, params):
+        self._appearancePanel = None
+        self._appearanceController = None
