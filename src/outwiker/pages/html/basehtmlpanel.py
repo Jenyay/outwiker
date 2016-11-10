7b65d9ed9cbc9541e730f61569bbba922f9f1cbe
Eugeniy Ilin <jenyay.ilin@gmail.com>
2016-11-10 12:22:36 +0300

    Code cleaning

--- src/outwiker/pages/html/basehtmlpanel.py
+++ src/outwiker/pages/html/basehtmlpanel.py
@@ -7,7 +7,10 @@ import os
 import wx
 import wx.lib.newevent
 
-from outwiker.actions.search import SearchAction, SearchNextAction, SearchPrevAction, SearchAndReplaceAction
+from outwiker.actions.search import (SearchAction,
+                                     SearchNextAction,
+                                     SearchPrevAction,
+                                     SearchAndReplaceAction)
 from outwiker.actions.attachfiles import AttachFilesAction
 from outwiker.actions.globalsearch import GlobalSearchAction
 from outwiker.core.commands import MessageBox, setStatusText
@@ -20,7 +23,7 @@ from outwiker.utilites.textfile import readTextFile
 from outwiker.gui.basetextpanel import BaseTextPanel
 from outwiker.gui.guiconfig import GeneralGuiConfig
 
-# Событие вызывается, когда переключаются вкладки страницы (код, HTML, ...)
+# Событие вызывается, когда переключаются вкладки страницы(код, HTML, ...)
 PageTabChangedEvent, EVT_PAGE_TAB_CHANGED = wx.lib.newevent.NewEvent()
 
 
@@ -31,9 +34,8 @@ class BaseHtmlPanel(BaseTextPanel):
     CODE_PAGE_INDEX = 0
     RESULT_PAGE_INDEX = 1
 
-
     def __init__(self, parent, application):
-        super (BaseHtmlPanel, self).__init__ (parent, application)
+        super(BaseHtmlPanel, self).__init__(parent, application)
 
         # Предыдущее содержимое результирующего HTML, чтобы не переписывать
         # его каждый раз
@@ -42,7 +44,7 @@ class BaseHtmlPanel(BaseTextPanel):
         # Страница, для которой уже есть сгенерированный HTML
         self._oldPage = None
 
-        # Где хранить параметы текущей страницы страницы (код, просмотр и т.д.)
+        # Где хранить параметы текущей страницы страницы(код, просмотр и т.д.)
         self.tabSectionName = u"Misc"
         self.tabParamName = u"PageIndex"
 
@@ -53,30 +55,33 @@ class BaseHtmlPanel(BaseTextPanel):
         self.notebook = wx.Notebook(self, -1, style=wx.NB_BOTTOM)
         self._codeEditor = self.getTextEditor()(self.notebook)
 
-        self.htmlWindow = getOS().getHtmlRender (self.notebook)
+        self.htmlWindow = getOS().getHtmlRender(self.notebook)
 
         self.__do_layout()
 
-        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self._onTabChanged, self.notebook)
-        self.Bind (self.EVT_SPELL_ON_OFF, handler=self._onSpellOnOff)
+        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED,
+                  self._onTabChanged,
+                  self.notebook)
+        self.Bind(self.EVT_SPELL_ON_OFF, handler=self._onSpellOnOff)
         self._application.onPageUpdate += self._onPageUpdate
 
-    def Clear (self):
-        self.Unbind(wx.EVT_NOTEBOOK_PAGE_CHANGED, source=self.notebook, handler=self._onTabChanged)
-        self.Unbind (self.EVT_SPELL_ON_OFF, handler=self._onSpellOnOff)
+    def Clear(self):
+        self.Unbind(wx.EVT_NOTEBOOK_PAGE_CHANGED,
+                    source=self.notebook,
+                    handler=self._onTabChanged)
+        self.Unbind(self.EVT_SPELL_ON_OFF, handler=self._onSpellOnOff)
         self._application.onPageUpdate -= self._onPageUpdate
 
-        super (BaseHtmlPanel, self).Clear()
+        super(BaseHtmlPanel, self).Clear()
 
-
-    def SetCursorPosition (self, position):
+    def SetCursorPosition(self, position):
         """
         Установить курсор в текстовом редакторе в положение position
         """
         self.codeEditor.SetSelection(position, position)
         self.codeEditor.ScrollLineToCursor()
 
-    def GetCursorPosition (self):
+    def GetCursorPosition(self):
         """
         Возвращает положение курсора в текстовом редакторе
         """
@@ -86,20 +91,19 @@ class BaseHtmlPanel(BaseTextPanel):
         return self._codeEditor
 
     @property
-    def codeEditor (self):
+    def codeEditor(self):
         return self._codeEditor
 
-
-    def addTool (self,
-                 menu,
-                 idstring,
-                 func,
-                 menuText,
-                 buttonText,
-                 image,
-                 alwaysEnabled=False,
-                 fullUpdate=True,
-                 panelname="plugins"):
+    def addTool(self,
+                menu,
+                idstring,
+                func,
+                menuText,
+                buttonText,
+                image,
+                alwaysEnabled=False,
+                fullUpdate=True,
+                panelname="plugins"):
         """
         !!! Внимание. Это устаревший способ добавления элементов интерфейса.
         Сохраняется только для совместимости со старыми версиями плагинов
@@ -114,88 +118,77 @@ class BaseHtmlPanel(BaseTextPanel):
         image -- имя файла с картинкой
         alwaysEnabled -- Кнопка должна быть всегда активна
         """
-        super (BaseHtmlPanel, self).addTool (menu,
-                                             idstring,
-                                             func,
-                                             menuText,
-                                             buttonText,
-                                             image,
-                                             alwaysEnabled,
-                                             fullUpdate,
-                                             panelname)
+        super(BaseHtmlPanel, self).addTool(menu,
+                                           idstring,
+                                           func,
+                                           menuText,
+                                           buttonText,
+                                           image,
+                                           alwaysEnabled,
+                                           fullUpdate,
+                                           panelname)
 
         tool = self._tools[idstring]
-        self.enableTool (tool, self._isEnabledTool (tool))
+        self.enableTool(tool, self._isEnabledTool(tool))
 
-
-    def Print (self):
+    def Print(self):
         currpanel = self.notebook.GetCurrentPage()
         if currpanel is not None:
             currpanel.Print()
-
 
     @abstractmethod
     def getTextEditor(self):
         pass
 
-
     @property
-    def selectedPageIndex (self):
+    def selectedPageIndex(self):
         """
-        Возвращает номер выбранной страницы (код или просмотр)
+        Возвращает номер выбранной страницы(код или просмотр)
         """
         return self.notebook.GetSelection()
 
-
     @selectedPageIndex.setter
-    def selectedPageIndex (self, index):
+    def selectedPageIndex(self, index):
         """
-        Устанавливает выбранную страницу (код или просмотр)
+        Устанавливает выбранную страницу(код или просмотр)
         """
         if index >= 0 and index < self.pageCount:
-            self.notebook.SetSelection (index)
-
+            self.notebook.SetSelection(index)
 
     @property
-    def pageCount (self):
-        return self.notebook.GetPageCount ()
+    def pageCount(self):
+        return self.notebook.GetPageCount()
 
-
-    def addPage (self, parent, title):
+    def addPage(self, parent, title):
         self.notebook.AddPage(parent, title)
 
-
-    def onPreferencesDialogClose (self, prefDialog):
+    def onPreferencesDialogClose(self, prefDialog):
         self.codeEditor.setDefaultSettings()
 
-
-    def onAttachmentPaste (self, fnames):
-        text = self._getAttachString (fnames)
-        self.codeEditor.AddText (text)
+    def onAttachmentPaste(self, fnames):
+        text = self._getAttachString(fnames)
+        self.codeEditor.AddText(text)
         self.codeEditor.SetFocus()
 
-
-    def UpdateView (self, page):
+    def UpdateView(self, page):
         self.htmlWindow.page = self._currentpage
 
-        self.codeEditor.SetReadOnly (False)
-        self.codeEditor.SetText (self._currentpage.content)
+        self.codeEditor.SetReadOnly(False)
+        self.codeEditor.SetText(self._currentpage.content)
         self.codeEditor.EmptyUndoBuffer()
-        self.codeEditor.SetReadOnly (page.readonly)
+        self.codeEditor.SetReadOnly(page.readonly)
         self.SetCursorPosition(self._getCursorPositionOption(page).value)
 
         self._updateHtmlWindow()
-        tabIndex = self.loadPageTab (self._currentpage)
+        tabIndex = self.loadPageTab(self._currentpage)
         if tabIndex < 0:
             tabIndex = self._getDefaultPage()
 
         self.codeEditor.SetFocus()
         self.selectedPageIndex = tabIndex
 
-
     def GetContentFromGui(self):
         return self.codeEditor.GetText()
-
 
     def __do_layout(self):
         self.addPage(self.codeEditor, _("HTML"))
@@ -210,45 +203,41 @@ class BaseHtmlPanel(BaseTextPanel):
         self.Layout()
 
     @abstractmethod
-    def _enableActions (self, enabled):
+    def _enableActions(self, enabled):
         pass
-
 
     def _getDefaultPage(self):
         assert self._currentpage is not None
 
-        if (len (self._currentpage.content) > 0 or
-                len (Attachment (self._currentpage).attachmentFull) > 0):
+        if(len(self._currentpage.content) > 0 or
+                len(Attachment(self._currentpage).attachmentFull) > 0):
             return self.RESULT_PAGE_INDEX
 
         return self.CODE_PAGE_INDEX
 
-
-    def _onTabChanged (self, event):
-        newevent = PageTabChangedEvent (tab=self.selectedPageIndex)
+    def _onTabChanged(self, event):
+        newevent = PageTabChangedEvent(tab=self.selectedPageIndex)
         wx.PostEvent(self, newevent)
 
-
-    def savePageTab (self, page):
+    def savePageTab(self, page):
         """
-        Соханить текущую вкладку (код, просмотр и т.п.) в настройки страницы
+        Соханить текущую вкладку(код, просмотр и т.п.) в настройки страницы
         """
         assert page is not None
-        tabOption = IntegerOption (page.params,
-                                   self.tabSectionName,
-                                   self.tabParamName,
-                                   -1)
+        tabOption = IntegerOption(page.params,
+                                  self.tabSectionName,
+                                  self.tabParamName,
+                                  -1)
         tabOption.value = self.selectedPageIndex
 
-
-    def loadPageTab (self, page):
+    def loadPageTab(self, page):
         """
-        Прочитать из страницы настройки текущей вкладки (код, просмотр и т.п.)
+        Прочитать из страницы настройки текущей вкладки(код, просмотр и т.п.)
         """
         assert page is not None
 
         # Get global tab option
-        generalConfig = GeneralGuiConfig (self._application.config)
+        generalConfig = GeneralGuiConfig(self._application.config)
         generalTab = generalConfig.pageTab.value
 
         if generalTab == GeneralGuiConfig.PAGE_TAB_CODE:
@@ -257,58 +246,54 @@ class BaseHtmlPanel(BaseTextPanel):
             return self.RESULT_PAGE_INDEX
 
         # Get tab option from page
-        tabOption = IntegerOption (page.params,
-                                   self.tabSectionName,
-                                   self.tabParamName,
-                                   -1)
+        tabOption = IntegerOption(page.params,
+                                  self.tabSectionName,
+                                  self.tabParamName,
+                                  -1)
         return tabOption.value
 
-
-    def _onSwitchToCode (self):
+    def _onSwitchToCode(self):
         """
         Обработка события при переключении на код страницы
         """
-        self._enableActions (not self._application.selectedPage.readonly)
+        self._enableActions(not self._application.selectedPage.readonly)
         self.checkForExternalEditAndSave()
-        self._enableAllTools ()
+        self._enableAllTools()
         self.codeEditor.SetFocus()
 
-
-    def _onSwitchToPreview (self):
+    def _onSwitchToPreview(self):
         """
         Обработка события при переключении на просмотр страницы
         """
         self.Save()
-        self._enableActions (False)
-        self._enableAllTools ()
+        self._enableActions(False)
+        self._enableAllTools()
         self.htmlWindow.SetFocus()
         self.htmlWindow.Update()
         self._updatePage()
         self._updateHtmlWindow()
 
-
     def _updatePage(self):
         assert self._currentpage is not None
 
-        setStatusText (_(u"Page rendered. Please wait…"), self._statusbar_item)
+        setStatusText(_(u"Page rendered. Please wait…"), self._statusbar_item)
         self._application.onHtmlRenderingBegin(self._currentpage,
                                                self.htmlWindow)
 
         self._application.onPageUpdateNeeded(self._currentpage,
                                              PageUpdateNeededParams(True))
 
-        setStatusText (u"", self._statusbar_item)
-        self._application.onHtmlRenderingEnd (self._currentpage,
-                                              self.htmlWindow)
+        setStatusText(u"", self._statusbar_item)
+        self._application.onHtmlRenderingEnd(self._currentpage,
+                                             self.htmlWindow)
 
-
-    def _updateHtmlWindow (self):
+    def _updateHtmlWindow(self):
         """
         Подготовить и показать HTML текущей страницы
         """
         assert self._currentpage is not None
 
-        setStatusText (_(u"Page loading. Please wait…"), self._statusbar_item)
+        setStatusText(_(u"Page loading. Please wait…"), self._statusbar_item)
 
         try:
             path = self._currentpage.getHtmlPath()
@@ -317,27 +302,26 @@ class BaseHtmlPanel(BaseTextPanel):
 
             html = readTextFile(path)
 
-            if (self._oldPage != self._currentpage or
+            if(self._oldPage != self._currentpage or
                     self._oldHtmlResult != html):
-                self.htmlWindow.LoadPage (path)
+                self.htmlWindow.LoadPage(path)
                 self._oldHtmlResult = html
                 self._oldPage = self._currentpage
         except EnvironmentError as e:
             print(e)
-            MessageBox (_(u'Page loading error: {}').format(
+            MessageBox(_(u'Page loading error: {}').format(
                 self._currentpage.title),
                 _(u'Error'), wx.ICON_ERROR | wx.OK)
 
-        setStatusText (u"", self._statusbar_item)
+        setStatusText(u"", self._statusbar_item)
 
-
-    def _enableAllTools (self):
+    def _enableAllTools(self):
         """
-        Активировать или дезактивировать инструменты (пункты меню и кнопки)
+        Активировать или дезактивировать инструменты(пункты меню и кнопки)
         в зависимости от текущей выбранной вкладки
         """
         for tool in self.allTools:
-            self.enableTool (tool, self._isEnabledTool (tool))
+            self.enableTool(tool, self._isEnabledTool(tool))
 
         # Отдельно проверим возможность работы поиска по странице
         # Поиск не должен работать только на странице просмотра
@@ -345,24 +329,26 @@ class BaseHtmlPanel(BaseTextPanel):
 
         actionController = self._application.actionController
 
-        actionController.enableTools (SearchAction.stringId, searchEnabled)
-        actionController.enableTools (SearchNextAction.stringId, searchEnabled)
-        actionController.enableTools (SearchPrevAction.stringId, searchEnabled)
+        actionController.enableTools(SearchAction.stringId, searchEnabled)
+        actionController.enableTools(SearchNextAction.stringId, searchEnabled)
+        actionController.enableTools(SearchPrevAction.stringId, searchEnabled)
 
-        actionController.enableTools (SearchAndReplaceAction.stringId,
-                                      searchEnabled and not self._application.selectedPage.readonly)
+        actionController.enableTools(
+            SearchAndReplaceAction.stringId,
+            searchEnabled and not self._application.selectedPage.readonly)
 
-        actionController.enableTools (AttachFilesAction.stringId,
-                                      searchEnabled and not self._application.selectedPage.readonly)
+        actionController.enableTools(
+            AttachFilesAction.stringId,
+            searchEnabled and not self._application.selectedPage.readonly)
 
-        actionController.enableTools (GlobalSearchAction.stringId,
-                                      searchEnabled and not self._application.selectedPage.readonly)
+        actionController.enableTools(
+            GlobalSearchAction.stringId,
+            searchEnabled and not self._application.selectedPage.readonly)
 
         self.mainWindow.UpdateAuiManager()
 
-
-    def _isEnabledTool (self, tool):
-        if "notebook" not in dir (self):
+    def _isEnabledTool(self, tool):
+        if "notebook" not in dir(self):
             return True
 
         assert self.notebook is not None
@@ -373,15 +359,13 @@ class BaseHtmlPanel(BaseTextPanel):
 
         return enabled
 
-
-    def GetSearchPanel (self):
+    def GetSearchPanel(self):
         if self.selectedPageIndex == self.CODE_PAGE_INDEX:
             return self.codeEditor.searchPanel
 
         return None
 
-
-    def switchCodeResult (self):
+    def switchCodeResult(self):
         """
         Переключение между кодом и результатом
         """
@@ -393,26 +377,25 @@ class BaseHtmlPanel(BaseTextPanel):
         else:
             self.selectedPageIndex = self.CODE_PAGE_INDEX
 
-
-    def turnText (self, left, right):
+    def turnText(self, left, right):
         """
         Обернуть выделенный текст строками left и right.
         Метод предназначен в первую очередь для упрощения доступа к
         одноименному методу из codeEditor
         """
-        self._application.mainWindow.pagePanel.pageView.codeEditor.turnText(left, right)
+        self._application.mainWindow.pagePanel.pageView.codeEditor.turnText(
+            left, right)
 
-
-    def replaceText (self, text):
+    def replaceText(self, text):
         """
         Заменить выделенный текст строкой text
         Метод предназначен в первую очередь для упрощения доступа к
         одноименному методу из codeEditor
         """
-        self._application.mainWindow.pagePanel.pageView.codeEditor.replaceText(text)
+        self._application.mainWindow.pagePanel.pageView.codeEditor.replaceText(
+            text)
 
-
-    def escapeHtml (self):
+    def escapeHtml(self):
         """
         Заменить символы на их HTML-представление
         Метод предназначен в первую очередь для упрощения доступа к
@@ -420,8 +403,7 @@ class BaseHtmlPanel(BaseTextPanel):
         """
         self._application.mainWindow.pagePanel.pageView.codeEditor.escapeHtml()
 
-
-    def _onSpellOnOff (self, event):
+    def _onSpellOnOff(self, event):
         self._codeEditor.setDefaultSettings()
 
     def SetFocus(self):
@@ -430,7 +412,7 @@ class BaseHtmlPanel(BaseTextPanel):
         elif self.selectedPageIndex == self.RESULT_PAGE_INDEX:
             return self.htmlWindow.SetFocus()
 
-    def _onPageUpdate (self, sender, **kwargs):
+    def _onPageUpdate(self, sender, **kwargs):
         if (sender == self._currentpage and
                 self.notebook.GetSelection() == self.RESULT_PAGE_INDEX):
             self._updatePage()
