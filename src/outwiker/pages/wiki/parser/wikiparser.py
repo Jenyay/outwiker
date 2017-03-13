24115944487aa1eb9d54f59cbfb7c64b2e50f138
Jenyay <jenyay.ilin@gmail.com>
2017-03-13 10:12:48 +0300

    Added properties for head and footer items for wiki parser.

--- src/outwiker/pages/wiki/parser/wikiparser.py
+++ src/outwiker/pages/wiki/parser/wikiparser.py
@@ -27,52 +27,54 @@ from outwiker.libs.pyparsing import NoMatch
 from outwiker.core.system import getOS
 
 
-class Parser (object):
-    def __init__ (self, page, config):
+class Parser(object):
+    def __init__(self, page, config):
         self.page = page
         self.config = config
         self.error_template = u"<b>{error}</b>"
 
-        # Dictionary with nonstandard parameters (for plugins for example)
+        # Dictionary with nonstandard parameters(for plugins for example)
         self.customProps = {}
 
         # Массив строк, которые надо добавить в заголовок страницы
         self.__headers = []
 
+        self.__footers = []
+
         # Команды, которые обрабатывает парсер.
-        # Формат команд: (:name params... :) content... (:nameend:)
+        # Формат команд:(:name params... :) content...(:nameend:)
         # Ключ - имя команды, значение - экземпляр класса команды
         self.commands = {}
 
-        self.italicized = FontsFactory.makeItalic (self)
-        self.bolded = FontsFactory.makeBold (self)
-        self.boldItalicized = FontsFactory.makeBoldItalic (self)
-        self.underlined = FontsFactory.makeUnderline (self)
-        self.strike = FontsFactory.makeStrike (self)
-        self.subscript = FontsFactory.makeSubscript (self)
-        self.superscript = FontsFactory.makeSuperscript (self)
-        self.quote = QuoteFactory.make (self)
-        self.code = FontsFactory.makeCode (self)
-        self.mark = FontsFactory.makeMark (self)
+        self.italicized = FontsFactory.makeItalic(self)
+        self.bolded = FontsFactory.makeBold(self)
+        self.boldItalicized = FontsFactory.makeBoldItalic(self)
+        self.underlined = FontsFactory.makeUnderline(self)
+        self.strike = FontsFactory.makeStrike(self)
+        self.subscript = FontsFactory.makeSubscript(self)
+        self.superscript = FontsFactory.makeSuperscript(self)
+        self.quote = QuoteFactory.make(self)
+        self.code = FontsFactory.makeCode(self)
+        self.mark = FontsFactory.makeMark(self)
         self.small = FontsFactory.makeSmall(self)
         self.big = FontsFactory.makeBig(self)
         self.headings = HeadingFactory.make(self)
         self.thumb = ThumbnailFactory.make(self)
         self.noformat = NoFormatFactory.make(self)
-        self.preformat = PreFormatFactory.make (self)
+        self.preformat = PreFormatFactory.make(self)
         self.horline = HorLineFactory.make(self)
-        self.link = LinkFactory.make (self)
+        self.link = LinkFactory.make(self)
         self.align = AlignFactory.make(self)
         self.table = TableFactory.make(self)
-        self.url = UrlFactory.make (self)
-        self.urlImage = UrlImageFactory.make (self)
-        self.attaches = AttachFactory.make (self)
-        self.attachImages = AttachImagesFactory.make (self)
+        self.url = UrlFactory.make(self)
+        self.urlImage = UrlImageFactory.make(self)
+        self.attaches = AttachFactory.make(self)
+        self.attachImages = AttachImagesFactory.make(self)
         self.adhoctokens = AdHocFactory.make(self)
-        self.lists = ListFactory.make (self)
-        self.lineBreak = LineBreakFactory.make (self)
-        self.lineJoin = LineJoinFactory.make (self)
-        self.command = CommandFactory.make (self)
+        self.lists = ListFactory.make(self)
+        self.lineBreak = LineBreakFactory.make(self)
+        self.lineJoin = LineJoinFactory.make(self)
+        self.command = CommandFactory.make(self)
         self.text = TextFactory.make(self)
 
         # Common wiki tokens
@@ -184,7 +186,7 @@ class Parser (object):
             self.command,
         ]
 
-        # Tokens for using inside list items (bullets and numeric)
+        # Tokens for using inside list items(bullets and numeric)
         self.listItemsTokens = [
             self.attaches,
             self.urlImage,
@@ -218,100 +220,112 @@ class Parser (object):
         self._headingMarkup = None
         self._textLevelMarkup = None
 
-
-    def _createMarkup (self, tokensList):
+    def _createMarkup(self, tokensList):
         markup = NoMatch()
         for token in tokensList:
             markup |= token
 
         return markup
 
-
     @property
-    def head (self):
+    def head(self):
         """
         Свойство возвращает строку из добавленных заголовочных элементов
-        (то, что должно быть внутри тега <HEAD>...</HEAD>)
+       (то, что должно быть внутри тега <head>...</head>)
         """
-        return u"\n".join (self.__headers)
+        return u"\n".join(self.__headers)
 
+    @property
+    def footer(self):
+        '''
+        Added in outwiker.core 1.3
+        '''
+        return u'\n'.join(self.__footers)
 
-    def appendToHead (self, header):
+    def appendToHead(self, header):
         """
         Добавить строку в заголовок
         """
-        self.__headers.append (header)
+        self.__headers.append(header)
 
+    @property
+    def headItems(self):
+        '''
+        Return list of the strings for the <head> HTML tag.
 
-    def toHtml (self, text):
+        Added in outwiker.core 1.3
+        '''
+        return self.__headers
+
+    @property
+    def footerItems(self):
+        '''
+        Added in outwiker.core 1.3
+        '''
+        return self.__footers
+
+    def toHtml(self, text):
         """
         Сгенерить HTML без заголовков типа <HTML> и т.п.
         """
-        thumb = Thumbnails (self.page)
+        thumb = Thumbnails(self.page)
         thumb.clearDir()
 
         return self.parseWikiMarkup(text)
 
-
-    def parseWikiMarkup (self, text):
+    def parseWikiMarkup(self, text):
         if self._wikiMarkup is None:
-            self._wikiMarkup = self._createMarkup (self.wikiTokens)
+            self._wikiMarkup = self._createMarkup(self.wikiTokens)
 
         try:
-            return self._wikiMarkup.transformString (text)
+            return self._wikiMarkup.transformString(text)
         except Exception:
-            error = unicode (traceback.format_exc(), getOS().filesEncoding)
-            return self.error_template.format (error = error)
+            error = unicode(traceback.format_exc(), getOS().filesEncoding)
+            return self.error_template.format(error=error)
 
-
-    def parseListItemMarkup (self, text):
+    def parseListItemMarkup(self, text):
         if self._listItemMarkup is None:
-            self._listItemMarkup = self._createMarkup (self.listItemsTokens)
+            self._listItemMarkup = self._createMarkup(self.listItemsTokens)
 
         try:
-            return self._listItemMarkup.transformString (text)
+            return self._listItemMarkup.transformString(text)
         except Exception:
-            error = unicode (traceback.format_exc(), getOS().filesEncoding)
-            return self.error_template.format (error = error)
+            error = unicode(traceback.format_exc(), getOS().filesEncoding)
+            return self.error_template.format(error=error)
 
-
-    def parseLinkMarkup (self, text):
+    def parseLinkMarkup(self, text):
         if self._linkMarkup is None:
-            self._linkMarkup = self._createMarkup (self.linkTokens)
+            self._linkMarkup = self._createMarkup(self.linkTokens)
 
         try:
-            return self._linkMarkup.transformString (text)
+            return self._linkMarkup.transformString(text)
         except Exception:
-            error = unicode (traceback.format_exc(), getOS().filesEncoding)
-            return self.error_template.format (error = error)
+            error = unicode(traceback.format_exc(), getOS().filesEncoding)
+            return self.error_template.format(error=error)
 
-
-    def parseHeadingMarkup (self, text):
+    def parseHeadingMarkup(self, text):
         if self._headingMarkup is None:
-            self._headingMarkup = self._createMarkup (self.headingTokens)
+            self._headingMarkup = self._createMarkup(self.headingTokens)
 
         try:
-            return self._headingMarkup.transformString (text)
+            return self._headingMarkup.transformString(text)
         except Exception:
-            error = unicode (traceback.format_exc(), getOS().filesEncoding)
-            return self.error_template.format (error = error)
+            error = unicode(traceback.format_exc(), getOS().filesEncoding)
+            return self.error_template.format(error=error)
 
-
-    def parseTextLevelMarkup (self, text):
+    def parseTextLevelMarkup(self, text):
         if self._textLevelMarkup is None:
-            self._textLevelMarkup = self._createMarkup (self.textLevelTokens)
+            self._textLevelMarkup = self._createMarkup(self.textLevelTokens)
 
         try:
-            return self._textLevelMarkup.transformString (text)
+            return self._textLevelMarkup.transformString(text)
         except Exception:
-            error = unicode (traceback.format_exc(), getOS().filesEncoding)
-            return self.error_template.format (error = error)
+            error = unicode(traceback.format_exc(), getOS().filesEncoding)
+            return self.error_template.format(error=error)
 
-
-    def addCommand (self, command):
+    def addCommand(self, command):
         self.commands[command.name] = command
 
-
-    def removeCommand (self, commandName):
+    def removeCommand(self, commandName):
         if commandName in self.commands:
             del self.commands[commandName]
