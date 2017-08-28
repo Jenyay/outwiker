da047e4097d85268d14d02caa3142dcd406cd181
Eugeniy Ilin <jenyay.ilin@gmail.com>
2017-08-28 14:35:55 +0300

    UpdateNotifier. Fixed errors in Windows.

--- plugins/updatenotifier/updatenotifier/loaders.py
+++ plugins/updatenotifier/updatenotifier/loaders.py
@@ -1,33 +1,33 @@
 # -*- coding: UTF-8 -*-
 """
-Модуль с классами, предназначенными для загрузки страницы из интернета или эмуляции этого процесса
+Модуль с классами, предназначенными для загрузки страницы из интернета
+или эмуляции этого процесса
 """
 
 import urllib2
 
 
-class NormalLoader (object):
+class NormalLoader(object):
     """
-    Класс для честной загрузки страницы из интернета
+    Класс для загрузки страницы из интернета
     """
-    def load (self, url):
+    def load(self, url):
         """
         Метод может бросать исключения urllib2.HTTPError и urllib2.URLError
         """
-        fp = urllib2.urlopen (url)
+        fp = urllib2.urlopen(url)
 
         try:
-            text = fp.read()
+            text = unicode(fp.read(), 'utf8')
         finally:
             fp.close()
 
         return text
 
 
-
-class DisconnectedLoader (object):
+class DisconnectedLoader(object):
     """
     Класс загрузчика, эмулирующий отсутствие интернета
     """
-    def load (self, url):
+    def load(self, url):
         raise urllib2.URLError("Disconnected")
