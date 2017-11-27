de1651951b37acca2a2f08427f0e217f0391518e
Jenyay <jenyay.ilin@gmail.com>
2017-11-27 21:04:27 +0300

    Now directories inside __thumb directory will be removed before wiki rendering.

--- src/outwiker/pages/wiki/thumbnails.py
+++ src/outwiker/pages/wiki/thumbnails.py
@@ -2,6 +2,7 @@
 
 import os
 import os.path
+import shutil
 
 from outwiker.core.attachment import Attachment
 from outwiker.core.defines import PAGE_ATTACH_DIR
@@ -13,39 +14,39 @@ class Thumbnails (object):
     """
     thumbDir = u"__thumb"
 
-    def __init__ (self, page):
+    def __init__(self, page):
         self.page = page
 
-
     @staticmethod
-    def getRelativeThumbDir ():
-        return os.path.join (PAGE_ATTACH_DIR, Thumbnails.thumbDir)
+    def getRelativeThumbDir():
+        return os.path.join(PAGE_ATTACH_DIR, Thumbnails.thumbDir)
 
-
-    def getThumbPath (self, create):
+    def getThumbPath(self, create):
         """
         Получить полный путь до папки с превьюшками
         create - нужно ли создавать директорию, если ее еще нет
         """
-        path = os.path.join (Attachment (self.page).getAttachPath(create=create), Thumbnails.thumbDir)
+        path = os.path.join(Attachment(self.page).getAttachPath(create=create),
+                            Thumbnails.thumbDir)
 
-        if create and not os.path.exists (path):
-            os.mkdir (path)
+        if create and not os.path.exists(path):
+            os.mkdir(path)
 
         return path
 
-
-    def clearDir (self):
+    def clearDir(self):
         """
-        Удалить все содержимое папки __thumb (вложенные директории пока не трогаем)
+        Удалить все содержимое папки __thumb
         """
-        path = self.getThumbPath (create=False)
+        path = self.getThumbPath(create=False)
 
-        if not os.path.exists (path):
+        if not os.path.exists(path):
             return
 
-        for fname in os.listdir (path):
-            fullpath = os.path.join (path, fname)
+        for fname in os.listdir(path):
+            fullpath = os.path.join(path, fname)
 
-            if os.path.isfile (fullpath):
-                os.remove (fullpath)
+            if os.path.isfile(fullpath):
+                os.remove(fullpath)
+            elif os.path.isdir(fullpath):
+                shutil.rmtree(fullpath)
