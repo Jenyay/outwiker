687b4a43da8e2ddcf32186232f442ecbf44875a9
Eugeniy Ilin <jenyay.ilin@gmail.com>
2017-12-11 16:06:17 +0300

    Format the code

--- src/outwiker/gui/pagelist.py
+++ src/outwiker/gui/pagelist.py
@@ -10,55 +10,54 @@ from outwiker.gui.controls.hyperlink import HyperLinkCtrl, EVT_HYPERLINK_LEFT
 PageClickEvent, EVT_PAGE_CLICK = wx.lib.newevent.NewEvent()
 
 
-class PageList (wx.ScrolledWindow):
-    def __init__ (self, parent):
-        super (PageList, self).__init__ (
+class PageList(wx.ScrolledWindow):
+    def __init__(self, parent):
+        super(PageList, self).__init__(
             parent,
             style=wx.BORDER_SIMPLE | wx.HSCROLL | wx.VSCROLL
         )
 
         self.__space = 4
 
-        self.SetBackgroundColour (wx.Colour (255, 255, 255))
+        self.SetBackgroundColour(wx.Colour(255, 255, 255))
 
         # Список отображаемых элементов
         self.__titleItems = []
-        self.__sizer = wx.FlexGridSizer (cols=1)
-        self.__sizer.AddGrowableCol (0)
+        self.__sizer = wx.FlexGridSizer(cols=1)
+        self.__sizer.AddGrowableCol(0)
 
-        self.SetSizer (self.__sizer)
-        self.SetAutoLayout (True)
+        self.SetSizer(self.__sizer)
+        self.SetAutoLayout(True)
 
-        self.Bind (wx.EVT_SIZE, self.__onSize)
+        self.Bind(wx.EVT_SIZE, self.__onSize)
 
-
-    def __onSize (self, event):
+    def __onSize(self, event):
         self.__updateScroll()
 
+    def __updateScroll(self):
+        self.SetScrollbars(0, 0, 0, 0)
 
-    def __updateScroll (self):
-        self.SetScrollbars (0, 0, 0, 0)
-
-        count = len (self.__titleItems)
+        count = len(self.__titleItems)
         if count > 1:
             lastItemY = self.__titleItems[-1].GetPosition()[1]
-            lastItemH = self.__titleItems[-1].GetPosition()[1] - self.__titleItems[-2].GetPosition()[1]
+            lastItemH = (self.__titleItems[-1].GetPosition()[1] -
+                         self.__titleItems[-2].GetPosition()[1])
 
             pixelPerUnitX = 5
-            unitX = (self.__getItemMaxWidth() + 2 * self.__space) / pixelPerUnitX + 1
+            unitX = ((self.__getItemMaxWidth() + 2 * self.__space) /
+                     pixelPerUnitX + 1)
 
-            pixelPerUnitY = (lastItemY + lastItemH) / len (self.__titleItems)
-            unitY = len (self.__titleItems) + 1
+            pixelPerUnitY = (lastItemY + lastItemH) / len(self.__titleItems)
+            unitY = len(self.__titleItems) + 1
 
-            self.SetScrollbars (pixelPerUnitX,
-                                pixelPerUnitY,
-                                unitX,
-                                unitY,
-                                0,
-                                0)
+            self.SetScrollbars(pixelPerUnitX,
+                               pixelPerUnitY,
+                               unitX,
+                               unitY,
+                               0,
+                               0)
 
-
-    def __getItemMaxWidth (self):
+    def __getItemMaxWidth(self):
         maxwidth = 0
         for item in self.__titleItems:
             width = item.GetBestSize()[0]
@@ -67,86 +66,83 @@ class PageList (wx.ScrolledWindow):
 
         return maxwidth
 
-
-
-    def clear (self):
+    def clear(self):
         """
         Удалить все элементы из списка
         """
         for item in self.__titleItems:
-            self.__sizer.Detach (item)
+            self.__sizer.Detach(item)
             item.Destroy()
 
         self.__titleItems = []
         self.Layout()
         self.__updateScroll()
 
-
-    def setPageList (self, pages):
+    def setPageList(self, pages):
         """
         pages - список страниц, отображаемый в списке
         """
         self.clear()
 
         for page in pages:
-            item = PageTitleItem (self, page)
+            item = PageTitleItem(self, page)
 
-            self.__titleItems.append (item)
-            self.__sizer.Add (item, 0, flag=wx.EXPAND | wx.ALL, border=self.__space)
+            self.__titleItems.append(item)
+            self.__sizer.Add(
+                item,
+                0,
+                flag=wx.EXPAND | wx.ALL, border=self.__space)
 
         self.Layout()
         self.__updateScroll()
 
 
-
-class PageTitleItem (wx.Panel):
-    def __init__ (self, parent, page):
-        super (PageTitleItem, self).__init__ (parent)
+class PageTitleItem(wx.Panel):
+    def __init__(self, parent, page):
+        super(PageTitleItem, self).__init__(parent)
         self.__page = page
 
-        self.__color = wx.Colour (0, 0, 255)
+        self.__color = wx.Colour(0, 0, 255)
         self.__fontSize = 10
         self.__propagationLevel = 15
-        self.__backColor = wx.Colour (255, 255, 255)
+        self.__backColor = wx.Colour(255, 255, 255)
 
-        self.SetBackgroundColour (self.__backColor)
+        self.SetBackgroundColour(self.__backColor)
 
         url = "/" + page.subpath
-        self.__label = HyperLinkCtrl (self,
-                                      label = page.title,
-                                      URL=url,
-                                      style=wx.adv.HL_ALIGN_CENTRE | wx.NO_BORDER)
+        self.__label = HyperLinkCtrl(
+            self,
+            label=page.title,
+            URL=url,
+            style=wx.adv.HL_ALIGN_CENTRE | wx.NO_BORDER)
 
-        self.__formatLabel (self.__label)
-        self.__label.Bind (EVT_HYPERLINK_LEFT, self.__pageClicked)
+        self.__formatLabel(self.__label)
+        self.__label.Bind(EVT_HYPERLINK_LEFT, self.__pageClicked)
         self.Fit()
 
-
-    def __pageClicked (self, event):
-        event = PageClickEvent (page=self.__page)
-        event.ResumePropagation (self.__propagationLevel)
+    def __pageClicked(self, event):
+        event = PageClickEvent(page=self.__page)
+        event.ResumePropagation(self.__propagationLevel)
         wx.PostEvent(self.GetParent(), event)
 
-
     @property
-    def page (self):
+    def page(self):
         return self.__page
 
+    def __formatLabel(self, label):
+        font = wx.Font(self.__fontSize,
+                       wx.FONTFAMILY_DEFAULT,
+                       wx.FONTSTYLE_NORMAL,
+                       wx.FONTWEIGHT_NORMAL,
+                       underline=False)
 
-    def __formatLabel (self, label):
-        font = wx.Font (self.__fontSize,
-                        wx.FONTFAMILY_DEFAULT,
-                        wx.FONTSTYLE_NORMAL,
-                        wx.FONTWEIGHT_NORMAL,
-                        underline=False)
-
-        label.SetFont (font)
-        label.SetToolTip (self.__page.subpath.replace ("/", " / "))
-        label.SetBackgroundColour (self.__backColor)
-        label.SetColours (self.__color, self.__color, self.__color)
-        label.AutoBrowse (False)
-        label.DoPopup (False)
+        label.SetFont(font)
+        label.SetToolTip(self.__page.subpath.replace("/", " / "))
+        label.SetBackgroundColour(self.__backColor)
+        label.SetColours(self.__color, self.__color, self.__color)
+        label.AutoBrowse(False)
+        label.DoPopup(False)
         label.ReportErrors(False)
-        label.EnableRollover (True)
-        label.SetUnderlines (False, False, False)
-        label.SetSize (label.GetBestSize())
+        label.EnableRollover(True)
+        label.SetUnderlines(False, False, False)
+        label.SetSize(label.GetBestSize())
--- src/outwiker/gui/pagelistpopup.py
+++ src/outwiker/gui/pagelistpopup.py
@@ -5,38 +5,35 @@ import wx
 from .pagelist import PageList
 
 
-class PageListPopup (wx.PopupTransientWindow):
-    def __init__ (self, parent):
+class PageListPopup(wx.PopupTransientWindow):
+    def __init__(self, parent):
         super(PageListPopup, self).__init__(parent)
 
         self.SetWindowStyle(wx.BORDER_SUNKEN)
-        self.__pagelist = PageList (self)
-        self.__layout ()
+        self.__pagelist = PageList(self)
+        self.__layout()
 
-
-    def __layout (self):
-        sizer = wx.FlexGridSizer (cols=1)
-        sizer.AddGrowableCol (0)
-        sizer.AddGrowableRow (0)
-        sizer.Add (self.__pagelist, 0, flag=wx.EXPAND)
-        self.SetSizer (sizer)
+    def __layout(self):
+        sizer = wx.FlexGridSizer(cols=1)
+        sizer.AddGrowableCol(0)
+        sizer.AddGrowableRow(0)
+        sizer.Add(self.__pagelist, 0, flag=wx.EXPAND)
+        self.SetSizer(sizer)
         self.Layout()
 
+    def setPageList(self, pagelist):
+        self.__pagelist.setPageList(pagelist)
 
-    def setPageList (self, pagelist):
-        self.__pagelist.setPageList (pagelist)
-
-
-    def Popup (self):
+    def Popup(self):
         self.Dismiss()
         self.Layout()
-        self.SetPosition (self.__getBestPosition())
-        super (PageListPopup, self).Popup ()
+        self.SetPosition(self.__getBestPosition())
+        super(PageListPopup, self).Popup()
 
-
-    def __getBestPosition (self):
+    def __getBestPosition(self):
         """
-        Рассчитывает координаты окна таким образом, чтобы оно было около курсора, но не вылезало за пределы окна
+        Рассчитывает координаты окна таким образом, чтобы оно было около
+        курсора, но не вылезало за пределы окна
         """
         mousePosition = wx.GetMousePosition()
         displaySize = wx.GetDisplaySize()
