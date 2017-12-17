0658b09cfd4ba7102f6332cda875ce75545ef211
Vladimir <vladimir.tuboltsev@gmail.com>
2017-12-17 19:57:41 +0300

    spoiler 2.0.1: Py3 migration

--- plugins/spoiler/spoiler/commandspoiler.py
+++ plugins/spoiler/spoiler/commandspoiler.py
@@ -35,10 +35,10 @@ class SpoilerCommand (Command):
     display: block;
     }</STYLE>"""
 
-        self.__blockTemplate = ur"""
+        self.__blockTemplate = r"""
         <div class="spoiler_style1"><div class="spoiler_style2"><span onClick="if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') {  this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = ''; this.innerHTML = '<b></b><a href=\'#\' onClick=\'return false;\'>{collapsetext}</a>'; } else { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none'; this.innerHTML = '<b></b><a href=\'#\' onClick=\'return false;\'>{expandtext}</a>'; }" /><b></b><a href="#" onClick="return false;">{expandtext}</a></span></div><div class="spoiler_quotecontent"><div style="display: none;">{content}</div></div></div>"""
 
-        self.__inlineTemplate = ur"""<span><span onClick="this.parentNode.getElementsByTagName('span')[1].style.display = ''; this.style.display = 'none';"><a href="#">{expandtext}</a></span><span style="display: none;" onClick="this.parentNode.getElementsByTagName('span')[0].style.display = ''; this.style.display = 'none';">{content}</span></span>"""
+        self.__inlineTemplate = r"""<span><span onClick="this.parentNode.getElementsByTagName('span')[1].style.display = ''; this.style.display = 'none';"><a href="#">{expandtext}</a></span><span style="display: none;" onClick="this.parentNode.getElementsByTagName('span')[0].style.display = ''; this.style.display = 'none';">{content}</span></span>"""
     
         self.__expandParam = u"expandtext"
         self.__collapseParam = u"collapsetext"
@@ -82,7 +82,7 @@ class SpoilerCommand (Command):
         """
         Возвращает шаблон в зависимости от вида спойлера
         """
-        if self.__inlineParam in params_dict.keys():
+        if self.__inlineParam in list(params_dict.keys()):
             return self.__inlineTemplate
         else:
             return self.__blockTemplate
