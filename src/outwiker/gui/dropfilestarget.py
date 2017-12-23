354016ee638feeba4d43632739ce328ecf06d437
Jenyay <jenyay.ilin@gmail.com>
2017-12-23 18:08:15 +0300

    Improved Drag'n'Drop files to the OutWiker window.

--- src/outwiker/gui/dropfilestarget.py
+++ src/outwiker/gui/dropfilestarget.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from abc import ABCMeta, abstractmethod
-
 import wx
 
 from outwiker.core.commands import attachFiles
 
 
-class DropFilesTargetBase (wx.FileDropTarget):
+class DropFilesTarget(wx.FileDropTarget):
     """
     Класс для возможности перетаскивания файлов
     между другими программами и OutWiker
     """
-    #__metaclass__ = ABCMeta
-
     def __init__(self, application, dropWnd):
         wx.FileDropTarget.__init__(self)
         self._application = application
@@ -24,16 +20,25 @@ class DropFilesTargetBase (wx.FileDropTarget):
         self._dropWnd.SetDropTarget(None)
         self._dropWnd = None
 
-    #@abstractmethod
     def OnDropFiles(self, x, y, files):
-        pass
+        if len(files) == 1 and '\n' in files[0]:
+            files = files[0].split('\n')
 
+        file_protocol = 'file://'
 
-class DropFilesTarget (DropFilesTargetBase):
-    def OnDropFiles(self, x, y, files):
+        correctedFiles = []
+        for fname in files:
+            if not fname.strip():
+                continue
+
+            if fname.startswith(file_protocol):
+                fname = fname[len(file_protocol):]
+
+            correctedFiles.append(fname)
+
         if (self._application.wikiroot is not None and
                 self._application.wikiroot.selectedPage is not None):
-            attachFiles(self._dropWnd,
+            attachFiles(self._application.mainWindow,
                         self._application.wikiroot.selectedPage,
-                        files)
+                        correctedFiles)
             return True
