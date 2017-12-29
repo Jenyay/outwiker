e6f617690a38e006864676bd9d35f27ea620fcd6
Jenyay <jenyay.ilin@gmail.com>
2017-12-29 15:44:42 +0300

    Set GTK theme for version for Linux.

--- src/linux_runtime_hook.py
+++ src/linux_runtime_hook.py
@@ -5,7 +5,7 @@ import sys
 
 os.environ['GDK_PIXBUF_MODULE_FILE'] = sys._MEIPASS + '/lib/gdk-pixbuf/loaders.cache'
 os.environ['LD_LIBRARY_PATH'] = sys._MEIPASS
-# os.environ['GTK_THEME'] = 'Adwaita'
+os.environ['GTK_THEME'] = 'Radiance'
 
 # print u'LD_LIBRARY_PATH = {}'.format(os.environ['LD_LIBRARY_PATH'])
 # print u'GDK_PIXBUF_MODULE_FILE = {}'.format(os.environ['GDK_PIXBUF_MODULE_FILE'])
