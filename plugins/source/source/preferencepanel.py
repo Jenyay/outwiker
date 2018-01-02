3eeada2dbaf98bccbb03def15069d4f6049560fe
Jenyay <jenyay.ilin@gmail.com>
2018-01-02 21:09:51 +0300

    Migrate the preferences panels to outwiker.gui.preferences.baseprefpanel.BasePrefPanel

--- plugins/source/source/preferencepanel.py
+++ plugins/source/source/preferencepanel.py
@@ -1,263 +1,258 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 import wx
 
 from outwiker.gui.preferences.configelements import IntegerElement
+from outwiker.gui.preferences.baseprefpanel import BasePrefPanel
 
 from .sourceconfig import SourceConfig
 from .i18n import get_
 from .misc import fillStyleComboBox
 
 
-class PreferencePanel (wx.Panel):
+class PreferencePanel(BasePrefPanel):
     """
     Панель с настройками
     """
-    def __init__ (self, parent, config):
+    def __init__(self, parent, config):
         """
-        parent - родитель панели (должен быть wx.Treebook)
+        parent - родитель панели(должен быть wx.Treebook)
         config - настройки из plugin._application.config
         """
-        wx.Panel.__init__ (self, parent, style=wx.TAB_TRAVERSAL)
+        super(PreferencePanel, self).__init__(parent)
 
         global _
         _ = get_()
 
         self.__createGui()
-        self.__controller = PrefPanelController (self, config)
-
+        self.__controller = PrefPanelController(self, config)
+        self.SetupScrolling()
 
     def __createGui(self):
         """
         Создать элементы управления
         """
-        mainSizer = wx.FlexGridSizer (0, 1, 0, 0)
+        mainSizer = wx.FlexGridSizer(0, 1, 0, 0)
         mainSizer.AddGrowableCol(0)
         mainSizer.AddGrowableRow(3)
 
-        self.__createTabWidthGui (mainSizer)
-        self.__createDefaultStyleGui (mainSizer)
-        self.__createLangGui (mainSizer)
+        self.__createTabWidthGui(mainSizer)
+        self.__createDefaultStyleGui(mainSizer)
+        self.__createLangGui(mainSizer)
         self.SetSizer(mainSizer)
 
-
-    def __createTabWidthGui (self, mainSizer):
+    def __createTabWidthGui(self, mainSizer):
         """
-        Создать элементы управления, связанные с выбором размера табуляции по умолчанию
+        Создать элементы управления, связанные
+            с выбором размера табуляции по умолчанию
         """
-        tabSizer = wx.FlexGridSizer (0, 2, 0, 0)
-        tabSizer.AddGrowableCol (1)
+        tabSizer = wx.FlexGridSizer(0, 2, 0, 0)
+        tabSizer.AddGrowableCol(1)
 
         tabWidthLabel = wx.StaticText(self, -1, _(u"Default Tab Width"))
 
-        self.tabWidthSpin = wx.SpinCtrl (
+        self.tabWidthSpin = wx.SpinCtrl(
             self,
             style=wx.SP_ARROW_KEYS | wx.TE_AUTO_URL
         )
-        self.tabWidthSpin.SetMinSize (wx.Size (150, -1))
+        self.tabWidthSpin.SetMinSize(wx.Size(150, -1))
 
-        tabSizer.Add (
+        tabSizer.Add(
             tabWidthLabel,
             proportion=1,
             flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL,
             border=2
         )
 
-        tabSizer.Add (
+        tabSizer.Add(
             self.tabWidthSpin,
             proportion=1,
             flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT,
             border=2
         )
 
-        mainSizer.Add (
+        mainSizer.Add(
             tabSizer,
             proportion=1,
             flag=wx.ALL | wx.EXPAND,
             border=2)
 
-
-    def __createDefaultStyleGui (self, mainSizer):
+    def __createDefaultStyleGui(self, mainSizer):
         """
         Создать элементы управления, связанные с выбором стиля по умолчанию
         """
-        styleSizer = wx.FlexGridSizer (0, 2, 0, 0)
-        styleSizer.AddGrowableCol (1)
+        styleSizer = wx.FlexGridSizer(0, 2, 0, 0)
+        styleSizer.AddGrowableCol(1)
 
         styleLabel = wx.StaticText(self, -1, _(u"Default Style"))
 
-        self.styleComboBox = wx.ComboBox (self, -1, style=wx.CB_DROPDOWN | wx.CB_READONLY)
-        self.styleComboBox.SetMinSize ((150, -1))
+        self.styleComboBox = wx.ComboBox(self,
+                                         -1,
+                                         style=wx.CB_DROPDOWN | wx.CB_READONLY)
+        self.styleComboBox.SetMinSize((150, -1))
 
-        styleSizer.Add (
+        styleSizer.Add(
             styleLabel,
             proportion=1,
             flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL,
             border=2
         )
 
-        styleSizer.Add (
+        styleSizer.Add(
             self.styleComboBox,
             proportion=1,
             flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT,
             border=2
         )
 
-        mainSizer.Add (
+        mainSizer.Add(
             styleSizer,
             proportion=1,
             flag=wx.ALL | wx.EXPAND,
             border=2)
 
-
-
-    def __createLangGui (self, mainSizer):
+    def __createLangGui(self, mainSizer):
         """
         Создание элементов управления, связанных с выбором используемых языков
         """
         # Метка с комментарием о том, что это за языки в списке
         languageLabel = wx.StaticText(self, -1, _(u"Used Languages"))
-        mainSizer.Add (
+        mainSizer.Add(
             languageLabel,
             proportion=1,
             flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL,
             border=2)
 
         # Сайзер для расположения списка языков и кнопок
-        langSizer = wx.FlexGridSizer (0, 2, 0, 0)
-        langSizer.AddGrowableRow (0)
-        langSizer.AddGrowableCol (0)
+        langSizer = wx.FlexGridSizer(0, 2, 0, 0)
+        langSizer.AddGrowableRow(0)
+        langSizer.AddGrowableCol(0)
 
-        self.langList = wx.CheckListBox (self, -1)
-        langSizer.Add (
+        self.langList = wx.CheckListBox(self, -1)
+        langSizer.Add(
             self.langList,
             proportion=1,
-            flag = wx.ALL | wx.EXPAND,
+            flag=wx.ALL | wx.EXPAND,
             border=2)
 
         # Кнопки
-        buttonsSizer = wx.BoxSizer (wx.VERTICAL)
-        self.selectAllButton = wx.Button (self, label=_(u"Select All"))
-        self.clearButton = wx.Button (self, label=_(u"Clear"))
+        buttonsSizer = wx.BoxSizer(wx.VERTICAL)
+        self.selectAllButton = wx.Button(self, label=_(u"Select All"))
+        self.clearButton = wx.Button(self, label=_(u"Clear"))
 
-        buttonsSizer.Add (
+        buttonsSizer.Add(
             self.selectAllButton,
             proportion=1,
             flag=wx.ALL | wx.EXPAND,
             border=2)
 
-        buttonsSizer.Add (
+        buttonsSizer.Add(
             self.clearButton,
             proportion=1,
             flag=wx.ALL | wx.EXPAND,
             border=2)
 
-        langSizer.Add (
+        langSizer.Add(
             buttonsSizer,
             proportion=1,
             flag=wx.ALL,
             border=2)
 
-        mainSizer.Add (
+        mainSizer.Add(
             langSizer,
             proportion=1,
             flag=wx.ALL | wx.EXPAND,
             border=2)
 
-
     def LoadState(self):
         self.__controller.loadState()
 
-
-    def Save (self):
+    def Save(self):
         self.__controller.save()
 
 
-
-class PrefPanelController (object):
+class PrefPanelController(object):
     """
     Контроллер для панели настроек
     """
-    def __init__ (self, owner, config):
+    def __init__(self, owner, config):
         self.MIN_TAB_WIDTH = 1
         self.MAX_TAB_WIDTH = 50
 
         self.__owner = owner
-        self.__config = SourceConfig (config)
+        self.__config = SourceConfig(config)
 
-        self.__owner.selectAllButton.Bind (wx.EVT_BUTTON, self._onSelectAll)
-        self.__owner.clearButton.Bind (wx.EVT_BUTTON, self._onClear)
+        self.__owner.selectAllButton.Bind(wx.EVT_BUTTON, self._onSelectAll)
+        self.__owner.clearButton.Bind(wx.EVT_BUTTON, self._onClear)
 
-
-    def loadState (self):
-        self._tabWidthOption = IntegerElement (
+    def loadState(self):
+        self._tabWidthOption = IntegerElement(
             self.__config.tabWidth,
             self.__owner.tabWidthSpin,
             self.MIN_TAB_WIDTH,
             self.MAX_TAB_WIDTH
         )
 
-        fillStyleComboBox (self.__config,
-                           self.__owner.styleComboBox,
-                           self.__config.defaultStyle.value.strip())
+        fillStyleComboBox(self.__config,
+                          self.__owner.styleComboBox,
+                          self.__config.defaultStyle.value.strip())
 
-        allLanguages = self._getAllLanguages ()
+        allLanguages = self._getAllLanguages()
         self.__owner.langList.Clear()
-        self.__owner.langList.AppendItems (allLanguages)
+        self.__owner.langList.AppendItems(allLanguages)
 
         # Уберем языки, которых нет в списке
-        selectedLanguages = [item for item in self.__config.languageList.value if item in allLanguages]
+        selectedLanguages = [item
+                             for item
+                             in self.__config.languageList.value
+                             if item in allLanguages]
 
-        self.__owner.langList.SetCheckedStrings (selectedLanguages)
+        self.__owner.langList.SetCheckedStrings(selectedLanguages)
 
-
-    def save (self):
+    def save(self):
         self._tabWidthOption.save()
         self.__config.defaultStyle.value = self.__owner.styleComboBox.GetValue()
         self.__config.languageList.value = self.__owner.langList.GetCheckedStrings()
 
+    def _onSelectAll(self, event):
+        self.__owner.langList.SetChecked(range(self.__owner.langList.GetCount()))
 
-    def _onSelectAll (self, event):
-        self.__owner.langList.SetChecked (range (self.__owner.langList.GetCount()))
+    def _onClear(self, event):
+        self.__owner.langList.SetChecked([])
 
-
-    def _onClear (self, event):
-        self.__owner.langList.SetChecked ([])
-
-
-    def _getAllLanguages (self):
+    def _getAllLanguages(self):
         """
         Получить список всех языков, о которых знает pygments
         """
         from .pygments.lexers._mapping import LEXERS
-        languages = [self._getLongestName (lexer[2]).lower() for lexer in LEXERS.values()]
+        languages = [self._getLongestName(lexer[2]).lower()
+                     for lexer in LEXERS.values()]
 
         # Сделаем некоторые замены
-        languages = self._replaceLangItem (languages, "php3", "php")
+        languages = self._replaceLangItem(languages, "php3", "php")
 
         languages.sort()
         return languages
 
-
-    def _replaceLangItem (self, items, oldname, newname):
+    def _replaceLangItem(self, items, oldname, newname):
         """
         Заменить элемент в списке
         Возвращает новый список
         """
         return [newname if item == oldname else item for item in items]
 
-
     @staticmethod
-    def _getLongestName (namelist):
+    def _getLongestName(namelist):
         """
-        Возвращает самое длинное название языка из списка имен одного и того же языка
+        Возвращает самое длинное название языка из списка имен одного
+            и того же языка
         """
         maxlen = 0
         bestname = u""
 
         for name in namelist:
-            if len (name) > maxlen:
-                maxlen = len (name)
+            if len(name) > maxlen:
+                maxlen = len(name)
                 bestname = name
 
         return bestname
--- plugins/updatenotifier/updatenotifier/preferencepanel.py
+++ plugins/updatenotifier/updatenotifier/preferencepanel.py
@@ -1,70 +1,77 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 import wx
+
+from outwiker.gui.preferences.baseprefpanel import BasePrefPanel
 
 from .i18n import get_
 from .prefpanelcontroller import PrefPanelController
 
 
-class PreferencePanel (wx.Panel):
+class PreferencePanel(BasePrefPanel):
     """
     Панель с настройками
     """
-    def __init__ (self, parent, config):
+    def __init__(self, parent, config):
         """
-        parent - родитель панели (должен быть wx.Treebook)
+        parent - родитель панели(должен быть wx.Treebook)
         config - настройки из plugin._application.config
         """
-        wx.Panel.__init__ (self, parent, style=wx.TAB_TRAVERSAL)
+        super(PreferencePanel, self).__init__(parent)
 
         global _
         _ = get_()
 
         self.__createGui()
-        self.__controller = PrefPanelController (self, config)
-
+        self.__controller = PrefPanelController(self, config)
+        self.SetupScrolling()
 
     def __createGui(self):
         """
         Создать элементы управления
         """
-        mainSizer = wx.FlexGridSizer (cols=1)
-        mainSizer.AddGrowableCol (0)
+        mainSizer = wx.FlexGridSizer(cols=1)
+        mainSizer.AddGrowableCol(0)
 
         # Интервал обновлений
-        intervalLabel = wx.StaticText (self, -1, _(u"Check for updates"))
-        self.intervalComboBox = wx.ComboBox (self, -1, style=wx.CB_DROPDOWN | wx.CB_READONLY)
-        self.intervalComboBox.SetMinSize ((150, -1))
+        intervalLabel = wx.StaticText(self, -1, _(u"Check for updates"))
+        self.intervalComboBox = wx.ComboBox(
+            self,
+            -1,
+            style=wx.CB_DROPDOWN | wx.CB_READONLY)
 
-        intervalSizer = wx.FlexGridSizer (cols=2)
-        intervalSizer.AddGrowableCol (0)
-        intervalSizer.AddGrowableCol (1)
+        self.intervalComboBox.SetMinSize((150, -1))
 
-        intervalSizer.Add (intervalLabel,
-                           1,
-                           flag=wx.ALIGN_CENTER_VERTICAL | wx.ALL,
-                           border=4)
+        intervalSizer = wx.FlexGridSizer(cols=2)
+        intervalSizer.AddGrowableCol(0)
+        intervalSizer.AddGrowableCol(1)
 
-        intervalSizer.Add (self.intervalComboBox,
-                           1,
-                           flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.ALL,
-                           border=4)
+        intervalSizer.Add(intervalLabel,
+                          1,
+                          flag=wx.ALIGN_CENTER_VERTICAL | wx.ALL,
+                          border=4)
 
-        mainSizer.Add (intervalSizer, 1, flag=wx.EXPAND | wx.ALL, border=4)
+        intervalSizer.Add(self.intervalComboBox,
+                          1,
+                          flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.ALL,
+                          border=4)
+
+        mainSizer.Add(intervalSizer, 1, flag=wx.EXPAND | wx.ALL, border=4)
 
         # Игнорировать нестибильные версии?
-        self.ignoreUnstableCheckBox = wx.CheckBox (self, label=_(u"Ignore updates unstable versions"))
-        mainSizer.Add (self.ignoreUnstableCheckBox,
-                       flag=wx.ALIGN_CENTER_VERTICAL | wx.ALL,
-                       border=4)
+        self.ignoreUnstableCheckBox = wx.CheckBox(
+            self,
+            label=_(u"Ignore updates unstable versions"))
+
+        mainSizer.Add(self.ignoreUnstableCheckBox,
+                      flag=wx.ALIGN_CENTER_VERTICAL | wx.ALL,
+                      border=4)
 
         self.SetSizer(mainSizer)
         self.Layout()
 
-
     def LoadState(self):
         self.__controller.loadState()
 
-
-    def Save (self):
+    def Save(self):
         self.__controller.save()
