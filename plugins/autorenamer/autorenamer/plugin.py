5ab806a74eb3f08c23718fdcb1d6fe3854955f2f
Jenyay <jenyay.ilin@gmail.com>
2018-01-06 21:45:47 +0300

    Fix Autorenamer plug-in.

--- plugins/autorenamer/autorenamer/plugin.py
+++ plugins/autorenamer/autorenamer/plugin.py
@@ -1,86 +1,46 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-import wx
+import os.path
 
-from outwiker.gui.preferences.preferencepanelinfo import PreferencePanelInfo
-from outwiker.pages.html.basehtmlpanel import EVT_PAGE_TAB_CHANGED
-from .actions import AddAutoRenameTagAction 
-from .preferencesPanel import PreferencesPanel
-from .commands import AutoRenameTagCommand
-from .renamer import Renamer
+from outwiker.core.pluginbase import Plugin
 
-from .i18n import get_
+from .i18n import set_
 
-class AutoRenamer (object):
-	def __init__ (self, plugin, application):
-		self._application = application
-		self._plugin = plugin
-		self.ID_ADDAUTORENAMETAG = wx.NewId()
-		self._menu = None
+from .autorenamer import AutoRenamer
 
-	def initialize (self):
-		global _
-		_ = get_()
 
-		self._renamer = Renamer(self._application)
+class PluginAutoRenamer(Plugin):
+    def __init__(self, application):
+        Plugin.__init__(self, application)
+        self._autorenamer = AutoRenamer(self, application)
 
-		self._application.onForceSave += self._renamer.RenamePage
-		self._application.onPreferencesDialogCreate += self.__onPreferencesDialogCreate
-		self._application.onPageViewCreate += self.__onPageViewCreate
-		self._application.onPageViewDestroy += self.__onPageViewDestroy
-		self._application.onWikiParserPrepare += self.__onWikiParserPrepare
+    @property
+    def name(self):
+        return u"AutoRenamer"
 
-	def destroy (self):
-		self._application.onForceSave -= self._renamer.RenamePage
-		self._application.onPreferencesDialogCreate -= self.__onPreferencesDialogCreate
-		self._application.onPageViewCreate -= self.__onPageViewCreate
-		self._application.onPageViewDestroy -= self.__onPageViewDestroy
-		self._application.onWikiParserPrepare -= self.__onWikiParserPrepare
+    @property
+    def description(self):
+        description = _(
+            u'''Plugin allows to rename all pages automatically using the first line of the page or automatically rename just those pages where you place (:autorename:) mark''')
+        author = _(u'''<b>Author:</b> Vitalii Koshura (delionkur-lestat@mail.ru)''')
+        return u"""{description}\n\n{author}""".format(description=description, author=author)
 
-	def __onPreferencesDialogCreate (self, dialog):
-		prefPanel = PreferencesPanel (dialog.treeBook, self._application.config)
-		panelName = _(u"AutoRenamer [Plugin]")
-		panelList = [PreferencePanelInfo (prefPanel, panelName)]
-		dialog.appendPreferenceGroup (panelName, panelList)
+    @property
+    def url(self):
+        return u"https://github.com/AenBleidd/OutwikerPlugin"
 
-	def __onPageViewCreate (self, page):
-		assert self._application.mainWindow is not None
+    def initialize(self):
+        self._initlocale(u"AutoRenamer")
+        self._autorenamer.initialize()
 
-		if page.getTypeString() == u"wiki":
-			self.addMenuItem()
-			self._application.mainWindow.pagePanel.pageView.Bind (EVT_PAGE_TAB_CHANGED, self._onTabChanged)
-			self.enableMenu()
+    def destroy(self):
+        self._autorenamer.destroy()
 
-	def __onPageViewDestroy (self, page):
-		assert self._application.mainWindow is not None
-
-		if page.getTypeString() == u"wiki":
-			self.removeMenuItem()
-			self._application.mainWindow.pagePanel.pageView.Unbind (EVT_PAGE_TAB_CHANGED, handler=self._onTabChanged)
-
-	def _onTabChanged(self, event):
-		self.enableMenu()
-		event.Skip()
-
-	def __onWikiParserPrepare (self, parser):
-		parser.addCommand (AutoRenameTagCommand(self._application, parser))
-
-	def enableMenu(self):
-		pageView = self._application.mainWindow.pagePanel.pageView
-		enabled = (pageView.selectedPageIndex == pageView.CODE_PAGE_INDEX)
-		self._application.actionController.enableTools (AddAutoRenameTagAction.stringId, enabled)
-	
-
-	def addMenuItem (self):
-		self._application.actionController.register (AddAutoRenameTagAction (self._application), None)
-		if self._application.mainWindow is not None:
-			self._menu = wx.Menu()
-			self._submenuItem = self._application.mainWindow.pagePanel.pageView.toolsMenu.AppendSubMenu (self._menu, _(u"AutoRenamer"))
-			self._application.actionController.appendMenuItem (AddAutoRenameTagAction.stringId, self._menu)
-
-	def removeMenuItem (self):
-		if self._application.mainWindow is not None:
-			self._application.actionController.removeMenuItem (AddAutoRenameTagAction.stringId)
-			self._application.mainWindow.pagePanel.pageView.toolsMenu.DestroyItem (self._submenuItem)
-			self._submenuItem = None
-			self._application.actionController.removeAction (AddAutoRenameTagAction.stringId)
+    def _initlocale(self, domain):
+        langdir = os.path.join(os.path.dirname(__file__), "locale")
+        global _
+        try:
+            _ = self._init_i18n(domain, langdir)
+        except BaseException as e:
+            print(e)
+        set_(_)
