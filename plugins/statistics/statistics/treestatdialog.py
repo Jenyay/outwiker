fd4cecf3cea2471cc3e71e61d737626ec9c8e645
Jenyay <jenyay.ilin@gmail.com>
2018-01-17 09:41:40 +0300

    Statistics plug-in. Fix tree statistics.

--- plugins/statistics/statistics/treestatdialog.py
+++ plugins/statistics/statistics/treestatdialog.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 import wx
 
@@ -13,124 +13,127 @@ from .datepageinfo import DatePageInfo
 from .pagecontentlengthinfo import PageContentLengthInfo
 from .pageattachmentsizeinfo import PageAttachmentSizeInfo
 from .statisticsconfig import StatisticsConfig
-from .longprocessrunner import LongProcessRunner
+from outwiker.gui.longprocessrunner import LongProcessRunner
 
 
-class TreeStatDialog (wx.Dialog):
-    def __init__ (self, parent, application, treestat):
+class TreeStatDialog(wx.Dialog):
+    def __init__(self, parent, application, treestat):
         """
         treestat - экземпляр класса TreeStat
         """
-        super (TreeStatDialog, self).__init__ (
+        super(TreeStatDialog, self).__init__(
             parent,
             style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
 
         self._application = application
         self._treestat = treestat
-        self._config = StatisticsConfig (self._application.config)
+        self._config = StatisticsConfig(self._application.config)
 
         # Размер списков со страницами
         self._itemsCount = 20
 
         # Шаблон для содержимого HTML-рендера
         self._htmlTemplate = u"""<!DOCTYPE html>
-<HTML>
-<HEAD>
-	<META HTTP-EQUIV='X-UA-Compatible' CONTENT='IE=edge' />
-	<META HTTP-EQUIV='CONTENT-TYPE' CONTENT='TEXT/HTML; CHARSET=UTF-8'/>
-</HEAD>
+<html>
+<head>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+    <meta http-equiv='content-type' content='text/html; charset=utf-8'/>
+</head>
 
-<BODY>
+<body>
 {content}
-</BODY>
-</HTML>"""
+</body>
+</html>"""
 
         global _
         _ = get_()
 
-        self.SetTitle (_(u"Tree Statistic"))
+        self.SetTitle(_(u"Tree Statistic"))
 
-        self.SetSize ((self._config.treeDialogWidth.value, self._config.treeDialogHeight.value))
+        self.SetSize((self._config.treeDialogWidth.value,
+                      self._config.treeDialogHeight.value))
 
         self.Show()
-        self._createGUI ()
-        self.Center (wx.BOTH)
-
+        self._createGUI()
+        self.Center(wx.BOTH)
         self._updateStatistics()
 
+    def _createGUI(self):
+        mainSizer = wx.FlexGridSizer(cols=1)
+        mainSizer.AddGrowableCol(0)
+        mainSizer.AddGrowableRow(0)
 
-    def _createGUI (self):
-        mainSizer = wx.FlexGridSizer (cols=1)
-        mainSizer.AddGrowableCol (0)
-        mainSizer.AddGrowableRow (0)
+        self._htmlRender = getHtmlRender(self)
 
-        self._htmlRender = getHtmlRender (self)
-
-        # Шаманство, связанное с тем, что HTML-рендер ожидает, что есть выбранная страница
-        # Если бы ему это было не обязательно, то достаточно было бы использовать только следующую строку
+        # Шаманство, связанное с тем, что HTML-рендер ожидает,
+        # что есть выбранная страница.
+        # Если бы ему это было не обязательно, то достаточно
+        # было бы использовать только следующую строку
         # self._htmlRender.page = self._application.selectedPage
 
         if self._application.selectedPage is not None:
             self._htmlRender.page = self._application.selectedPage
-        elif len (self._application.wikiroot.children) != 0:
+        elif len(self._application.wikiroot.children) != 0:
             self._htmlRender.page = self._application.wikiroot.children[0]
 
-        mainSizer.Add (self._htmlRender, flag=wx.EXPAND | wx.ALL, border=4)
+        mainSizer.Add(self._htmlRender, flag=wx.EXPAND | wx.ALL, border=4)
 
         # Кнопка Ok
-        okBtn = wx.Button (self, wx.ID_OK)
-        mainSizer.Add (okBtn, flag=wx.ALIGN_RIGHT | wx.ALL, border=4)
+        okBtn = wx.Button(self, wx.ID_OK)
+        mainSizer.Add(okBtn, flag=wx.ALIGN_RIGHT | wx.ALL, border=4)
 
-        self.Bind (wx.EVT_CLOSE, self._onClose)
-        self.Bind (wx.EVT_BUTTON, self._onClose, id=wx.ID_OK)
+        self.Bind(wx.EVT_CLOSE, self._onClose)
+        self.Bind(wx.EVT_BUTTON, self._onClose, id=wx.ID_OK)
 
-        self.SetSizer (mainSizer)
+        self.SetSizer(mainSizer)
         self.Layout()
 
-
-    def _updateStatistics (self):
-        # Шаманство, связанное с тем, что HTML-рендер ожидает, что есть выбранная страница
+    def _updateStatistics(self):
+        # Шаманство, связанное с тем, что HTML-рендер ожидает,
+        # что есть выбранная страница
         if self._htmlRender.page is None:
-            self._setHtml (_(u"A tree has no pages"))
+            self._setHtml(_(u"A tree has no pages"))
             return
 
-        self._setHtml (_(u"Collecting statistics. Please wait..."))
+        self._setHtml(_(u"Collecting statistics. Please wait..."))
 
-        runner = LongProcessRunner (self._getContent,
-                                    self,
-                                    _(u"Statistics"),
-                                    _(u"Collecting statistics..."))
+        runner = LongProcessRunner(self._getContent,
+                                   self,
+                                   _(u"Statistics"),
+                                   _(u"Collecting statistics..."))
 
-        resultList = runner.run ()
+        resultList = runner.run()
+        self._setHtml(resultList)
 
-        self._setHtml (resultList)
+    def _setHtml(self, content):
+        resultHtml = self._htmlTemplate.format(content=content)
+        self._htmlRender.SetPage(resultHtml, getCurrentDir())
 
-
-    def _setHtml (self, content):
-        resultHtml = self._htmlTemplate.format (content=content)
-        self._htmlRender.SetPage (resultHtml, getCurrentDir())
-
-
-    def _getContent (self):
+    def _getContent(self):
         """
         Создать HTML со статистикой. То, что должно быть внутри тега <body>
         """
-        infoList = [PageCountInfo (self._treestat.pageCount),
-                    MaxDepthInfo (self._treestat.maxDepth),
-                    TagsInfo (self._treestat.frequentTags, self._itemsCount),
-                    DatePageInfo (self._treestat.pageDate, self._itemsCount, self._application.config),
-                    PageContentLengthInfo (self._treestat.pageContentLength, self._itemsCount),
-                    PageAttachmentSizeInfo (self._treestat.pageAttachmentsSize, self._itemsCount)]
+        infoList = [
+            PageCountInfo(self._treestat.pageCount),
+            MaxDepthInfo(self._treestat.maxDepth),
+            TagsInfo(self._treestat.frequentTags,
+                     self._itemsCount),
+            DatePageInfo(self._treestat.pageDate,
+                         self._itemsCount,
+                         self._application.config),
+            PageContentLengthInfo(self._treestat.pageContentLength,
+                                  self._itemsCount),
+            PageAttachmentSizeInfo(self._treestat.pageAttachmentsSize,
+                                   self._itemsCount)
+        ]
 
-        return u"".join ([info.content for info in infoList])
+        return u"".join([info.content for info in infoList])
 
-
-    def _onClose (self, event):
+    def _onClose(self, event):
         self._saveParams()
         event.Skip()
 
-
-    def _saveParams (self):
-        width, height = self.GetSizeTuple()
+    def _saveParams(self):
+        width, height = self.GetSize()
         self._config.treeDialogWidth.value = width
         self._config.treeDialogHeight.value = height
