47c4b9f8afe287fd0e6dc1784004a5b205a47523
Jenyay <jenyay.ilin@gmail.com>
2018-01-26 21:55:17 +0300

    Style plug-in. Migrate to ActionsGUIController

--- plugins/style/style/controller.py
+++ plugins/style/style/controller.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
 
+from outwiker.pages.wiki.wikipage import WikiWikiPage
+from outwiker.pages.wiki.defines import MENU_WIKI_COMMANDS
+from outwiker.utilites.actionsguicontroller import (ActionsGUIController,
+                                                    ActionGUIInfo)
+
 from .stylecommand import StyleCommand
+from .actions import StyleAction
 
 
 class Controller(object):
     def __init__(self, application):
         self._application = application
 
+        self._GUIController = ActionsGUIController(
+            self._application,
+            WikiWikiPage.getTypeString(),
+        )
+
     def initialize(self):
         self._application.onWikiParserPrepare += self._onWikiParserPrepare
+        self._initialize_guicontroller()
+
+    def _initialize_guicontroller(self):
+        action_gui_info = [
+            ActionGUIInfo(StyleAction(self._application),
+                          MENU_WIKI_COMMANDS
+                          ),
+        ]
+
+        if self._application.mainWindow is not None:
+            self._GUIController.initialize(action_gui_info)
 
     def destroy(self):
         self._application.onWikiParserPrepare -= self._onWikiParserPrepare
+        self._destroy_guicontroller()
+
+    def _destroy_guicontroller(self):
+        if self._application.mainWindow is not None:
+            self._GUIController.destroy()
 
     def _onWikiParserPrepare(self, parser):
         parser.addCommand(StyleCommand(parser))
--- plugins/style/style/plugin.py
+++ plugins/style/style/plugin.py
@@ -5,7 +5,6 @@ import os.path
 from outwiker.core.pluginbase import Plugin
 
 from .controller import Controller
-from .guicontroller import GUIController
 
 
 def _no_translate(text):
@@ -22,7 +21,6 @@ class PluginStyle(Plugin):
         """
         super().__init__(application)
         self._controller = Controller(application)
-        self._GUIController = GUIController(application)
 
     @property
     def name(self):
@@ -46,7 +44,6 @@ body {background-color: #EEE;}
     def initialize(self):
         self._initlocale(u"style")
         self._controller.initialize()
-        self._GUIController.initialize()
 
     def _initlocale(self, domain):
         from .i18n import set_
@@ -71,4 +68,3 @@ body {background-color: #EEE;}
         Здесь плагин должен отписаться от всех событий
         """
         self._controller.destroy()
-        self._GUIController.destroy()
