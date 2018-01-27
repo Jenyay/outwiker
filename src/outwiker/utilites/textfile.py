af962bee5b0005666348c53a010dd4af2012f576
Jenyay <jenyay.ilin@gmail.com>
2018-01-27 18:26:27 +0300

    Refactoring.

--- src/outwiker/utilites/textfile.py
+++ src/outwiker/utilites/textfile.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-
-import codecs
+# -*- coding: utf-8 -*-
 
 
 def readTextFile(fname):
@@ -8,7 +6,7 @@ def readTextFile(fname):
     Read text content. Content must be in utf-8 encoding.
     The function return unicode object
     """
-    with codecs.open(fname, "r", "utf-8") as fp:
+    with open(fname, "r", encoding="utf-8") as fp:
         return fp.read()
 
 
@@ -16,5 +14,5 @@ def writeTextFile(fname, text):
     """
     Write text with utf-8 encoding
     """
-    with codecs.open(fname, "w", "utf-8") as fp:
+    with open(fname, "w", encoding="utf-8") as fp:
         fp.write(text)
