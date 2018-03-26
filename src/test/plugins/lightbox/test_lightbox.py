9af2291ff6792d824ffabf066b013c9e08f37645
Jenyay <jenyay.ilin@gmail.com>
2018-03-26 22:07:55 +0300

    Lightbox. Migrate to new test engine.

--- src/test/plugins/lightbox/test_lightbox.py
+++ src/test/plugins/lightbox/test_lightbox.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 import unittest
 import os.path
@@ -12,7 +12,7 @@ from outwiker.pages.wiki.parserfactory import ParserFactory
 from test.utils import removeDir
 
 
-class LightboxPluginTest (unittest.TestCase):
+class LightboxPluginTest(unittest.TestCase):
     def setUp(self):
         self.encoding = "utf8"
 
@@ -21,72 +21,68 @@ class LightboxPluginTest (unittest.TestCase):
         dirlist = ["../plugins/lightbox"]
 
         self.loader = PluginsLoader(Application)
-        self.loader.load (dirlist)
+        self.loader.load(dirlist)
 
         self.factory = ParserFactory()
-        self.parser = self.factory.make (self.testPage, Application.config)
+        self.parser = self.factory.make(self.testPage, Application.config)
 
-
-    def __createWiki (self):
+    def __createWiki(self):
         # Здесь будет создаваться вики
-        self.path = mkdtemp (prefix='Абырвалг абыр')
+        self.path = mkdtemp(prefix='Абырвалг абыр')
 
-        self.wikiroot = WikiDocument.create (self.path)
+        self.wikiroot = WikiDocument.create(self.path)
 
-        WikiPageFactory().create (self.wikiroot, "Страница 1", [])
+        WikiPageFactory().create(self.wikiroot, "Страница 1", [])
         self.testPage = self.wikiroot["Страница 1"]
 
-
     def tearDown(self):
-        removeDir (self.path)
+        removeDir(self.path)
         self.loader.clear()
 
+    def testPluginLoad(self):
+        self.assertEqual(len(self.loader), 1)
 
-    def testPluginLoad (self):
-        self.assertEqual (len (self.loader), 1)
-
-
-    def testContentParse1 (self):
+    def testContentParse1(self):
         text = """Бла-бла-бла (:lightbox:) бла-бла-бла"""
 
         validResult = """$("a[href$='.jpg']"""
 
-        result = self.parser.toHtml (text)
-        self.assertTrue (validResult in result)
+        result = self.parser.toHtml(text)
+        self.assertTrue(validResult in result)
 
-
-    def testHeaders (self):
+    def testHeaders(self):
         text = """Бла-бла-бла (:lightbox:) бла-бла-бла"""
 
-        self.parser.toHtml (text)
+        self.parser.toHtml(text)
 
-        self.assertTrue ('<script type="text/javascript" src="./__attach/__thumb/jquery-1.7.2.min.js"></script>' in self.parser.head)
+        self.assertTrue(
+            '<script type="text/javascript" src="./__attach/__thumb/jquery-1.7.2.min.js"></script>' in self.parser.head)
 
-        self.assertTrue ('<link rel="stylesheet" href="./__attach/__thumb/jquery.fancybox.css" type="text/css" media="screen" />' in self.parser.head)
+        self.assertTrue(
+            '<link rel="stylesheet" href="./__attach/__thumb/jquery.fancybox.css" type="text/css" media="screen" />' in self.parser.head)
 
-        self.assertTrue ('<script type="text/javascript" src="./__attach/__thumb/jquery.fancybox.pack.js"></script>' in self.parser.head)
+        self.assertTrue(
+            '<script type="text/javascript" src="./__attach/__thumb/jquery.fancybox.pack.js"></script>' in self.parser.head)
 
-
-    def testSingleHeaders (self):
+    def testSingleHeaders(self):
         """
         Проверка, что заголовки добавляются только один раз
         """
         text = """Бла-бла-бла (:lightbox:) бла-бла-бла (:lightbox:)"""
 
-        self.parser.toHtml (text)
+        self.parser.toHtml(text)
 
         header = '<script type="text/javascript" src="./__attach/__thumb/jquery-1.7.2.min.js"></script>'
 
-        posfirst = self.parser.head.find (header)
-        poslast = self.parser.head.rfind (header)
+        posfirst = self.parser.head.find(header)
+        poslast = self.parser.head.rfind(header)
 
-        self.assertEqual (posfirst, poslast)
+        self.assertEqual(posfirst, poslast)
 
-
-    def testFiles (self):
+    def testFiles(self):
         text = """Бла-бла-бла (:lightbox:) бла-бла-бла"""
 
-        self.parser.toHtml (text)
+        self.parser.toHtml(text)
 
         dirname = "__attach/__thumb"
         files = ["jquery.fancybox.css",
@@ -98,5 +94,5 @@ class LightboxPluginTest (unittest.TestCase):
                  ]
 
         for fname in files:
-            fullpath = os.path.join (self.testPage.path, dirname, fname)
-            self.assertTrue (os.path.exists (fullpath))
+            fullpath = os.path.join(self.testPage.path, dirname, fname)
+            self.assertTrue(os.path.exists(fullpath))
--- src/test/plugins/lightbox/test_loading.py
+++ src/test/plugins/lightbox/test_loading.py
@@ -1,17 +1,16 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 from test.plugins.baseloading import BasePluginLoadingTest
 
 
-class LightboxLoadingTest (BasePluginLoadingTest):
-    def getPluginDir (self):
+class LightboxLoadingTest(BasePluginLoadingTest):
+    def getPluginDir(self):
         """
         Должен возвращать путь до папки с тестируемым плагином
         """
         return "../plugins/lightbox"
 
-
-    def getPluginName (self):
+    def getPluginName(self):
         """
         Должен возвращать имя плагина, по которому его можно найти в PluginsLoader
         """
