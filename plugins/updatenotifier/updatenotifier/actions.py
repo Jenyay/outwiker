1c223a7c63d62230778b57e70a9447b64b206be8
Vladimir <vladimir.tuboltsev@gmail.com>
2018-04-07 23:05:42 +0300

    InstallerAction was added

--- plugins/updatenotifier/updatenotifier/actions.py
+++ plugins/updatenotifier/updatenotifier/actions.py
@@ -4,57 +4,76 @@ from outwiker.gui.baseaction import BaseAction
 from .i18n import get_
 
 
-class CheckForUpdatesAction (BaseAction):
+class CheckForUpdatesAction(BaseAction):
     """
     Проверка обновлений
     """
     stringId = u"UpdateNotifier_CheckForUpdates"
 
-    def __init__ (self, application, controller):
+    def __init__(self, application, controller):
         self._application = application
         self._controller = controller
 
         global _
         _ = get_()
 
-
     @property
-    def title (self):
+    def title(self):
         return _(u"Check for Updates...")
 
-
     @property
-    def description (self):
+    def description(self):
         return _(u"UpdateNotifier plugin. Check for updates")
 
-
-    def run (self, params):
+    def run(self, params):
         self._controller.checkForUpdates()
 
 
-class CheckForUpdatesSilenceAction (BaseAction):
+class CheckForUpdatesSilenceAction(BaseAction):
     """
     Проверка обновлений. Тихий режим
     """
     stringId = u"UpdateNotifier_CheckForUpdatesSilence"
 
-    def __init__ (self, application, controller):
+    def __init__(self, application, controller):
         self._application = application
         self._controller = controller
 
         global _
         _ = get_()
 
-
     @property
-    def title (self):
+    def title(self):
         return _(u"Silence check for Updates...")
 
-
     @property
-    def description (self):
+    def description(self):
         return _(u"UpdateNotifier plugin. Check for updates. Silence mode")
 
+    def run(self, params):
+        self._controller.checkForUpdatesSilence()
 
-    def run (self, params):
+
+class InstallerAction(BaseAction):
+    """
+    InstallerAction plugin action
+    """
+    stringId = u"UpdateNotifier_Installer"
+
+    def __init__(self, application, controller):
+        self._application = application
+        self._controller = controller
+
+        global _
+        _ = get_()
+
+    @property
+    def title(self):
+        return _(u"Install more plugins...")
+
+    @property
+    def description(self):
+        return _(u"UpdateNotifier plugin. Install new plugins or remove existing")
+
+    def run(self, params):
         self._controller.checkForUpdatesSilence()
