2c2bae17cb12f61f8c4fc2f26a90ceb801549081
Jenyay <jenyay.ilin@gmail.com>
2018-05-11 09:58:10 +0300

    Restore build with the PyGObject

--- buildtools/builders/binarybuilders.py
+++ buildtools/builders/binarybuilders.py
@@ -256,27 +256,27 @@ class PyInstallerBuilderLinuxBase(BasePyInstallerBuilder):
 class PyInstallerBuilderLinuxSimple(PyInstallerBuilderLinuxBase):
     pass
 
-    # def get_additional_files(self):
-    #     files = []
-    #     self._append_pixbuf_files(files)
-    #     self._append_immodules_files(files)
-    #     return files
-    #
-    # def _append_immodules_files(self, files):
-    #     dir_dest = u'lib/immodules'
-    #     modules_dir = u'/usr/lib/x86_64-linux-gnu/gtk-3.0/3.0.0/immodules/'
-    #
-    #     files.append(('need_for_build/linux/immodules.cache', dir_dest))
-    #     self.append_so_files(files, modules_dir, dir_dest)
-    #
-    # def _append_pixbuf_files(self, files):
-    #     dir_dest = u'lib/gdk-pixbuf'
-    #     modules_dir = u'/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders'
-    #
-    #     files.append(('need_for_build/linux/loaders.cache', dir_dest))
-    #     self.append_so_files(files, modules_dir, dir_dest)
-    #
-    # def get_params(self):
-    #     params = super().get_params()
-    #     params.append(u'--runtime-hook=linux_runtime_hook.py')
-    #     return params
+    def get_additional_files(self):
+        files = []
+        self._append_pixbuf_files(files)
+        self._append_immodules_files(files)
+        return files
+
+    def _append_immodules_files(self, files):
+        dir_dest = u'lib/immodules'
+        modules_dir = u'/usr/lib/x86_64-linux-gnu/gtk-3.0/3.0.0/immodules/'
+
+        files.append(('need_for_build/linux/immodules.cache', dir_dest))
+        self.append_so_files(files, modules_dir, dir_dest)
+
+    def _append_pixbuf_files(self, files):
+        dir_dest = u'lib/gdk-pixbuf'
+        modules_dir = u'/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders'
+
+        files.append(('need_for_build/linux/loaders.cache', dir_dest))
+        self.append_so_files(files, modules_dir, dir_dest)
+
+    def get_params(self):
+        params = super().get_params()
+        params.append(u'--runtime-hook=linux_runtime_hook.py')
+        return params
--- src/linux_runtime_hook.py
+++ src/linux_runtime_hook.py
@@ -3,11 +3,11 @@
 import os
 import sys
 
-# os.environ['LD_LIBRARY_PATH'] = sys._MEIPASS
-# os.environ['GDK_PIXBUF_MODULE_FILE'] = sys._MEIPASS + '/lib/gdk-pixbuf/loaders.cache'
-# os.environ['GTK_IM_MODULE_FILE'] = sys._MEIPASS + '/lib/immodules/immodules.cache'
+os.environ['LD_LIBRARY_PATH'] = sys._MEIPASS
+os.environ['GDK_PIXBUF_MODULE_FILE'] = sys._MEIPASS + '/lib/gdk-pixbuf/loaders.cache'
+os.environ['GTK_IM_MODULE_FILE'] = sys._MEIPASS + '/lib/immodules/immodules.cache'
 # os.environ['GTK_THEME'] = 'Radiance'
 
-# print u'LD_LIBRARY_PATH = {}'.format(os.environ['LD_LIBRARY_PATH'])
-# print u'GDK_PIXBUF_MODULE_FILE = {}'.format(os.environ['GDK_PIXBUF_MODULE_FILE'])
-# print u'GTK_THEME = {}'.format(os.environ['GTK_THEME'])
+print('LD_LIBRARY_PATH = {}'.format(os.environ['LD_LIBRARY_PATH']))
+print('GDK_PIXBUF_MODULE_FILE = {}'.format(os.environ['GDK_PIXBUF_MODULE_FILE']))
+# print('GTK_THEME = {}'.format(os.environ['GTK_THEME']))
