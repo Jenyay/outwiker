36ac73a59568983a66b5c215e82fd0b664cf13ea
Jenyay <jenyay.ilin@gmail.com>
2018-06-12 09:38:41 +0300

    Test refactoring. Rename test saver

--- src/test/core/test_registrynotestree.py
+++ src/test/core/test_registrynotestree.py
@@ -5,7 +5,7 @@ from unittest import TestCase
 from outwiker.core.registrynotestree import BaseSaver, NotesTreeRegistry
 
 
-class EmptySaver(BaseSaver):
+class SimpleSaver(BaseSaver):
     def __init__(self, items_dict):
         self._items_dict = items_dict
         self.load_count = 0
@@ -21,35 +21,35 @@ class EmptySaver(BaseSaver):
 
 class RegistryNotesTreeTest(TestCase):
     def test_load(self):
-        saver = EmptySaver({})
+        saver = SimpleSaver({})
         NotesTreeRegistry(saver)
 
         self.assertEqual(saver.load_count, 1)
         self.assertEqual(saver.save_count, 0)
 
     def test_save(self):
-        saver = EmptySaver({})
+        saver = SimpleSaver({})
         reg = NotesTreeRegistry(saver)
         reg.save()
 
         self.assertEqual(saver.save_count, 1)
 
     def test_get_section_or_create_01(self):
-        saver = EmptySaver({})
+        saver = SimpleSaver({})
         reg = NotesTreeRegistry(saver)
 
         reg.get_section_or_create('раздел-1')
         reg.has_section('раздел-1')
 
     def test_get_section_or_create_02(self):
-        saver = EmptySaver({})
+        saver = SimpleSaver({})
         reg = NotesTreeRegistry(saver)
 
         reg.get_section_or_create('раздел-1', 'раздел-2')
         reg.has_section('раздел-1', 'раздел-2')
 
     def test_get_section_or_create_03(self):
-        saver = EmptySaver({
+        saver = SimpleSaver({
             'бла-бла-бла': 1000,
         })
         reg = NotesTreeRegistry(saver)
@@ -58,7 +58,7 @@ class RegistryNotesTreeTest(TestCase):
         reg.has_section('бла-бла-бла')
 
     def test_get_section_or_create_04(self):
-        saver = EmptySaver({
+        saver = SimpleSaver({
             'раздел-1': {
                 'бла-бла-бла': 1000,
             },
@@ -69,7 +69,7 @@ class RegistryNotesTreeTest(TestCase):
         reg.has_section('раздел-1', 'бла-бла-бла')
 
     def test_get_section_or_create_05(self):
-        saver = EmptySaver({})
+        saver = SimpleSaver({})
         reg = NotesTreeRegistry(saver)
 
         subreg = reg.get_section_or_create('раздел-1', 'раздел-2', 'раздел-3')
@@ -80,7 +80,7 @@ class RegistryNotesTreeTest(TestCase):
                          100)
 
     def test_get_section_or_create_06_invalid_type(self):
-        saver = EmptySaver({
+        saver = SimpleSaver({
             'раздел-1': {
                 'раздел-2': 0,
             }
