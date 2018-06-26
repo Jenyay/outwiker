6b31782df466fe4bc088de728d909e5effff6d16
Jenyay <jenyay.ilin@gmail.com>
2018-06-26 23:04:34 +0300

    Tests refactoring.

--- src/test/wikiparser/test_wikistyles.py
+++ src/test/wikiparser/test_wikistyles.py
@@ -32,79 +32,79 @@ class WikiStylesTest(unittest.TestCase):
         self.testPage = self.wikiroot["Страница 2"]
 
     def testInline_style_01(self):
-        text = "текст %red%бла-бла-бла%% текст"
-        result = 'текст <span class="red">бла-бла-бла</span> текст'
+        text = "текст %class-red%бла-бла-бла%% текст"
+        result = 'текст <span class="class-red">бла-бла-бла</span> текст'
 
         self.assertEqual(result, self.parser.toHtml(text))
 
     def testInline_style_space_01(self):
-        text = "текст %red% бла-бла-бла %% текст"
-        result = 'текст <span class="red"> бла-бла-бла </span> текст'
+        text = "текст %class-red% бла-бла-бла %% текст"
+        result = 'текст <span class="class-red"> бла-бла-бла </span> текст'
 
         self.assertEqual(result, self.parser.toHtml(text))
 
     def testInline_style_nested_01(self):
-        text = "текст %red%бла_red %blue%бла_blue%% бла%% текст"
-        result = 'текст <span class="red">бла_red <span class="blue">бла_blue</span> бла</span> текст'
+        text = "текст %class-red%бла_class-red %class-blue%бла_class-blue%% бла%% текст"
+        result = 'текст <span class="class-red">бла_class-red <span class="class-blue">бла_class-blue</span> бла</span> текст'
 
         self.assertEqual(result, self.parser.toHtml(text))
 
     def testInline_style_nested_01_space(self):
-        text = "текст %red% бла_red %blue% бла_blue %% бла %% текст"
-        result = 'текст <span class="red"> бла_red <span class="blue"> бла_blue </span> бла </span> текст'
+        text = "текст %class-red% бла_class-red %class-blue% бла_class-blue %% бла %% текст"
+        result = 'текст <span class="class-red"> бла_class-red <span class="class-blue"> бла_class-blue </span> бла </span> текст'
 
         self.assertEqual(result, self.parser.toHtml(text))
 
     def testInline_style_nested_02(self):
-        text = "текст %red%%blue%бла_blue%%%% текст"
-        result = 'текст <span class="red"><span class="blue">бла_blue</span></span> текст'
+        text = "текст %class-red%%class-blue%бла_class-blue%%%% текст"
+        result = 'текст <span class="class-red"><span class="class-blue">бла_class-blue</span></span> текст'
 
         self.assertEqual(result, self.parser.toHtml(text))
 
     def testInline_style_nested_02_space(self):
-        text = "текст %red% %blue% бла_blue %% %% текст"
-        result = 'текст <span class="red"> <span class="blue"> бла_blue </span> </span> текст'
+        text = "текст %class-red% %class-blue% бла_class-blue %% %% текст"
+        result = 'текст <span class="class-red"> <span class="class-blue"> бла_class-blue </span> </span> текст'
 
         self.assertEqual(result, self.parser.toHtml(text))
 
     def testInline_style_nested_03(self):
-        text = "текст %red%бла_red %blue%бла_blue%% %yellow%бла_yellow%% бла-бла %% текст"
-        result_valid = 'текст <span class="red">бла_red <span class="blue">бла_blue</span> <span class="yellow">бла_yellow</span> бла-бла </span> текст'
+        text = "текст %class-red%бла_class-red %class-blue%бла_class-blue%% %class-yellow%бла_class-yellow%% бла-бла %% текст"
+        result_valid = 'текст <span class="class-red">бла_class-red <span class="class-blue">бла_class-blue</span> <span class="class-yellow">бла_class-yellow</span> бла-бла </span> текст'
         result = self.parser.toHtml(text)
 
         self.assertEqual(result_valid, result, result)
 
     def testInline_style_nested_skip_01(self):
-        text = "текст %red%абыр валг [=%%=] проверка%% текст"
-        result_valid = 'текст <span class="red">абыр валг %% проверка</span> текст'
+        text = "текст %class-red%абыр валг [=%%=] проверка%% текст"
+        result_valid = 'текст <span class="class-red">абыр валг %% проверка</span> текст'
         result = self.parser.toHtml(text)
 
         self.assertEqual(result_valid, result, result)
 
     def testInline_style_nested_skip_02(self):
-        text = "текст %red%абырвалг %blue%текст blue%% [=%%=] проверка%% текст"
-        result_valid = 'текст <span class="red">абырвалг <span class="blue">текст blue</span> %% проверка</span> текст'
+        text = "текст %class-red%абырвалг %class-blue%текст class-blue%% [=%%=] проверка%% текст"
+        result_valid = 'текст <span class="class-red">абырвалг <span class="class-blue">текст class-blue</span> %% проверка</span> текст'
         result = self.parser.toHtml(text)
 
         self.assertEqual(result_valid, result, result)
 
     def testInline_style_nested_skip_03(self):
-        text = "текст %red%абырвалг %blue%текст blue%% 111 [=%%=] проверка%% текст"
-        result_valid = 'текст <span class="red">абырвалг <span class="blue">текст blue</span> 111 %% проверка</span> текст'
+        text = "текст %class-red%абырвалг %class-blue%текст class-blue%% 111 [=%%=] проверка%% текст"
+        result_valid = 'текст <span class="class-red">абырвалг <span class="class-blue">текст class-blue</span> 111 %% проверка</span> текст'
         result = self.parser.toHtml(text)
 
         self.assertEqual(result_valid, result, result)
 
     def testInline_style_nested_skip_04(self):
-        text = "текст %red%абырвалг [=%%=] %blue%текст blue%% проверка%% текст"
-        result_valid = 'текст <span class="red">абырвалг %% <span class="blue">текст blue</span> проверка</span> текст'
+        text = "текст %class-red%абырвалг [=%%=] %class-blue%текст class-blue%% проверка%% текст"
+        result_valid = 'текст <span class="class-red">абырвалг %% <span class="class-blue">текст class-blue</span> проверка</span> текст'
         result = self.parser.toHtml(text)
 
         self.assertEqual(result_valid, result, result)
 
     def testInline_style_nested_skip_05(self):
-        text = "текст %red%абырвалг %yellow%текст yellow%% [=%%=] %blue%текст blue%% проверка%% текст"
-        result_valid = 'текст <span class="red">абырвалг <span class="yellow">текст yellow</span> %% <span class="blue">текст blue</span> проверка</span> текст'
+        text = "текст %class-red%абырвалг %class-yellow%текст class-yellow%% [=%%=] %class-blue%текст class-blue%% проверка%% текст"
+        result_valid = 'текст <span class="class-red">абырвалг <span class="class-yellow">текст class-yellow</span> %% <span class="class-blue">текст class-blue</span> проверка</span> текст'
         result = self.parser.toHtml(text)
 
         self.assertEqual(result_valid, result, result)
