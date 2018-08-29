f6fdbbaa455a3ab3b7666aada6f9f31f5d9597b7
Jenyay <jenyay.ilin@gmail.com>
2018-08-29 13:30:04 +0300

    fab docker_build command is more universal now

--- need_for_build/build_docker/build.sh
+++ need_for_build/build_docker/build.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 python3 -m pip install --user pipenv
 pipenv run pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04 wxPython==4.0.3
 pipenv update --dev
-pipenv run fab "$@"
+if [[ $1 =~ ^test.* ]]
+then
+	xvfb-run pipenv run fab "$@"
+else
+	pipenv run fab "$@"
+fi
