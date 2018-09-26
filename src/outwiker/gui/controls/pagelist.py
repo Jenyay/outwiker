9dc8bd7f5ed3784e1d60b717fec7c777fd6b2a38
Eugeniy Ilin <jenyay.ilin@gmail.com>
2018-09-26 17:16:29 +0300

    Refactoring for future settings for PageList

--- src/outwiker/gui/controls/pagelist.py
+++ src/outwiker/gui/controls/pagelist.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
 import wx
 import wx.lib.newevent
 
@@ -12,14 +10,90 @@ PageClickEvent, EVT_PAGE_CLICK = wx.lib.newevent.NewEvent()
 
 
 class PageData(object):
-    def __init__(self, page, parent=None):
+    def __init__(self, page):
         self.page = page
-        self.parent = parent
+
+
+class BaseColumn(object):
+    '''
+    Base class to manage columns
+    '''
+    def __init__(self, title, width, visible=True):
+        self.title = title
+        self.width = width
+        self.visible = visible
+
+    def insertColumn(self, listCtrl, column):
+        '''
+        Add column
+        '''
+        listCtrl.InsertColumn(column, self.title)
+        listCtrl.SetColumnWidth(column, self.width)
+
+    def setCellProperties(self, listCtrl, item_index, column):
+        pass
+
+    def getCellContent(self, page):
+        pass
+
+
+class PageTitleColumn(BaseColumn):
+    '''
+    Column with page title (link to page)
+    '''
+    def setCellProperties(self, listCtrl, item_index, column):
+        listCtrl.SetItemHyperText(item_index, column)
+
+    def getCellContent(self, page):
+        return page.display_title
+
+
+class ParentPageColumn(BaseColumn):
+    '''
+    Column with page parent path
+    '''
+    def setCellProperties(self, listCtrl, item_index, column):
+        pass
+
+    def getCellContent(self, page):
+        parent_page = page.parent
+        if parent_page.parent:
+            return parent_page.display_subpath + '/'
+
+        return ''
+
+
+class TagsColumn(BaseColumn):
+    '''
+    Column with page tags
+    '''
+    def setCellProperties(self, listCtrl, item_index, column):
+        pass
+
+    def getCellContent(self, page):
+        return ', '.join(page.tags)
+
+
+class ModifyDateColumn(BaseColumn):
+    '''
+    Column with modify date of page
+    '''
+    def setCellProperties(self, listCtrl, item_index, column):
+        pass
+
+    def getCellContent(self, page):
+        return page.datetime.strftime('%d.%m.%Y     %H:%M')
 
 
 class PageList(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
+        self._columns = []
+        self._columns.append(PageTitleColumn(_('Title'), 200, True))
+        self._columns.append(ParentPageColumn(_('Parent'), 200, True))
+        self._columns.append(TagsColumn(_('Tags'), 200, True))
+        self._columns.append(ModifyDateColumn(_('Modify date'), 200, True))
+
         self._propagationLevel = 15
         self.SetBackgroundColour(wx.Colour(255, 255, 255))
 
@@ -57,17 +131,13 @@ class PageList(wx.Panel):
         """
         self._listCtrl.ClearAll()
 
-        self._listCtrl.InsertColumn(0, _('Title'))
-        self._listCtrl.SetColumnWidth(0, 200)
+    @property
+    def _visibleColumns(self):
+        return [column for column in self._columns if column.visible]
 
-        self._listCtrl.InsertColumn(1, _('Parent'))
-        self._listCtrl.SetColumnWidth(1, 200)
-
-        self._listCtrl.InsertColumn(2, _('Tags'))
-        self._listCtrl.SetColumnWidth(2, 200)
-
-        self._listCtrl.InsertColumn(3, _('Modify date'))
-        self._listCtrl.SetColumnWidth(3, 200)
+    def _createColumns(self):
+        for n, column in enumerate(self._visibleColumns):
+            column.insertColumn(self._listCtrl, n)
 
     def setPageList(self, pages):
         """
@@ -75,32 +145,17 @@ class PageList(wx.Panel):
         """
         self._listCtrl.Freeze()
         self.clear()
+        self._createColumns()
 
         for page in pages:
+            items = [column.getCellContent(page)
+                     for column
+                     in self._visibleColumns]
+            item_index = self._listCtrl.Append(items)
+            for n, column in enumerate(self._visibleColumns):
+                column.setCellProperties(self._listCtrl, item_index, n)
+
             data = PageData(page)
-            # Title
-            index = self._listCtrl.InsertStringItem(sys.maxsize,
-                                                    page.display_title)
-
-            self._listCtrl.SetItemHyperText(index, 0)
-
-            # Parent
-            parent_page = page.parent
-            if parent_page.parent:
-                self._listCtrl.SetStringItem(
-                    index,
-                    1,
-                    parent_page.display_subpath + '/')
-                # self._listCtrl.SetItemHyperText(index, 1)
-                data.parent = parent_page
-
-            # Tags
-            self._listCtrl.SetStringItem(index, 2, ', '.join(page.tags))
-
-            # Modify date
-            date = page.datetime.strftime('%d.%m.%Y     %H:%M')
-            self._listCtrl.SetStringItem(index, 3, date)
-
-            self._listCtrl.SetItemPyData(index, data)
+            self._listCtrl.SetItemPyData(item_index, data)
 
         self._listCtrl.Thaw()
