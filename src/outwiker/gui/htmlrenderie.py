2803832be7b496d9ff1906bcaffd57a1f168f030
Eugeniy Ilin <jenyay.ilin@gmail.com>
2018-10-01 14:12:06 +0300

    Clean code

--- src/outwiker/gui/htmlrenderie.py
+++ src/outwiker/gui/htmlrenderie.py
@@ -1,6 +1,8 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 
 import wx
 import wx.lib.iewin
@@ -21,11 +23,11 @@ class HtmlRenderIE (HtmlRender):
     """
     Класс для рендеринга HTML с использованием движка IE под Windows
     """
-    def __init__ (self, parent):
-        HtmlRender.__init__ (self, parent)
+    def __init__(self, parent):
+        HtmlRender.__init__(self, parent)
         config = GeneralGuiConfig(Application.config)
 
-        self.render = wx.lib.iewin.IEHtmlWindow (self)
+        self.render = wx.lib.iewin.IEHtmlWindow(self)
         self.render.silent = not config.debug.value
 
         # Подпишемся на события IE
@@ -35,35 +37,33 @@ class HtmlRenderIE (HtmlRender):
 
         self.__layout()
 
-        self.Bind (wx.EVT_MENU, self.__onCopyFromHtml, id = wx.ID_COPY)
-        self.Bind (wx.EVT_MENU, self.__onCopyFromHtml, id = wx.ID_CUT)
+        self.Bind(wx.EVT_MENU, self.__onCopyFromHtml, id=wx.ID_COPY)
+        self.Bind(wx.EVT_MENU, self.__onCopyFromHtml, id=wx.ID_CUT)
 
+    def Print(self):
+        self.render.Print(True)
 
-    def Print (self):
-        self.render.Print (True)
-
-
-    def SetPage (self, htmltext, basepath):
+    def SetPage(self, htmltext, basepath):
         """
         Загрузить страницу из строки
         htmltext - текст страницы
-        basepath - путь, относительно которого отсчитываются относительные пути (НЕ ИСПОЛЬЗУЕТСЯ!!!)
+        basepath - путь, относительно которого отсчитываются относительные
+            пути (НЕ ИСПОЛЬЗУЕТСЯ!!!)
         """
         self.canOpenUrl = True
-        self.render.LoadString (htmltext)
+        self.render.LoadString(htmltext)
         self.canOpenUrl = False
-
 
     def StatusTextChange(self, status):
         statustext = u""
 
-        if len (status) != 0:
-            (url, page, filename, anchor) = self.__identifyUri (status)
+        if len(status) != 0:
+            (url, page, filename, anchor) = self.__identifyUri(status)
 
             if page is not None:
                 text = page.display_subpath
                 if anchor is not None:
-                    text = u"{}/{}".format (text, anchor)
+                    text = u"{}/{}".format(text, anchor)
 
                 statustext = text
             elif filename is not None:
@@ -73,8 +73,7 @@ class HtmlRenderIE (HtmlRender):
             else:
                 statustext = status
 
-        self.setStatusText (status, statustext)
-
+        self.setStatusText(status, statustext)
 
     def __onCopyFromHtml(self, event):
         document = self.render.document
@@ -83,19 +82,17 @@ class HtmlRenderIE (HtmlRender):
         if selection is not None:
             selrange = selection.createRange()
             if selrange is not None:
-                outwiker.core.commands.copyTextToClipboard (selrange.text)
+                outwiker.core.commands.copyTextToClipboard(selrange.text)
                 event.Skip()
 
-
-    def __layout (self):
+    def __layout(self):
         self.box = wx.BoxSizer(wx.VERTICAL)
         self.box.Add(self.render, 1, wx.EXPAND)
 
         self.SetSizer(self.box)
         self.Layout()
 
-
-    def LoadPage (self, fname):
+    def LoadPage(self, fname):
         self.canOpenUrl = True
         path = fname
 
@@ -103,38 +100,36 @@ class HtmlRenderIE (HtmlRender):
             path += Application.sharedData[APP_DATA_KEY_ANCHOR]
             del Application.sharedData[APP_DATA_KEY_ANCHOR]
 
-        self.render.Navigate (path)
+        self.render.Navigate(path)
 
-
-    def __cleanUpUrl (self, href):
+    def __cleanUpUrl(self, href):
         """
         Почистить ссылку, убрать file:///
         """
-        result = self.__removeFileProtokol (href)
-        result = urllib.parse.unquote (result)
-        result = result.replace ("/", u"\\")
+        result = self.__removeFileProtokol(href)
+        result = urllib.parse.unquote(result)
+        result = result.replace("/", u"\\")
 
         return result
 
-
-    def __removeFileProtokol (self, href):
+    def __removeFileProtokol(self, href):
         """
         Избавиться от протокола file:///, то избавимся от этой надписи
         """
         fileprotocol = u"file:///"
-        if href.startswith (fileprotocol):
-            return href[len (fileprotocol):]
+        if href.startswith(fileprotocol):
+            return href[len(fileprotocol):]
 
         return href
 
-
-    def BeforeNavigate2 (self, this, pDisp, URL, Flags,
-                         TargetFrameName, PostData, Headers, Cancel):
+    def BeforeNavigate2(self, this, pDisp, URL, Flags,
+                        TargetFrameName, PostData, Headers, Cancel):
         href = URL[0]
-        curr_href = self.__cleanUpUrl (self.render.locationurl)
+        curr_href = self.__cleanUpUrl(self.render.locationurl)
 
-        # Пока другого признака о том, что пытаемся открыть встроенный фрейм, не нашел
-        if 'LocationURL' in dir (pDisp) and pDisp.LocationURL == "about:blank":
+        # Пока другого признака о том, что пытаемся открыть встроенный фрейм,
+        # не нашел
+        if 'LocationURL' in dir(pDisp) and pDisp.LocationURL == "about:blank":
             Cancel[0] = False
             return
 
@@ -143,28 +138,27 @@ class HtmlRenderIE (HtmlRender):
             self.canOpenUrl = False
         else:
             Cancel[0] = True
-            self.__onLinkClicked (href)
+            self.__onLinkClicked(href)
 
-
-    def NewWindow3 (self, this, pDisp, Cancel, dwFlags, currentURL, href):
+    def NewWindow3(self, this, pDisp, Cancel, dwFlags, currentURL, href):
         Cancel[0] = True
 
-        (url, page, filename, anchor) = self.__identifyUri (href)
+        (url, page, filename, anchor) = self.__identifyUri(href)
         if page is not None:
-            Application.mainWindow.tabsController.openInTab (page, True)
+            Application.mainWindow.tabsController.openInTab(page, True)
 
-
-    def __identifyUri (self, href):
+    def __identifyUri(self, href):
         """
         Определить тип ссылки и вернуть кортеж (url, page, filename)
         """
-        identifier = UriIdentifierIE (self._currentPage,
-                                      self.__cleanUpUrl (self.render.locationurl))
+        identifier = UriIdentifierIE(
+            self._currentPage,
+            self.__cleanUpUrl(self.render.locationurl)
+        )
 
-        return identifier.identify (href)
+        return identifier.identify(href)
 
-
-    def __getKeyCode (self):
+    def __getKeyCode(self):
         modifier = 0
 
         if wx.GetKeyState(wx.WXK_SHIFT):
@@ -175,44 +169,44 @@ class HtmlRenderIE (HtmlRender):
 
         return modifier
 
-
-    def __onLinkClicked (self, href):
+    def __onLinkClicked(self, href):
         """
         Клик по ссылке
         """
-        (url, page, filename, anchor) = self.__identifyUri (href)
+        (url, page, filename, anchor) = self.__identifyUri(href)
 
         button = ID_MOUSE_LEFT
         modifier = self.__getKeyCode()
 
-        params = self._getClickParams (self._decodeIDNA (href),
-                                       button,
-                                       modifier,
-                                       url,
-                                       page,
-                                       filename,
-                                       anchor)
+        params = self._getClickParams(self._decodeIDNA(href),
+                                      button,
+                                      modifier,
+                                      url,
+                                      page,
+                                      filename,
+                                      anchor)
 
-        Application.onLinkClick (self._currentPage, params)
+        Application.onLinkClick(self._currentPage, params)
         if params.process:
             return
 
         if url is not None:
-            self.openUrl (url)
+            self.openUrl(url)
         elif page is not None and modifier == ID_KEY_CTRL:
             if anchor is not None:
                 Application.sharedData[APP_DATA_KEY_ANCHOR] = anchor
-            Application.mainWindow.tabsController.openInTab (page, True)
+            Application.mainWindow.tabsController.openInTab(page, True)
         elif page is not None:
             if anchor is not None:
                 Application.sharedData[APP_DATA_KEY_ANCHOR] = anchor
             self._currentPage.root.selectedPage = page
         elif filename is not None:
             try:
-                outwiker.core.system.getOS().startFile (filename)
+                outwiker.core.system.getOS().startFile(filename)
             except OSError:
                 text = _(u"Can't execute file '%s'") % filename
-                outwiker.core.commands.MessageBox (text, _(u"Error"), wx.ICON_ERROR | wx.OK)
+                outwiker.core.commands.MessageBox(text,
+                                                  _(u"Error"),
+                                                  wx.ICON_ERROR | wx.OK)
         elif anchor is not None:
-            self.LoadPage (href)
-
+            self.LoadPage(href)
