de6ff87a3f2a7062d10f62f84b23e1506e839f12
Jenyay <jenyay.ilin@gmail.com>
2018-11-05 21:42:58 +0300

    Remove empty item from ThumbDialog

--- src/outwiker/pages/wiki/thumbdialog.py
+++ src/outwiker/pages/wiki/thumbdialog.py
@@ -8,15 +8,16 @@ class ThumbDialog (wx.Dialog):
     HEIGHT = 1
     MAX_SIZE = 2
 
-    def __init__ (self, parent, filesList, selectedFile):
+    def __init__(self, parent, filesList, selectedFile):
         """
         parent - родительское окно
         filesList - список файлов, отображаемых в диалоге
-        selectedFile - файл выбранный по умолчанию. Если selectedFile == None, никакой файл по умолчанию не выбирается
+        selectedFile - файл выбранный по умолчанию. Если selectedFile == None,
+            никакой файл по умолчанию не выбирается
         """
-        super (ThumbDialog, self).__init__ (parent,
-                                            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER,
-                                            title=_("Thumbnails"))
+        super().__init__(parent,
+                         style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER,
+                         title=_("Thumbnails"))
 
         self.__filesList = filesList
         self.__selectedFile = selectedFile
@@ -25,71 +26,72 @@ class ThumbDialog (wx.Dialog):
         self.filesListCombo.SetFocus()
         self.Center(wx.BOTH)
 
-
     @property
-    def fileName (self):
+    def fileName(self):
         return self.filesListCombo.GetValue()
 
-
     @property
-    def scaleType (self):
+    def scaleType(self):
         return self.scaleCombo.GetSelection()
 
-
     @property
-    def size (self):
+    def size(self):
         return self.sizeCtrl.GetValue()
 
-
-    def __createGui (self):
+    def __createGui(self):
         # Элементы для выбор имени файла
-        filenameLabel = wx.StaticText (self, label=_(u"File name"))
-        self.filesListCombo = wx.ComboBox (self,
-                                           choices = [u""] + self.__filesList,
-                                           style=wx.CB_READONLY)
-        self.filesListCombo.SetSelection (0)
-        self.filesListCombo.SetMinSize ((250, -1))
+        filenameLabel = wx.StaticText(self, label=_(u"File name"))
+        self.filesListCombo = wx.ComboBox(self,
+                                          choices=self.__filesList,
+                                          style=wx.CB_READONLY)
+        self.filesListCombo.SetSelection(0)
+        self.filesListCombo.SetMinSize((250, -1))
 
-        if len (self.__selectedFile) > 0:
+        if self.__selectedFile:
             assert self.__selectedFile in self.__filesList
-            self.filesListCombo.SetStringSelection (self.__selectedFile)
+            self.filesListCombo.SetStringSelection(self.__selectedFile)
 
         # Элементы для выбора размера
-        scaleLabel = wx.StaticText (self, label=_(u"Thumbnail size"))
+        scaleLabel = wx.StaticText(self, label=_(u"Thumbnail size"))
 
         scaleItems = [_(u"Width"), _(u"Height"), _(u"Max size")]
-        self.scaleCombo = wx.ComboBox (self,
-                                       choices = scaleItems,
-                                       style=wx.CB_READONLY)
-        self.scaleCombo.SetSelection (0)
-        self.scaleCombo.SetMinSize ((250, -1))
+        self.scaleCombo = wx.ComboBox(self,
+                                      choices=scaleItems,
+                                      style=wx.CB_READONLY)
+        self.scaleCombo.SetSelection(0)
+        self.scaleCombo.SetMinSize((250, -1))
 
-        self.sizeCtrl = wx.SpinCtrl (self, min=0, max=10000, initial=0)
-        self.sizeCtrl.SetMinSize ((100, -1))
-        sizeLabel = wx.StaticText (self, label=_(u"0 - default size"))
+        self.sizeCtrl = wx.SpinCtrl(self, min=0, max=10000, initial=0)
+        self.sizeCtrl.SetMinSize((100, -1))
+        sizeLabel = wx.StaticText(self, label=_(u"0 - default size"))
 
-        okCancel = self.CreateButtonSizer (wx.OK | wx.CANCEL)
+        okCancel = self.CreateButtonSizer(wx.OK | wx.CANCEL)
 
         # Расстановка элементов
         mainSizer = wx.FlexGridSizer(rows=0, cols=2, vgap=0, hgap=0)
-        mainSizer.AddGrowableCol (0)
-        mainSizer.AddGrowableCol (1)
-        mainSizer.AddGrowableRow (4)
+        mainSizer.AddGrowableCol(0)
+        mainSizer.AddGrowableCol(1)
+        mainSizer.AddGrowableRow(4)
 
-        mainSizer.Add (filenameLabel, 0, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=4)
-        mainSizer.Add (self.filesListCombo, 0, flag = wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, border=4)
+        mainSizer.Add(filenameLabel, 0, flag=wx.ALL |
+                      wx.ALIGN_CENTER_VERTICAL, border=4)
+        mainSizer.Add(self.filesListCombo, 0, flag=wx.ALL |
+                      wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, border=4)
 
-        mainSizer.Add (scaleLabel, 0, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=4)
+        mainSizer.Add(scaleLabel, 0, flag=wx.ALL |
+                      wx.ALIGN_CENTER_VERTICAL, border=4)
         mainSizer.AddStretchSpacer()
 
-        mainSizer.Add (self.scaleCombo, 0, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, border=4)
-        mainSizer.Add (self.sizeCtrl, 0, flag=wx.ALL | wx.EXPAND, border=4)
+        mainSizer.Add(self.scaleCombo, 0, flag=wx.ALL |
+                      wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, border=4)
+        mainSizer.Add(self.sizeCtrl, 0, flag=wx.ALL | wx.EXPAND, border=4)
 
         mainSizer.AddStretchSpacer()
-        mainSizer.Add (sizeLabel, 0, flag=wx.ALL | wx.EXPAND, border=4)
+        mainSizer.Add(sizeLabel, 0, flag=wx.ALL | wx.EXPAND, border=4)
 
         mainSizer.AddStretchSpacer()
-        mainSizer.Add (okCancel, 0, flag=wx.ALL | wx.ALIGN_RIGHT | wx.ALIGN_BOTTOM, border=4)
+        mainSizer.Add(okCancel, 0, flag=wx.ALL |
+                      wx.ALIGN_RIGHT | wx.ALIGN_BOTTOM, border=4)
 
         self.SetSizer(mainSizer)
         self.Fit()
--- src/outwiker/pages/wiki/thumbdialogcontroller.py
+++ src/outwiker/pages/wiki/thumbdialogcontroller.py
@@ -7,7 +7,7 @@ from .parser.utils import isImage
 
 
 class ThumbDialogController (object):
-    def __init__ (self, parent, page, selectedText):
+    def __init__(self, parent, page, selectedText):
         """
         parent - родительское окно
         page - текущая страница (не может быть равна None)
@@ -22,32 +22,30 @@ class ThumbDialogController (object):
         # Строка, полученная из параметров, выбанных в диалоге
         self.result = u""
 
+    def showDialog(self):
+        filesList = list(
+            filter(isImage, Attachment(self._page).getAttachRelative()))
+        filesList.sort(key=lambda a: a.lower())
 
-    def showDialog (self):
-        filesList = list(filter (isImage, Attachment (self._page).getAttachRelative()))
-        filesList.sort (key=lambda a: a.lower())
-
-        if (self._selectedText.startswith (u"Attach:") and
-                self._selectedText[len (u"Attach:"):] in filesList):
-            selectedFile = self._selectedText[len (u"Attach:"):]
+        if (self._selectedText.startswith(u"Attach:") and
+                self._selectedText[len(u"Attach:"):] in filesList):
+            selectedFile = self._selectedText[len(u"Attach:"):]
         else:
             selectedFile = u""
 
-        dlg = self._createDialog (self._parent, filesList, selectedFile)
+        dlg = self._createDialog(self._parent, filesList, selectedFile)
         resultDlg = dlg.ShowModal()
 
-        self.result = self.__generateText (dlg)
+        self.result = self.__generateText(dlg)
 
         dlg.Destroy()
 
         return resultDlg
 
+    def _createDialog(self, parent, filesList, selectedFile):
+        return ThumbDialog(parent, filesList, selectedFile)
 
-    def _createDialog (self, parent, filesList, selectedFile):
-        return ThumbDialog (parent, filesList, selectedFile)
-
-
-    def __generateText (self, dlg):
+    def __generateText(self, dlg):
         size = dlg.size
         fname = dlg.fileName
         scaleType = dlg.scaleType
@@ -55,18 +53,19 @@ class ThumbDialogController (object):
         if size == 0:
             scaleText = u""
         elif scaleType == ThumbDialog.WIDTH:
-            scaleText = u" width={size}".format (size = size)
+            scaleText = u" width={size}".format(size=size)
         elif scaleType == ThumbDialog.HEIGHT:
-            scaleText = u" height={size}".format (size = size)
+            scaleText = u" height={size}".format(size=size)
         elif scaleType == ThumbDialog.MAX_SIZE:
-            scaleText = u" maxsize={size}".format (size = size)
+            scaleText = u" maxsize={size}".format(size=size)
         else:
             raise NotImplementedError
 
-        if len (fname) > 0:
-            fileText = u"Attach:{fname}".format (fname=fname)
+        if len(fname) > 0:
+            fileText = u"Attach:{fname}".format(fname=fname)
         else:
             fileText = u""
 
-        result = u"%thumb{scale}%{fname}%%".format (scale=scaleText, fname=fileText)
+        result = u"%thumb{scale}%{fname}%%".format(
+            scale=scaleText, fname=fileText)
         return result
