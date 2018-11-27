64e620a1c7ba2272277b3b845ab6285d31ebd344
Jenyay <jenyay.ilin@gmail.com>
2018-11-27 22:37:38 +0300

    Fix global search page name

--- src/outwiker/actions/globalsearch.py
+++ src/outwiker/actions/globalsearch.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 import wx
 
@@ -13,30 +13,26 @@ class GlobalSearchAction (BaseAction):
     """
     stringId = u"GlobalSearch"
 
-    def __init__ (self, application):
+    def __init__(self, application):
         self._application = application
 
-
     @property
-    def title (self):
+    def title(self):
         return _(u"Global Search…")
 
-
     @property
-    def description (self):
+    def description(self):
         return _(u"Create or open page for global search")
 
-
-    def run (self, params):
-        self._openGlobalSearch ()
-
+    def run(self, params):
+        self._openGlobalSearch()
 
     @testreadonly
-    def _openGlobalSearch (self):
+    def _openGlobalSearch(self):
         if self._application.wikiroot is not None:
             try:
-                GlobalSearch.create (self._application.wikiroot)
+                GlobalSearch.create(self._application.wikiroot)
             except IOError:
-                MessageBox (_(u"Can't create page"),
-                            _(u"Error"),
-                            wx.ICON_ERROR | wx.OK)
+                MessageBox(_(u"Can't create page"),
+                           _(u"Error"),
+                           wx.ICON_ERROR | wx.OK)
--- src/outwiker/pages/search/searchpage.py
+++ src/outwiker/pages/search/searchpage.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 import os.path
 
 from outwiker.core.tree import WikiPage
@@ -18,72 +18,72 @@ class SearchWikiPage (WikiPage):
     """
     Класс HTML-страниц
     """
-    def __init__ (self, path, title, parent, readonly = False):
-        WikiPage.__init__ (self, path, title, parent, readonly)
+
+    def __init__(self, path, title, parent, readonly=False):
+        WikiPage.__init__(self, path, title, parent, readonly)
 
         self.paramsSection = u"Search"
-        phraseOption = StringOption (self.params, self.paramsSection, u"phrase", u"")
+        phraseOption = StringOption(
+            self.params, self.paramsSection, u"phrase", u"")
 
         # Искомая фраза
         self._phrase = phraseOption.value
 
-        # Теги, по которым осуществляется поиск (не путать с тегами, установленными для данной страницы)
+        # Теги, по которым осуществляется поиск
+        # (не путать с тегами, установленными для данной страницы)
         self._searchTags = self._getSearchTags()
 
         # Стратегия для поиска
         self._strategy = self._getStrategy()
 
-
     @staticmethod
-    def getTypeString ():
+    def getTypeString():
         return u"search"
 
-
     @property
-    def phrase (self):
+    def phrase(self):
         return self._phrase
 
-
     @phrase.setter
-    def phrase (self, phrase):
+    def phrase(self, phrase):
         """
         Устанавливает искомую фразу
         """
         self._phrase = phrase
 
-        phraseOption = StringOption (self.params, self.paramsSection, u"phrase", u"")
+        phraseOption = StringOption(
+            self.params, self.paramsSection, u"phrase", u"")
         try:
             phraseOption.value = phrase
         except ReadonlyException:
             # Ничего страшного, если поисковая фраза не сохранится
             pass
 
-        Application.onPageUpdate (self, change=PAGE_UPDATE_CONTENT)
+        Application.onPageUpdate(self, change=PAGE_UPDATE_CONTENT)
 
-
-    def _getSearchTags (self):
+    def _getSearchTags(self):
         """
         Загрузить список тегов из настроек страницы
         """
-        tagsOption = StringOption (self.params, self.paramsSection, u"tags", u"")
-        tags = parseTagsList (tagsOption.value)
+        tagsOption = StringOption(
+            self.params, self.paramsSection, u"tags", u"")
+        tags = parseTagsList(tagsOption.value)
         return tags
 
-
     @property
-    def searchTags (self):
+    def searchTags(self):
         return self._searchTags
 
-
     @searchTags.setter
-    def searchTags (self, tags):
+    def searchTags(self, tags):
         """
         Выбрать теги для поиска
         """
         self._searchTags = tags
-        tags_str = getTagsString (tags)
+        tags_str = getTagsString(tags)
 
-        tagsOption = StringOption (self.params, self.paramsSection, u"tags", u"")
+        tagsOption = StringOption(
+            self.params, self.paramsSection, u"tags", u"")
 
         try:
             tagsOption.value = tags_str
@@ -91,35 +91,33 @@ class SearchWikiPage (WikiPage):
             # Ну не сохранятся искомые теги, ничего страшного
             pass
 
-        Application.onPageUpdate (self, change=PAGE_UPDATE_CONTENT)
+        Application.onPageUpdate(self, change=PAGE_UPDATE_CONTENT)
 
+    def _getStrategy(self):
+        strategyOption = IntegerOption(
+            self.params, self.paramsSection, u"strategy", 0)
+        return self._strategyByCode(strategyOption.value)
 
-    def _getStrategy (self):
-        strategyOption = IntegerOption (self.params, self.paramsSection, u"strategy", 0)
-        return self._strategyByCode (strategyOption.value)
-
-
-    def _strategyByCode (self, code):
+    def _strategyByCode(self, code):
         if code == 0:
             return AnyTagSearchStrategy
         else:
             return AllTagsSearchStrategy
 
-
     @property
-    def strategy (self):
+    def strategy(self):
         return self._strategy
 
-
     @strategy.setter
-    def strategy (self, strategy):
+    def strategy(self, strategy):
         if strategy == AllTagsSearchStrategy:
             strategyCode = 1
         else:
             strategyCode = 0
 
         self._strategy = strategy
-        strategyOption = IntegerOption (self.params, self.paramsSection, u"strategy", 0)
+        strategyOption = IntegerOption(
+            self.params, self.paramsSection, u"strategy", 0)
 
         try:
             strategyOption.value = strategyCode
@@ -127,42 +125,39 @@ class SearchWikiPage (WikiPage):
             # Ничего страшного
             pass
 
-        Application.onPageUpdate (self, change=PAGE_UPDATE_CONTENT)
+        Application.onPageUpdate(self, change=PAGE_UPDATE_CONTENT)
 
 
 class SearchPageFactory (PageFactory):
     """
     Фабрика для создания страниц поиска и их представлений
     """
+
     def getPageType(self):
         return SearchWikiPage
 
-
     @property
-    def title (self):
+    def title(self):
         """
         Название страницы, показываемое пользователю
         """
         return _(u"Search Page")
 
-
-    def getPageView (self, parent, application):
+    def getPageView(self, parent, application):
         """
         Вернуть контрол, который будет отображать и редактировать страницу
         """
-        return SearchPanel (parent, application)
+        return SearchPanel(parent, application)
 
 
 class GlobalSearch (object):
-    # TODO: Remove the call of the "_" function from class initialize
-    pageTitle = (u"# Search")
-
     @staticmethod
-    def create (root, phrase = u"", tags = [], strategy = AllTagsSearchStrategy):
+    def create(root, phrase=u"", tags=[], strategy=AllTagsSearchStrategy):
         """
-        Создать страницу с поиском. Если страница существует, то сделать ее активной
+        Создать страницу с поиском. Если страница существует,
+        то сделать ее активной
         """
-        title = GlobalSearch.pageTitle
+        title = _(u"# Search")
         number = 1
         page = None
 
@@ -171,11 +166,11 @@ class GlobalSearch (object):
         while page is None:
             page = root[title]
             if page is None:
-                page = SearchPageFactory().create (root, title, [])
-                page.icon = os.path.join (imagesDir, "global_search.png")
+                page = SearchPageFactory().create(root, title, [])
+                page.icon = os.path.join(imagesDir, "global_search.png")
             elif page.getTypeString() != SearchWikiPage.getTypeString():
                 number += 1
-                title = u"%s %d" % (GlobalSearch.pageTitle, number)
+                title = u"%s %d" % (title, number)
                 page = None
 
         page.phrase = phrase
