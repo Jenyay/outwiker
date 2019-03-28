6c1e497fc7477c20e6cc3cd6c0ca07f88454fc47
Jenyay <jenyay.ilin@gmail.com>
2019-03-28 23:32:53 +0300

    Improve long process window

--- src/outwiker/gui/controls/progress.py
+++ src/outwiker/gui/controls/progress.py
@@ -7,7 +7,8 @@ class ProgressWindow(wx.Frame):
     def __init__(self, parent: wx.Window,
                  title: str,
                  text: str):
-        super().__init__(parent, style=wx.FRAME_FLOAT_ON_PARENT | wx.FRAME_NO_TASKBAR)
+        super().__init__(parent,
+                         style=wx.FRAME_FLOAT_ON_PARENT | wx.FRAME_NO_TASKBAR)
         self._title = title
         self._text = text
         self._parentTop = wx.GetTopLevelParent(parent)
@@ -26,7 +27,9 @@ class ProgressWindow(wx.Frame):
         self._titleLabel = wx.StaticText(self)
         self._titleLabel.SetLabelMarkup('<b>' + self._title + '</b>')
 
-        self._textLabel = wx.StaticText(self, label=self._text + self._getTextSuffix(self._maxDotCount))
+        self._textLabel = wx.StaticText(
+            self,
+            label=self._text + self._getTextSuffix(self._maxDotCount))
 
         sizer.Add(self._titleLabel,
                   flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER | wx.ALL,
@@ -46,18 +49,18 @@ class ProgressWindow(wx.Frame):
         self._disabler = wx.WindowDisabler(self)
         self.CenterOnParent()
         self.Show()
-        self.Raise()
         self.Restore()
-        self.SetFocus()
+        # self.SetFocus()
         self.UpdatePulse()
+        self.Raise()
 
     def _getTextSuffix(self, updateCount):
         return '.' * (updateCount % (self._maxDotCount + 1))
 
     def UpdatePulse(self):
         self._updateCount += 1
-        self._textLabel.SetLabel(self._text + self._getTextSuffix(self._updateCount))
-        self.Raise()
+        self._textLabel.SetLabel(
+            self._text + self._getTextSuffix(self._updateCount))
         self.Update()
         wx.SafeYield()
 
