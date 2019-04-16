90c033d4733d31eb9413454b8df3e672b82c6fb2
Eugeniy Ilin <jenyay.ilin@gmail.com>
2019-04-16 13:19:42 +0300

    Refactoring of RootPagePanel

--- src/outwiker/gui/rootpagepanel.py
+++ src/outwiker/gui/rootpagepanel.py
@@ -14,7 +14,87 @@ class RootPagePanel(BasePagePanel):
         self._createGUI()
 
     def _createGUI(self):
-        # Path to wiki GUI
+        self._panels = [
+            NotesTreePathPanel(self, self._application),
+            ButtonsPanel(self, self._application)
+        ]
+
+        mainSizer = wx.FlexGridSizer(cols=1)
+        mainSizer.AddGrowableCol(0)
+
+        for panel in self._panels:
+            mainSizer.Add(panel, flag=wx.EXPAND | wx.ALL, border=4)
+
+        self.SetSizer(mainSizer)
+        self.Layout()
+
+    def UpdateView(self, page):
+        for panel in self._panels:
+            panel.updatePanel()
+
+    def Clear(self):
+        for panel in self._panels:
+            panel.clear()
+
+    def Print(self):
+        pass
+
+    def Save(self):
+        pass
+
+    def checkForExternalEditAndSave(self):
+        pass
+
+
+class ButtonsPanel(wx.Panel):
+    '''
+    Panel with buttons
+    '''
+    def __init__(self, parent, application):
+        super().__init__(parent)
+        self._application = application
+        self._createGUI()
+
+    def updatePanel(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def _createGUI(self):
+        buttonsSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._addNewPageButton = wx.Button(self,
+                                           label=_('Create new page...'))
+        self._addNewPageButton.Bind(wx.EVT_BUTTON, handler=self._onNewPage)
+
+        buttonsSizer.Add(self._addNewPageButton,
+                         flag=wx.ALIGN_LEFT | wx.ALL,
+                         border=4)
+
+        self.SetSizer(buttonsSizer)
+
+    def _onNewPage(self, event):
+        actionController = self._application.actionController
+        actionController.getAction(AddChildPageAction.stringId).run(None)
+
+
+class NotesTreePathPanel(wx.Panel):
+    '''
+    Panel with information about path to current notes tree
+    '''
+    def __init__(self, parent, application):
+        super().__init__(parent)
+        self._application = application
+        self._createGUI()
+
+    def updatePanel(self):
+        if self._application.wikiroot is not None:
+            self._pathTextCtrl.SetValue(self._application.wikiroot.path)
+
+    def clear(self):
+        pass
+
+    def _createGUI(self):
         self._pathStaticText = wx.StaticText(
             self,
             label=_('Path to current notes tree'),
@@ -23,14 +103,6 @@ class RootPagePanel(BasePagePanel):
         self._pathTextCtrl = wx.TextCtrl(self,
                                          style=wx.TE_READONLY,
                                          value=self._application.wikiroot.path)
-
-        # Add new note button
-        self._addNewPageButton = wx.Button(self,
-                                           label=_('Create new page...'))
-        self._addNewPageButton.Bind(wx.EVT_BUTTON, handler=self._onNewPage)
-
-        mainSizer = wx.FlexGridSizer(cols=1)
-        mainSizer.AddGrowableCol(0)
 
         pathSizer = wx.FlexGridSizer(cols=2)
         pathSizer.AddGrowableCol(1)
@@ -41,33 +113,4 @@ class RootPagePanel(BasePagePanel):
                       flag=wx.ALIGN_CENTER_VERTICAL | wx.EXPAND | wx.ALL,
                       border=4)
 
-        mainSizer.Add(pathSizer,
-                      flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.ALL,
-                      border=4)
-
-        mainSizer.Add(self._addNewPageButton,
-                      flag=wx.ALIGN_LEFT | wx.ALL,
-                      border=4)
-
-        self.SetSizer(mainSizer)
-        self.Layout()
-
-    def _onNewPage(self, event):
-        actionController = self._application.actionController
-        actionController.getAction(AddChildPageAction.stringId).run(None)
-
-    def UpdateView(self, page):
-        if self._application.wikiroot is not None:
-            self._pathTextCtrl.SetValue(self._application.wikiroot.path)
-
-    def Print(self):
-        pass
-
-    def Save(self):
-        pass
-
-    def Clear(self):
-        pass
-
-    def checkForExternalEditAndSave(self):
-        pass
+        self.SetSizer(pathSizer)
