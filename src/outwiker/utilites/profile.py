38c3c01383dfdbdbcd6aea9d4af03578a4905754
Eugeniy Ilin <jenyay.ilin@gmail.com>
2019-04-18 17:06:39 +0300

    Add logging to profile

--- src/outwiker/utilites/profile.py
+++ src/outwiker/utilites/profile.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import cProfile
+import logging
 import os.path
 
 from outwiker.core.system import getSpecialDirList
+
+logger = logging.getLogger('outwiker.utilites.profile')
 
 
 def profile(prefix):
@@ -13,7 +16,7 @@ def profile(prefix):
         '''
         def new_func(*args, **kwargs):
             profile_dir = getSpecialDirList('.')[-1]
-            profile_name = 'profile_{}{}'.format(prefix, func.__name__)
+            profile_name = '{}{}.profile'.format(prefix, func.__name__)
             profile_path = os.path.join(profile_dir, profile_name)
 
             profile = cProfile.Profile()
@@ -21,6 +24,8 @@ def profile(prefix):
             result = func(*args, **kwargs)
             profile.create_stats()
             profile.dump_stats(profile_path)
+
+            logger.debug('Write a profile file: {}'.format(profile_path))
             return result
 
         return new_func
