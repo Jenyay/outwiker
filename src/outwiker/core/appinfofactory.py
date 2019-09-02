3bcdb524fb6377c55c5d7907d2fba8d9879dc5d5
Eugene Ilin <jenyay.ilin@gmail.com>
2019-09-02 09:38:59 +0300

    Continue convert XmlAppInfo to AppInfo

--- src/outwiker/core/appinfofactory.py
+++ src/outwiker/core/appinfofactory.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .xmlversionparser import XmlAppInfo, DataForLanguage, T
-from .appinfo import AppInfo
+from .appinfo import AppInfo, AuthorInfo, VersionInfo
+from .version import Version
 
 
 class AppInfoFactory:
@@ -33,15 +34,47 @@ class AppInfoFactory:
                        xmlAppInfo: XmlAppInfo,
                        language: str) -> AppInfo:
         app_info_url = xmlAppInfo.app_info_url
+
         app_name = cls.extractDataForLanguage(
             xmlAppInfo.app_name, language, '')
+
         website = cls.extractDataForLanguage(xmlAppInfo.website, language, '')
+
         description = cls.extractDataForLanguage(
             xmlAppInfo.description, language, '')
+
+        author = cls._getAuthor(xmlAppInfo, language)
+        versions = cls._getVersions(xmlAppInfo, language)
 
         result = AppInfo(app_info_url=app_info_url,
                          app_name=app_name,
                          website=website,
-                         description=description
+                         description=description,
+                         author=author,
+                         versions=versions
                          )
         return result
+
+    @classmethod
+    def _getVersions(cls, xmlAppInfo: XmlAppInfo, language: str):
+        versions = []
+        for xmlversion in xmlAppInfo.versions:
+            try:
+                version = Version.parse('{} {}'.format(xmlversion.number, xmlversion.status))
+            except ValueError:
+                continue
+
+            date = xmlversion.date
+            changes = cls.extractDataForLanguage(xmlversion.changes, language, [])[:]
+            downloads = []
+
+            versions.append(VersionInfo(version, date, downloads, changes))
+
+        return versions
+
+    @classmethod
+    def _getAuthor(cls, xmlAppInfo: XmlAppInfo, language: str):
+        author_default = AuthorInfo()
+        author_src = cls.extractDataForLanguage(xmlAppInfo.author, language, author_default)
+        author = AuthorInfo(author_src.name, author_src.email, author_src.website)
+        return author
--- tests/core/test_appinfofactory.py
+++ tests/core/test_appinfofactory.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from outwiker.core.xmlversionparser import XmlAppInfo, DataForLanguage
+from outwiker.core.xmlversionparser import (
+    XmlAppInfo, XmlAuthorInfo, XmlVersionInfo, XmlChangeItem, DataForLanguage)
 from outwiker.core.appinfofactory import AppInfoFactory
+from outwiker.core.version import Version, StatusSet
 
 
 def test_extractDataForLanguage_empty():
     data = DataForLanguage()
-    assert AppInfoFactory.extractDataForLanguage(data, '', 'default') == 'default'
+    assert AppInfoFactory.extractDataForLanguage(
+        data, '', 'default') == 'default'
 
 
 def test_extractDataForLanguage_01():
@@ -25,7 +28,8 @@ def test_extractDataForLanguage_03():
     data = DataForLanguage()
     data.set_for_language('en', 'John')
     data.set_for_language('en_US', 'John Smith')
-    assert AppInfoFactory.extractDataForLanguage(data, 'en_US', '') == 'John Smith'
+    assert AppInfoFactory.extractDataForLanguage(
+        data, 'en_US', '') == 'John Smith'
 
 
 def test_extractDataForLanguage_04():
@@ -190,3 +194,192 @@ def test_fromXmlAppInfo_description_default():
     appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
 
     assert appInfo.description == 'bla-bla-bla'
+
+
+def test_fromXmlAppInfo_author():
+    xmlAppInfo = XmlAppInfo()
+    author_en = XmlAuthorInfo(
+        name='John', email='john@example.com', website='http://example.com')
+    xmlAppInfo.author.set_for_language('en', author_en)
+
+    language = 'en'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert appInfo.author.name == 'John'
+    assert appInfo.author.email == 'john@example.com'
+    assert appInfo.author.website == 'http://example.com'
+
+
+def test_fromXmlAppInfo_author_default():
+    xmlAppInfo = XmlAppInfo()
+
+    author_default = XmlAuthorInfo(
+        name='John',
+        email='john@example.com',
+        website='http://example.com')
+    xmlAppInfo.author.set_for_language('', author_default)
+
+    author_ru = XmlAuthorInfo(
+        name='Джон',
+        email='john_ru@example.com',
+        website='http://example.com/ru')
+    xmlAppInfo.author.set_for_language('ru', author_ru)
+
+    language = 'jp'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert appInfo.author.name == 'John'
+    assert appInfo.author.email == 'john@example.com'
+    assert appInfo.author.website == 'http://example.com'
+
+
+def test_fromXmlAppInfo_author_language_alternative():
+    xmlAppInfo = XmlAppInfo()
+
+    author_en = XmlAuthorInfo(
+        name='John',
+        email='john@example.com',
+        website='http://example.com')
+    xmlAppInfo.author.set_for_language('en', author_en)
+
+    language = 'en_US'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert appInfo.author.name == 'John'
+    assert appInfo.author.email == 'john@example.com'
+    assert appInfo.author.website == 'http://example.com'
+
+
+def test_fromXmlAppInfo_versions_simple():
+    xmlAppInfo = XmlAppInfo()
+    version_1 = XmlVersionInfo(number='1.0', status='dev', date=None)
+    version_2 = XmlVersionInfo(number='2.0', status='beta', date=None)
+
+    xmlAppInfo.versions.append(version_1)
+    xmlAppInfo.versions.append(version_2)
+
+    language = ''
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert len(appInfo.versions) == 2
+
+    assert appInfo.versions[0].version == Version(1, 0, status=StatusSet.DEV)
+    assert appInfo.versions[0].downloads == []
+    assert appInfo.versions[0].changes == []
+
+    assert appInfo.versions[1].version == Version(2, 0, status=StatusSet.BETA)
+    assert appInfo.versions[1].downloads == []
+    assert appInfo.versions[1].changes == []
+
+
+def test_fromXmlAppInfo_versions_invalid_number():
+    xmlAppInfo = XmlAppInfo()
+    version_1 = XmlVersionInfo(number='xxx')
+    version_2 = XmlVersionInfo(number='1.0', status='dev', date=None)
+
+    xmlAppInfo.versions.append(version_1)
+    xmlAppInfo.versions.append(version_2)
+
+    language = ''
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert len(appInfo.versions) == 1
+
+    assert appInfo.versions[0].version == Version(1, 0, status=StatusSet.DEV)
+    assert appInfo.versions[0].downloads == []
+    assert appInfo.versions[0].changes == []
+
+
+def test_fromXmlAppInfo_versions_invalid_status():
+    xmlAppInfo = XmlAppInfo()
+    version_1 = XmlVersionInfo(number='1.0', status='dev', date=None)
+    version_2 = XmlVersionInfo(number='2.0', status='xxx')
+
+    xmlAppInfo.versions.append(version_1)
+    xmlAppInfo.versions.append(version_2)
+
+    language = ''
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert len(appInfo.versions) == 2
+
+    assert appInfo.versions[0].version == Version(1, 0, status=StatusSet.DEV)
+    assert appInfo.versions[0].downloads == []
+    assert appInfo.versions[0].changes == []
+
+    assert appInfo.versions[1].version == Version(2, 0)
+    assert appInfo.versions[1].downloads == []
+    assert appInfo.versions[1].changes == []
+
+
+def test_fromXmlAppInfo_versions_changes():
+    xmlAppInfo = XmlAppInfo()
+    version = XmlVersionInfo(number='1.0', status='dev', date=None)
+
+    changes_en = [XmlChangeItem('Change 1'), XmlChangeItem('Change 2')]
+    version.changes.set_for_language('en', changes_en)
+
+    xmlAppInfo.versions.append(version)
+
+    language = 'en'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert len(appInfo.versions[0].changes) == 2
+    assert appInfo.versions[0].changes[0].description == 'Change 1'
+    assert appInfo.versions[0].changes[1].description == 'Change 2'
+
+
+def test_fromXmlAppInfo_versions_changes_language_default():
+    xmlAppInfo = XmlAppInfo()
+    version = XmlVersionInfo(number='1.0', status='dev', date=None)
+
+    changes_default = [XmlChangeItem('Change 1'), XmlChangeItem('Change 2')]
+    changes_en = [XmlChangeItem('Change 1 En'), XmlChangeItem('Change 2 En')]
+    version.changes.set_for_language('', changes_default)
+    version.changes.set_for_language('en', changes_en)
+
+    xmlAppInfo.versions.append(version)
+
+    language = 'ru'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert len(appInfo.versions[0].changes) == 2
+    assert appInfo.versions[0].changes[0].description == 'Change 1'
+    assert appInfo.versions[0].changes[1].description == 'Change 2'
+
+
+def test_fromXmlAppInfo_versions_changes_language_alternative():
+    xmlAppInfo = XmlAppInfo()
+    version = XmlVersionInfo(number='1.0', status='dev', date=None)
+
+    changes_ru = [XmlChangeItem('Изменение 1'), XmlChangeItem('Изменение 2')]
+    changes_en = [XmlChangeItem('Change 1'), XmlChangeItem('Change 2')]
+    version.changes.set_for_language('ru', changes_ru)
+    version.changes.set_for_language('en', changes_en)
+
+    xmlAppInfo.versions.append(version)
+
+    language = 'ru_RU'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+
+    assert len(appInfo.versions[0].changes) == 2
+    assert appInfo.versions[0].changes[0].description == 'Изменение 1'
+    assert appInfo.versions[0].changes[1].description == 'Изменение 2'
+
+
+def test_fromXmlAppInfo_versions_changes_change_list():
+    xmlAppInfo = XmlAppInfo()
+    version = XmlVersionInfo(number='1.0', status='dev', date=None)
+
+    changes_en = [XmlChangeItem('Change 1'), XmlChangeItem('Change 2')]
+    version.changes.set_for_language('en', changes_en)
+
+    xmlAppInfo.versions.append(version)
+
+    language = 'en'
+    appInfo = AppInfoFactory.fromXmlAppInfo(xmlAppInfo, language)
+    changes_en.append(XmlChangeItem('Change 3'))
+
+    assert len(appInfo.versions[0].changes) == 2
+    assert appInfo.versions[0].changes[0].description == 'Change 1'
+    assert appInfo.versions[0].changes[1].description == 'Change 2'
