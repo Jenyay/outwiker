e2ec8bf65f3338a116a68ddce322da3f7b2f09a7
Eugene Ilin <jenyay.ilin@gmail.com>
2019-09-11 09:32:53 +0300

    Remove Organizer plug-in

--- plugins/organizer/organizer/utils.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-
-def splitTags(tagsStr):
-    """
-    Return list of the tags. Tags must be comma separated with optional spaces.
-    The tags will be in lower case.
-    """
-    return [tag.strip().lower() for tag in tagsStr.split(u',') if tag.strip()]
