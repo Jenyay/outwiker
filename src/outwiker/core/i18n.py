f5588ed16ea78ce359e3fc0655c7b0ba5afd8be8
Jenyay <jenyay.ilin@gmail.com>
2021-04-04 09:58:08 +0300

    Fix i18n

--- src/outwiker/core/i18n.py
+++ src/outwiker/core/i18n.py
@@ -34,6 +34,7 @@ def getDefaultLanguage():
 def init_i18n(language):
     langdir = os.path.join(getCurrentDir(), u'locale')
     lang = loadLanguage(language, langdir, u"outwiker")
+    assert lang is not None
     lang.install()
     return lang
 
@@ -49,9 +50,13 @@ def loadLanguage(language, langdir, domain):
                     if language == AUTO_LANGUAGE
                     else language)
 
-    lang = gettext.translation(domain,
-                               langdir,
-                               languages=[reallanguage, 'en'])
+    try:
+        lang = gettext.translation(domain,
+                                   langdir,
+                                   languages=[reallanguage, 'en'])
+    except IOError:
+        return None
+
     return lang
 
 
