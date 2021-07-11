b102c92dc6e54985d8faa0306bfdc5e90e78cbbf
Eugene Ilin <jenyay.ilin@gmail.com>
2021-07-11 17:56:54 +0300

    Change date format in XML files

--- buildtools/src/buildtools/versionstools/__init__.py
+++ buildtools/src/buildtools/versionstools/__init__.py
@@ -3,6 +3,7 @@
 from .displayversion import display_version
 from .initupdater import InitUpdater
 from .versionsxmlupdater import VersionsXmlUpdater
+from .appdataxml import AppDataXmlUpdater
 
 
-__all__ = [display_version, InitUpdater, VersionsXmlUpdater]
+__all__ = [display_version, InitUpdater, VersionsXmlUpdater, AppDataXmlUpdater]
