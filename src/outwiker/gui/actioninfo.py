50530cf7b3f71480bb075b2829bc0669773b835b
Eugene Ilin <jenyay.ilin@gmail.com>
2021-07-27 10:07:40 +0300

    Replace namedtuples in actioninfo.py by classes
    Old Python version (used in snap package) does not supportes 'defaults' parameter

--- src/outwiker/gui/actioninfo.py
+++ src/outwiker/gui/actioninfo.py
@@ -1,12 +1,30 @@
 # -*- coding=utf-8 -*-
 
-from collections import namedtuple
+# Python 3.6 (?) does not support defaults parameter
+# Old Python version used in a snap package
+# from collections import namedtuple
 
 
-ActionInfo = namedtuple('ActionInfo',
-                        ['action_type', 'hotkey', 'area', 'hidden'],
-                        defaults=[None, None, False])
+# ActionInfo = namedtuple('ActionInfo',
+                        # ['action_type', 'hotkey', 'area', 'hidden'],
+                        # defaults=[None, None, False])
 
-PolyactionInfo = namedtuple('PolyactionInfo',
-                            ['stringId', 'title', 'description', 'hotkey'],
-                            defaults=[None])
+# PolyactionInfo = namedtuple('PolyactionInfo',
+                            # ['stringId', 'title', 'description', 'hotkey'],
+                            # defaults=[None])
+
+
+class ActionInfo:
+    def __init__(self, action_type, hotkey=None, area=None, hidden=None):
+        self.action_type = action_type
+        self.hotkey = hotkey
+        self.area = area
+        self.hidden = hidden
+
+
+class PolyactionInfo:
+    def __init__(self, stringId, title, description, hotkey=None):
+        self.stringId = stringId
+        self.title = title
+        self.description = description
+        self.hotkey = hotkey
