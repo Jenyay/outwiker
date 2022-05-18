95ad60caca0dbf2c6761bb60e2f1456bd81dc809
Eugene Ilin <jenyay.ilin@gmail.com>
2022-05-18 10:11:15 +0300

    Fix iconmaker

--- src/outwiker/core/iconmaker.py
+++ src/outwiker/core/iconmaker.py
@@ -12,23 +12,22 @@ class IconMaker:
         """ Create icon by file fname_in. Result will have saved as fname_out.
         """
         img_new = Image.new('RGBA', (ICON_WIDTH, ICON_HEIGHT))
-        img_src = Image.open(fname_in)
+        with Image.open(fname_in) as img_src:
+            # Resize source imaga, if it is required
+            width_src, height_src = img_src.size
+            scale = max(float(width_src) / float(ICON_WIDTH), float(height_src) /
+                        float(ICON_HEIGHT))
+            if scale > 1:
+                img_src = img_src.resize((int(width_src / scale),
+                                          int(height_src / scale)),
+                                         Image.Resampling.LANCZOS)
 
-        # Resize source imaga, if it is required
-        width_src, height_src = img_src.size
-        scale = max(float(width_src) / float(ICON_WIDTH), float(height_src) /
-                    float(ICON_HEIGHT))
-        if scale > 1:
-            img_src = img_src.resize((int(width_src / scale),
-                                      int(height_src / scale)),
-                                     Image.Resampling.LANCZOS)
+            # Paste source image to result image
+            dx = int((ICON_WIDTH - img_src.size[0]) / 2.0)
+            dy = int((ICON_HEIGHT - img_src.size[1]) / 2.0)
 
-        # Paste source image to result image
-        dx = int((ICON_WIDTH - img_src.size[0]) / 2.0)
-        dy = int((ICON_HEIGHT - img_src.size[1]) / 2.0)
+            assert dx >= 0 and dx < ICON_WIDTH
+            assert dy >= 0 and dy < ICON_HEIGHT
 
-        assert dx >= 0 and dx < ICON_WIDTH
-        assert dy >= 0 and dy < ICON_HEIGHT
-
-        img_new.paste(img_src, (dx, dy))
-        img_new.save(fname_out)
+            img_new.paste(img_src, (dx, dy))
+            img_new.save(fname_out)
