c789dfad20b620559ae6cf1e47a47c3aecfcc9b5
Eugeniy Ilin <jenyay.ilin@gmail.com>
2022-05-20 12:51:40 +0300

    Colorize link to attachment on wiki pages

--- src/outwiker/pages/wiki/wikiconfig.py
+++ src/outwiker/pages/wiki/wikiconfig.py
@@ -7,49 +7,52 @@ from outwiker.core.config import (BooleanOption,
 from outwiker.gui.stcstyle import StcStyle
 
 
-class WikiConfig (object):
+class WikiConfig:
     """
     Класс, хранящий указатели на настройки, связанные с викиы
     """
     # Секция конфига для параметров, связанных с викистраницей
-    WIKI_SECTION = u"Wiki"
+    WIKI_SECTION = "Wiki"
 
     # Секция, куда записывать параметры стилей оформления редактора
-    STYLES_SECTION = u"EditorStyles"
+    STYLES_SECTION = "EditorStyles"
 
     # Имя параметра "Показывать ли код HTML?"
-    SHOW_HTML_CODE_PARAM = u"ShowHtmlCode"
+    SHOW_HTML_CODE_PARAM = "ShowHtmlCode"
 
     # Имя параметра для размера превьюшек по умолчанию
-    THUMB_SIZE_PARAM = u"ThumbSize"
+    THUMB_SIZE_PARAM = "ThumbSize"
 
     # Имя параметра, показывающего, надо ли выводить список
     # прикрепленных файлов вместо пустой страницы
-    SHOW_ATTACH_BLANK_PARAM = u"ShowAttachInsteadBlank"
+    SHOW_ATTACH_BLANK_PARAM = "ShowAttachInsteadBlank"
 
     # Размер превьюшек по умолчанию
     THUMB_SIZE_DEFAULT = 250
 
     # Имя параметра "Стиль ссылок по умолчанию"
-    LINK_STYLE_PARAM = u"DefaultLinkStyle"
+    LINK_STYLE_PARAM = "DefaultLinkStyle"
 
     # Стиль ссылок по умолчанию ([[... -> ...]] или [[... | ...]])
     LINK_STYLE_DEFAULT = 0
 
     # Стили редактора
-    STYLE_LINK_PARAM = u"link"
-    STYLE_LINK_DEFAULT = StcStyle.parse(u"fore:#0000FF,underline")
+    STYLE_LINK_PARAM = "link"
+    STYLE_LINK_DEFAULT = StcStyle.parse("fore:#0000FF,underline")
 
-    STYLE_HEADING_PARAM = u"heading"
-    STYLE_HEADING_DEFAULT = StcStyle.parse(u"bold")
+    STYLE_HEADING_PARAM = "heading"
+    STYLE_HEADING_DEFAULT = StcStyle.parse("bold")
 
-    STYLE_COMMAND_PARAM = u"command"
-    STYLE_COMMAND_DEFAULT = StcStyle.parse(u"fore:#6A686B")
+    STYLE_COMMAND_PARAM = "command"
+    STYLE_COMMAND_DEFAULT = StcStyle.parse("fore:#6A686B")
 
-    STYLE_COMMENT_PARAM = u"comment"
-    STYLE_COMMENT_DEFAULT = StcStyle.parse(u"fore:#12B535")
+    STYLE_COMMENT_PARAM = "comment"
+    STYLE_COMMENT_DEFAULT = StcStyle.parse("fore:#12B535")
 
-    COLORIZE_SYNTAX_PARAM = u'ColorizeSyntax'
+    STYLE_ATTACHMENT_PARAM = "attachment"
+    STYLE_ATTACHMENT_DEFAULT = StcStyle.parse("fore:#5b81c9,underline")
+
+    COLORIZE_SYNTAX_PARAM = 'ColorizeSyntax'
     COLORIZE_SYNTAX_DEFAULT = True
 
     RECENT_STYLE_NAME_PARAM = 'RecentStyleName'
@@ -104,6 +107,11 @@ class WikiConfig (object):
                                       WikiConfig.STYLES_SECTION,
                                       WikiConfig.STYLE_COMMENT_PARAM,
                                       WikiConfig.STYLE_COMMENT_DEFAULT)
+
+        self.attachment = StcStyleOption(self.config,
+                                      WikiConfig.STYLES_SECTION,
+                                      WikiConfig.STYLE_ATTACHMENT_PARAM,
+                                      WikiConfig.STYLE_ATTACHMENT_DEFAULT)
 
         self.colorizeSyntax = BooleanOption(self.config,
                                             self.WIKI_SECTION,
