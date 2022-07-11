6640ee7762170bd10f88dace9285c00886d4c10d
Eugeniy Ilin <jenyay.ilin@gmail.com>
2022-07-11 16:25:44 +0300

    Thumbdialog refactoring

--- src/outwiker/pages/wiki/thumbdialog.py
+++ src/outwiker/pages/wiki/thumbdialog.py
@@ -2,8 +2,10 @@
 
 import wx
 
+from outwiker.gui.testeddialog import TestedDialog
 
-class ThumbDialog (wx.Dialog):
+
+class ThumbDialog(TestedDialog):
     WIDTH = 0
     HEIGHT = 1
     MAX_SIZE = 2
@@ -15,9 +17,11 @@ class ThumbDialog (wx.Dialog):
         selectedFile - файл выбранный по умолчанию. Если selectedFile == None,
             никакой файл по умолчанию не выбирается
         """
-        super().__init__(parent,
-                         style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER,
-                         title=_("Thumbnails"))
+        super().__init__(
+            parent,
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER,
+            title=_("Thumbnails"),
+        )
 
         self.__filesList = filesList
         self.__selectedFile = selectedFile
@@ -39,11 +43,15 @@ class ThumbDialog (wx.Dialog):
         return self.sizeCtrl.GetValue()
 
     def __createGui(self):
-        # Элементы для выбор имени файла
-        filenameLabel = wx.StaticText(self, label=_(u"File name"))
-        self.filesListCombo = wx.ComboBox(self,
-                                          choices=self.__filesList,
-                                          style=wx.CB_READONLY)
+        # Controls for file name selection
+        filenameLabel = wx.StaticText(self, label=_("Select attached image"))
+        font = filenameLabel.GetFont()
+        font.SetWeight(wx.FONTWEIGHT_BOLD)
+        filenameLabel.SetFont(font)
+
+        self.filesListCombo = wx.ComboBox(
+            self, choices=self.__filesList, style=wx.CB_READONLY
+        )
         self.filesListCombo.SetSelection(0)
         self.filesListCombo.SetMinSize((250, -1))
 
@@ -51,47 +59,52 @@ class ThumbDialog (wx.Dialog):
             assert self.__selectedFile in self.__filesList
             self.filesListCombo.SetStringSelection(self.__selectedFile)
 
-        # Элементы для выбора размера
-        scaleLabel = wx.StaticText(self, label=_(u"Thumbnail size"))
+        # Controls for thumbnail size selection
+        scaleLabel = wx.StaticText(self, label=_("Thumbnail size"))
 
-        scaleItems = [_(u"Width"), _(u"Height"), _(u"Max size")]
-        self.scaleCombo = wx.ComboBox(self,
-                                      choices=scaleItems,
-                                      style=wx.CB_READONLY)
+        scaleItems = [_("Width"), _("Height"), _("Max size")]
+        self.scaleCombo = wx.ComboBox(self, choices=scaleItems, style=wx.CB_READONLY)
         self.scaleCombo.SetSelection(0)
         self.scaleCombo.SetMinSize((250, -1))
 
         self.sizeCtrl = wx.SpinCtrl(self, min=0, max=10000, initial=0)
         self.sizeCtrl.SetMinSize((100, -1))
-        sizeLabel = wx.StaticText(self, label=_(u"0 - default size"))
+        sizeLabel = wx.StaticText(self, label=_("0 - default size"))
 
         okCancel = self.CreateButtonSizer(wx.OK | wx.CANCEL)
 
-        # Расстановка элементов
-        mainSizer = wx.FlexGridSizer(rows=0, cols=2, vgap=0, hgap=0)
+        # Layout controls
+        mainSizer = wx.FlexGridSizer(cols=1)
         mainSizer.AddGrowableCol(0)
-        mainSizer.AddGrowableCol(1)
-        mainSizer.AddGrowableRow(4)
+        mainSizer.AddGrowableRow(5)
 
-        mainSizer.Add(filenameLabel, 0, flag=wx.ALL |
-                      wx.ALIGN_CENTER_VERTICAL, border=4)
-        mainSizer.Add(self.filesListCombo, 0, flag=wx.ALL |
-                      wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, border=4)
+        mainSizer.Add(filenameLabel, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=4)
+        mainSizer.Add(
+            self.filesListCombo,
+            flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.EXPAND,
+            border=4,
+        )
 
-        mainSizer.Add(scaleLabel, 0, flag=wx.ALL |
-                      wx.ALIGN_CENTER_VERTICAL, border=4)
-        mainSizer.AddStretchSpacer()
+        mainSizer.Add(scaleLabel, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=4)
 
-        mainSizer.Add(self.scaleCombo, 0, flag=wx.ALL |
-                      wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, border=4)
-        mainSizer.Add(self.sizeCtrl, 0, flag=wx.ALL | wx.EXPAND, border=4)
+        # Scale controls
+        scaleSizer = wx.FlexGridSizer(cols=2)
+        scaleSizer.AddGrowableCol(0)
+        scaleSizer.AddGrowableCol(1)
 
-        mainSizer.AddStretchSpacer()
-        mainSizer.Add(sizeLabel, 0, flag=wx.ALL | wx.EXPAND, border=4)
+        scaleSizer.Add(
+            self.scaleCombo,
+            flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.EXPAND,
+            border=4,
+        )
+        scaleSizer.Add(self.sizeCtrl, flag=wx.ALL | wx.EXPAND, border=4)
+        mainSizer.Add(scaleSizer, flag=wx.ALL | wx.EXPAND, border=2)
 
-        mainSizer.AddStretchSpacer()
-        mainSizer.Add(okCancel, 0, flag=wx.ALL |
-                      wx.ALIGN_RIGHT | wx.ALIGN_BOTTOM, border=4)
+        mainSizer.Add(sizeLabel, flag=wx.ALL | wx.ALIGN_RIGHT, border=4)
+
+        mainSizer.Add(
+            okCancel, flag=wx.ALL | wx.ALIGN_RIGHT | wx.ALIGN_BOTTOM, border=4
+        )
 
         self.SetSizer(mainSizer)
         self.Fit()
