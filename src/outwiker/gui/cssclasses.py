9211dc44d7d21caf39dc729b7c8fe1282b724603
Eugene Ilin <jenyay.ilin@gmail.com>
2022-11-05 17:05:02 +0300

    Add CSS styles for attachlist items

--- src/outwiker/gui/cssclasses.py
+++ src/outwiker/gui/cssclasses.py
@@ -3,4 +3,6 @@
 CSS_ERROR = 'ow-error'
 CSS_IMAGE = 'ow-image'
 CSS_ATTACH = 'ow-attach'
+CSS_ATTACH_FILE = 'ow-attach-file'
+CSS_ATTACH_DIR = 'ow-attach-dir'
 CSS_ATTACH_LIST = 'ow-attach-list'
--- src/outwiker/pages/wiki/parser/commands/attachlist.py
+++ src/outwiker/pages/wiki/parser/commands/attachlist.py
@@ -7,7 +7,7 @@ from typing import List, Tuple
 from outwiker.core.attachment import Attachment
 from outwiker.core.defines import PAGE_ATTACH_DIR
 from outwiker.pages.wiki.parser.command import Command
-from outwiker.gui.cssclasses import CSS_ATTACH, CSS_ATTACH_LIST
+import outwiker.gui.cssclasses as css
 
 
 class SimpleView:
@@ -15,33 +15,86 @@ class SimpleView:
     Класс для простого представления списка прикрепленных файлов - каждая страница на отдельной строке
     """
     def __init__(self):
-        self._item_template = '<a class="{css_class}" href="{link}">{title}</a>\n'    
-        self._list_template = '<div class="{css_class}">{content}</div>'
+        self._item_file_css_class = '{} {}'.format(css.CSS_ATTACH, css.CSS_ATTACH_FILE)
+        self._item_dir_css_class = '{} {}'.format(css.CSS_ATTACH, css.CSS_ATTACH_DIR)
 
-    def make(self, fnames, attach_path, subdir):
+        self._item_template = '<li class="{css_class}"><a href="{link}">{title}</a></li>'    
+        self._list_template = '<ul class="{css_class}">{content}</ul>'
+
+    def make(self, dirnames, fnames,  subdir):
         """
         fnames - имена файлов, которые нужно вывести (относительный путь)
         attach_path - путь до прикрепленных файлов (полный)
         """
-        content = ''.join([self._get_link(fname, attach_path, subdir) for fname in fnames]).rstrip()
-        return self._list_template.format(css_class=CSS_ATTACH_LIST, content=content)
+        content_items = [self._get_item_dir(name, subdir) for name in dirnames]
+        content_items += [self._get_item_file(name, subdir) for name in fnames]
+        content = ''.join(content_items)
 
-    def _get_link(self, fname: str, attach_path: str, subdir: str) -> str:
-        if os.path.isdir(os.path.join(attach_path, subdir, fname)):
-            title = self._get_dir_item(fname)
-        else:
-            title = self._get_file_item(fname) 
+        return self._list_template.format(css_class=css.CSS_ATTACH_LIST, content=content)
 
-        return self._item_template.format(link=self._get_attach_path(subdir, fname), title=title, css_class=CSS_ATTACH)
+    def _get_item_dir(self, name: str, subdir: str) -> str:
+        link = self._get_attach_path(subdir, name)
+        return self._item_template.format(link=link, title=name, css_class=self._item_dir_css_class)
 
-    def _get_dir_item(self, dirname: str) -> str:
-        return "[{}]".format(dirname)
-
-    def _get_file_item(self, fname: str) -> str:
-        return fname
+    def _get_item_file(self, name: str, subdir: str) -> str:
+        link = self._get_attach_path(subdir, name)
+        return self._item_template.format(link=link, title=name, css_class=self._item_file_css_class)
 
     def _get_attach_path(self, subdir: str, fname: str) -> str:
         return os.path.join(PAGE_ATTACH_DIR, subdir, fname).replace("\\", "/")
+
+    def get_css_styles(self) -> str:
+        return '''<style>
+.ow-attach-list ul {
+  margin-left: 10px;
+  padding-left: 20px;
+  border-left: 1px dashed #ddd;
+}
+
+.ow-attach-list li {
+  list-style: none;
+  font-style: italic;
+  font-weight: normal;
+}
+
+.ow-attach-list a {
+  border-bottom: 1px solid transparent;
+  text-decoration: none;
+  transition: all 0.2s ease;
+}
+
+.ow-attach-list a:hover {
+  border-color: #eee;
+  color: #000;
+}
+
+.ow-attach-list .ow-attach-dir,
+.ow-attach-list .ow-attach-dir > a {
+  font-weight: bold;
+  font-style: normal;
+}
+
+.ow-attach-list li:before {
+  margin-right: 10px;
+  content: "";
+  height: 20px;
+  vertical-align: middle;
+  width: 20px;
+  background-repeat: no-repeat;
+  display: inline-block;
+  /* file icon by default */
+  background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill='lightgrey' d='M85.714,42.857V87.5c0,1.487-0.521,2.752-1.562,3.794c-1.042,1.041-2.308,1.562-3.795,1.562H19.643 c-1.488,0-2.753-0.521-3.794-1.562c-1.042-1.042-1.562-2.307-1.562-3.794v-75c0-1.487,0.521-2.752,1.562-3.794 c1.041-1.041,2.306-1.562,3.794-1.562H50V37.5c0,1.488,0.521,2.753,1.562,3.795s2.307,1.562,3.795,1.562H85.714z M85.546,35.714 H57.143V7.311c3.05,0.558,5.505,1.767,7.366,3.627l17.41,17.411C83.78,30.209,84.989,32.665,85.546,35.714z' /></svg>");
+  background-position: center 2px;
+  background-size: 60% auto;
+}
+
+.ow-attach-list li.ow-attach-dir:before {
+  /* folder icon if folder class is specified */
+  background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill='lightblue' d='M96.429,37.5v39.286c0,3.423-1.228,6.361-3.684,8.817c-2.455,2.455-5.395,3.683-8.816,3.683H16.071 c-3.423,0-6.362-1.228-8.817-3.683c-2.456-2.456-3.683-5.395-3.683-8.817V23.214c0-3.422,1.228-6.362,3.683-8.817 c2.455-2.456,5.394-3.683,8.817-3.683h17.857c3.422,0,6.362,1.228,8.817,3.683c2.455,2.455,3.683,5.395,3.683,8.817V25h37.5 c3.422,0,6.361,1.228,8.816,3.683C95.201,31.138,96.429,34.078,96.429,37.5z' /></svg>");
+  background-position: center top;
+  background-size: 75% auto;
+}
+</style>'''
 
 
 class AttachListCommand(Command):
@@ -61,6 +114,7 @@ class AttachListCommand(Command):
         super().__init__(parser)
         self.PARAM_SORT = 'sort'
         self.PARAM_SUBDIR = 'subdir'
+        self._append_header = False
 
     @property
     def name(self):
@@ -81,13 +135,17 @@ class AttachListCommand(Command):
         self._sortFiles(files, params_dict)
 
         view = SimpleView()
-        return view.make(dirs + files, attachpath, subdir)
+        if not self._append_header:
+            self.parser.appendToHead(view.get_css_styles())
+            self._append_header = True
+
+        return view.make(dirs, files, subdir)
 
     def separateDirFiles(self, attachlist: List[str], attachpath: Path) -> Tuple[List[str], List[str]]:
         """
         Разделить файлы и директории, заодно отбросить директории, начинающиеся с "__"
         """
-        dirs = list(filter(lambda name: Path(attachpath, name).is_dir(), attachlist))
+        dirs = list(filter(lambda name: Path(attachpath, name).is_dir() and not name.startswith('__'), attachlist))
         files = list(filter(lambda name: not Path(attachpath, name).is_dir(), attachlist))
 
         return (dirs, files)
