73c8479383db3ecc7aca19a6787dacfdaa8f73f9
Eugeniy Ilin <jenyay.ilin@gmail.com>
2022-11-10 16:07:58 +0300

    Add ow-quote CSS class

--- src/outwiker/pages/wiki/parser/tokenquote.py
+++ src/outwiker/pages/wiki/parser/tokenquote.py
@@ -3,6 +3,7 @@
 from .tokenblock import SimpleNestedBlock
 from .tokenlinebreak import LineBreakToken
 
+import outwiker.core.cssclasses as css
 
 class QuoteFactory:
     """
@@ -16,7 +17,7 @@ class QuoteFactory:
 class QuoteToken(SimpleNestedBlock):
     start = '[>'
     end = '<]'
-    start_html = '<blockquote>'
+    start_html = '<blockquote class="{css_class}">'.format(css_class=css.CSS_QUOTE)
     end_html = '</blockquote>'
     name = 'quote'
     ignore = LineBreakToken().getToken()
