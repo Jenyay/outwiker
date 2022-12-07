b6e0bc2548db7bccc62779cd0da6d59445a37efd
Eugene Ilin <jenyay.ilin@gmail.com>
2022-12-07 22:31:45 +0300

    Clear StatusBarController

--- src/outwiker/gui/statusbar.py
+++ src/outwiker/gui/statusbar.py
@@ -2,14 +2,12 @@
 
 from dataclasses import dataclass
 from typing import List, Optional
-import datetime
 
 import wx
 
 from outwiker.core.application import ApplicationParams
-from outwiker.gui.defines import (STATUSBAR_MESSAGE_ITEM,
-                                  STATUSBAR_PAGE_DATETIME_ITEM)
-from .guiconfig import MainWindowConfig, GeneralGuiConfig
+from outwiker.gui.defines import STATUSBAR_MESSAGE_ITEM
+from outwiker.gui.guiconfig import MainWindowConfig
 
 
 @dataclass(slots=True)
@@ -25,16 +23,10 @@ class StatusBarController:
                  application: ApplicationParams):
         self._statusbar = statusbar
         self._application = application
-
         self._mainWindowConfig = MainWindowConfig(self._application.config)
-        self._generalConfig = GeneralGuiConfig(self._application.config)
-        datetime_width = self._mainWindowConfig.datetimeStatusWidth.value
 
         self._statusbar_items: List[StatusBarItemInfo] = []
-
         self.addItem(STATUSBAR_MESSAGE_ITEM)
-        self.addItem(STATUSBAR_PAGE_DATETIME_ITEM, datetime_width)
-
         self._bindEvents()
 
     def addItem(self, name: str, width: int = -1, position: Optional[int] = None) -> None:
@@ -64,17 +56,9 @@ class StatusBarController:
         self._unbindEvents()
 
     def _bindEvents(self):
-        self._application.onTreeUpdate += self._onTreeUpdate
-        self._application.onPageUpdate += self._onPageUpdate
-        self._application.onWikiOpen += self._onWikiOpen
-        self._application.onPageSelect += self._onPageSelect
         self._application.onPreferencesDialogClose += self._onPreferencesDialogClose
 
     def _unbindEvents(self):
-        self._application.onTreeUpdate -= self._onTreeUpdate
-        self._application.onPageUpdate -= self._onPageUpdate
-        self._application.onWikiOpen -= self._onWikiOpen
-        self._application.onPageSelect -= self._onPageSelect
         self._application.onPreferencesDialogClose -= self._onPreferencesDialogClose
 
     def _initStatusBar(self):
@@ -82,35 +66,10 @@ class StatusBarController:
         self._statusbar.SetFieldsCount(len(widths), widths)
         self.updateStatusBar()
 
-    def _onTreeUpdate(self, _sender):
-        self._updatePageDateTime()
-
-    def _onPageUpdate(self, _page, **_kwargs):
-        self._updatePageDateTime()
-
-    def _onWikiOpen(self, _wikiroot):
-        self._updatePageDateTime()
-
-    def _onPageSelect(self, _newpage):
-        self._updatePageDateTime()
-
-    def _updatePageDateTime(self):
-        dateFormat = self._generalConfig.dateTimeFormat.value
-        text = ''
-
-        if(self._application.selectedPage is not None and
-                self._application.selectedPage.datetime is not None):
-            text = datetime.datetime.strftime(
-                self._application.selectedPage.datetime,
-                dateFormat)
-
-        self.setStatusText(STATUSBAR_PAGE_DATETIME_ITEM, text)
-
     def _onPreferencesDialogClose(self, _prefDialog):
         """
         Обработчик события изменения настроек главного окна
         """
-        self._updatePageDateTime()
         self.updateStatusBar()
 
     def updateStatusBar(self):
