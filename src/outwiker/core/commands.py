b2e8dc936b713a590c5d5278566618b3e15334bd
Eugene Ilin <jenyay.ilin@gmail.com>
2023-04-05 09:30:52 +0300

    Fix commands

--- src/outwiker/core/commands.py
+++ src/outwiker/core/commands.py
@@ -3,9 +3,9 @@
 # TODO: Remove in next version
 from outwiker.api.core.images import isImage
 from outwiker.api.core.tree import testreadonly
-from outwiker.api.services.attachment import attachFiles
-from outwiker.api.services.clipboard import copyTextToClipboard, getClipboardText
-from outwiker.api.services.texteditor import insertCurrentDate
-from outwiker.api.services.tree import openWiki
+from outwiker.api.app.attachment import attachFiles
+from outwiker.api.app.clipboard import copyTextToClipboard, getClipboardText
+from outwiker.api.app.texteditor import insertCurrentDate
+from outwiker.api.app.tree import openWiki
 from outwiker.api.gui.dialogs import MessageBox
-from outwiker.api.services.tree import testPageTitle, renamePage
+from outwiker.api.app.tree import testPageTitle, renamePage
