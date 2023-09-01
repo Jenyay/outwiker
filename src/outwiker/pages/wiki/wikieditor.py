c640e9783a79683d12a9aecbfe7b68ffec575033
Eugeniy Ilin <jenyay.ilin@gmail.com>
2023-09-01 16:13:27 +0300

    Improve lists autocomplete on wiki pages

--- src/outwiker/pages/wiki/wikieditor.py
+++ src/outwiker/pages/wiki/wikieditor.py
@@ -31,30 +31,32 @@ class WikiEditor(TextEditor):
         self.STYLE_THUMBNAIL_ID = (1 << 4) + 4
 
         # Комбинации стилей
-        self.STYLE_BOLD_ITALIC_UNDERLINE_ID = (self.STYLE_BOLD_ID |
-                                               self.STYLE_ITALIC_ID |
-                                               self.STYLE_UNDERLINE_ID)
+        self.STYLE_BOLD_ITALIC_UNDERLINE_ID = (
+            self.STYLE_BOLD_ID | self.STYLE_ITALIC_ID | self.STYLE_UNDERLINE_ID
+        )
 
         self.STYLE_BOLD_ITALIC_ID = self.STYLE_BOLD_ID | self.STYLE_ITALIC_ID
         self.STYLE_BOLD_UNDERLINE_ID = self.STYLE_BOLD_ID | self.STYLE_UNDERLINE_ID
         self.STYLE_ITALIC_UNDERLINE_ID = self.STYLE_ITALIC_ID | self.STYLE_UNDERLINE_ID
 
-        self.STYLE_LINK_BOLD_ITALIC_UNDERLINE_ID = (self.STYLE_BOLD_ID |
-                                                    self.STYLE_ITALIC_ID |
-                                                    self.STYLE_UNDERLINE_ID |
-                                                    self.STYLE_LINK_ID)
+        self.STYLE_LINK_BOLD_ITALIC_UNDERLINE_ID = (
+            self.STYLE_BOLD_ID
+            | self.STYLE_ITALIC_ID
+            | self.STYLE_UNDERLINE_ID
+            | self.STYLE_LINK_ID
+        )
 
-        self.STYLE_LINK_ITALIC_UNDERLINE_ID = (self.STYLE_ITALIC_ID |
-                                               self.STYLE_UNDERLINE_ID |
-                                               self.STYLE_LINK_ID)
+        self.STYLE_LINK_ITALIC_UNDERLINE_ID = (
+            self.STYLE_ITALIC_ID | self.STYLE_UNDERLINE_ID | self.STYLE_LINK_ID
+        )
 
-        self.STYLE_LINK_BOLD_UNDERLINE_ID = (self.STYLE_BOLD_ID |
-                                             self.STYLE_UNDERLINE_ID |
-                                             self.STYLE_LINK_ID)
+        self.STYLE_LINK_BOLD_UNDERLINE_ID = (
+            self.STYLE_BOLD_ID | self.STYLE_UNDERLINE_ID | self.STYLE_LINK_ID
+        )
 
-        self.STYLE_LINK_BOLD_ITALIC_ID = (self.STYLE_BOLD_ID |
-                                          self.STYLE_ITALIC_ID |
-                                          self.STYLE_LINK_ID)
+        self.STYLE_LINK_BOLD_ITALIC_ID = (
+            self.STYLE_BOLD_ID | self.STYLE_ITALIC_ID | self.STYLE_LINK_ID
+        )
 
         self.STYLE_LINK_ITALIC_ID = self.STYLE_ITALIC_ID | self.STYLE_LINK_ID
         self.STYLE_LINK_UNDERLINE_ID = self.STYLE_UNDERLINE_ID | self.STYLE_LINK_ID
@@ -69,15 +71,27 @@ class WikiEditor(TextEditor):
         self._styles[self.STYLE_BOLD_UNDERLINE_ID] = "bold,underline"
         self._styles[self.STYLE_ITALIC_UNDERLINE_ID] = "italic,underline"
         self._styles[self.STYLE_LINK_ID] = config.link.value.tostr()
-        self._styles[self.STYLE_LINK_BOLD_ITALIC_UNDERLINE_ID] = self._styles[self.STYLE_LINK_ID] + \
-            ",bold,italic,underline"
-        self._styles[self.STYLE_LINK_ITALIC_UNDERLINE_ID] = self._styles[self.STYLE_LINK_ID] + \
-            ",italic,underline"
-        self._styles[self.STYLE_LINK_BOLD_UNDERLINE_ID] = self._styles[self.STYLE_LINK_ID] + ",bold,underline"
-        self._styles[self.STYLE_LINK_UNDERLINE_ID] = self._styles[self.STYLE_LINK_ID] + ",underline"
-        self._styles[self.STYLE_LINK_BOLD_ITALIC_ID] = self._styles[self.STYLE_LINK_ID] + ",bold,italic"
-        self._styles[self.STYLE_LINK_ITALIC_ID] = self._styles[self.STYLE_LINK_ID] + ",italic"
-        self._styles[self.STYLE_LINK_BOLD_ID] = self._styles[self.STYLE_LINK_ID] + ",bold"
+        self._styles[self.STYLE_LINK_BOLD_ITALIC_UNDERLINE_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",bold,italic,underline"
+        )
+        self._styles[self.STYLE_LINK_ITALIC_UNDERLINE_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",italic,underline"
+        )
+        self._styles[self.STYLE_LINK_BOLD_UNDERLINE_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",bold,underline"
+        )
+        self._styles[self.STYLE_LINK_UNDERLINE_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",underline"
+        )
+        self._styles[self.STYLE_LINK_BOLD_ITALIC_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",bold,italic"
+        )
+        self._styles[self.STYLE_LINK_ITALIC_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",italic"
+        )
+        self._styles[self.STYLE_LINK_BOLD_ID] = (
+            self._styles[self.STYLE_LINK_ID] + ",bold"
+        )
         self._styles[self.STYLE_HEADING_ID] = config.heading.value.tostr()
         self._styles[self.STYLE_COMMAND_ID] = config.command.value.tostr()
         self._styles[self.STYLE_COMMENT_ID] = config.comment.value.tostr()
@@ -94,26 +108,27 @@ class WikiEditor(TextEditor):
 
         self.textCtrl.SetLexer(wx.stc.STC_LEX_CONTAINER)
         self.textCtrl.SetModEventMask(
-            wx.stc.STC_MOD_INSERTTEXT | wx.stc.STC_MOD_DELETETEXT)
+            wx.stc.STC_MOD_INSERTTEXT | wx.stc.STC_MOD_DELETETEXT
+        )
 
-        for (styleid, style) in self._styles.items():
+        for styleid, style in self._styles.items():
             self.textCtrl.StyleSetSpec(styleid, style)
             self.textCtrl.StyleSetSize(styleid, self.config.fontSize.value)
             self.textCtrl.StyleSetFaceName(styleid, self.config.fontName.value)
-            self.textCtrl.StyleSetBackground(
-                styleid, self.config.backColor.value)
+            self.textCtrl.StyleSetBackground(styleid, self.config.backColor.value)
 
         self.textCtrl.StyleSetSpec(
-            self.STYLE_HEADING_ID, self._styles[self.STYLE_HEADING_ID])
+            self.STYLE_HEADING_ID, self._styles[self.STYLE_HEADING_ID]
+        )
         self.textCtrl.StyleSetSize(
-            self.STYLE_HEADING_ID,
-            self.config.fontSize.value + 2)
+            self.STYLE_HEADING_ID, self.config.fontSize.value + 2
+        )
         self.textCtrl.StyleSetFaceName(
-            self.STYLE_HEADING_ID,
-            self.config.fontName.value)
+            self.STYLE_HEADING_ID, self.config.fontName.value
+        )
         self.textCtrl.StyleSetBackground(
-            self.STYLE_HEADING_ID,
-            self.config.backColor.value)
+            self.STYLE_HEADING_ID, self.config.backColor.value
+        )
 
     @property
     def colorizeSyntax(self):
@@ -129,16 +144,11 @@ class WikiEditor(TextEditor):
         # Собираем все элементы
         if len(items) > 0:
             itemsList = reduce(
-                lambda result,
-                item: result +
-                itemStart +
-                item.strip() +
-                "\n",
-                items,
-                u"")
+                lambda result, item: result + itemStart + item.strip() + "\n", items, ""
+            )
         else:
             itemsList = itemStart + "\n"
 
-        itemsList = itemsList[: -1]
+        itemsList = itemsList[:-1]
 
         self.textCtrl.ReplaceSelection(itemsList)
