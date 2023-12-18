edada5a16e058c1f07086e3e8d366025b5fb0f48
Eugeniy Ilin <jenyay.ilin@gmail.com>
2023-12-18 14:23:53 +0300

    Add popup menu item to remove tag

--- src/outwiker/app/gui/tagpopupmenu.py
+++ src/outwiker/app/gui/tagpopupmenu.py
@@ -1,12 +1,14 @@
 import wx
 
 from outwiker.core.treetools import testreadonly
-from outwiker.core.tagscommands import renameTag
+from outwiker.core.tagscommands import renameTag, removeTagsFromBranch
+from outwiker.gui.dialogs.messagebox import MessageBox
 
 
 class TagPopupMenu:
     def __init__(self, parent, tag_name, application):
         self.ID_RENAME_TAG = None
+        self.ID_DESTROY_TAG = None
 
         self._parent = parent
         self._tag_name = tag_name
@@ -20,6 +22,9 @@ class TagPopupMenu:
         self.ID_RENAME_TAG = menu.Append(wx.ID_ANY, _("Rename tag")).GetId()
         menu.Bind(wx.EVT_MENU, handler=self._onRenameTag, id=self.ID_RENAME_TAG)
 
+        self.ID_DESTROY_TAG = menu.Append(wx.ID_ANY, _("Remove tag from all pages")).GetId()
+        menu.Bind(wx.EVT_MENU, handler=self._onDestroyTag, id=self.ID_DESTROY_TAG)
+
         return menu
 
     @testreadonly
@@ -30,6 +35,11 @@ class TagPopupMenu:
                 wikiroot = self._application.wikiroot
                 tag_name_new = dlg.GetValue()
                 renameTag(wikiroot, tag_name_old, tag_name_new)
+
+    @testreadonly
+    def _onDestroyTag(self, event):
+        if MessageBox(_("Remove tags from all pages?"), _("Tag removing"), wx.YES_NO, self._application.mainWindow) == wx.YES:
+            removeTagsFromBranch(self._application.wikiroot, [self._tag_name])
 
     @property
     def menu(self) -> wx.Menu:
