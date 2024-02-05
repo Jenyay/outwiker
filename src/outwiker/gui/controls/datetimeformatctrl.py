23d33787141fa89486276a314beaf7cd88e3a098
Eugeniy Ilin <jenyay.ilin@gmail.com>
2024-02-05 16:43:41 +0300

    Fix localizations

--- src/outwiker/gui/controls/datetimeformatctrl.py
+++ src/outwiker/gui/controls/datetimeformatctrl.py
@@ -8,7 +8,7 @@ from outwiker.gui.controls.formatctrl import FormatCtrl
 
 
 class DateTimeFormatCtrl(wx.Panel):
-    def __init__(self, parent, buttonBitmap, initial=u''):
+    def __init__(self, parent, buttonBitmap, initial=""):
         """
         initial - Начальное значение в поле ввода
         """
@@ -17,46 +17,45 @@ class DateTimeFormatCtrl(wx.Panel):
         self._buttonBitmap = buttonBitmap
         self._initial = initial
         self._hints = [
-            (u"%a", _(u"Abbreviated weekday name")),
-            (u"%A", _(u"Full weekday name")),
-            (u"%b", _(u"Abbreviated month name")),
-            (u"%B", _(u"Full month name")),
-            (u"%c", _(u"Appropriate date and time representation")),
-            (u"%d", _(u"Day of the month as a decimal number [01,31]")),
-            (u"%H", _(u"Hour(24-hour clock) as a decimal number [00,23]")),
-            (u"%I", _(u"Hour(12-hour clock) as a decimal number [01,12]")),
-            (u"%m", _(u"Month as a decimal number [01,12]")),
-            (u"%M", _(u"Minute as a decimal number [00,59]")),
-            (u"%p", _(u"AM or PM")),
-            (u"%S", _(u"Second as a decimal number [00,61]")),
-            (u"%x", _(u"Appropriate date representation")),
-            (u"%X", _(u"Appropriate time representation")),
-            (u"%y", _(u"Year without century [00,99]")),
-            (u"%Y", _(u"Year with century")),
-            (u"%%", _(u"A literal '%' character")),
+            ("%a", _("Abbreviated weekday name")),
+            ("%A", _("Full weekday name")),
+            ("%b", _("Abbreviated month name")),
+            ("%B", _("Full month name")),
+            ("%c", _("Appropriate date and time representation")),
+            ("%d", _("Day of the month as a decimal number (01, 02, ..., 31)")),
+            ("%H", _("Hour(24-hour clock) as a decimal number (00, 01, ..., 23)")),
+            ("%I", _("Hour(12-hour clock) as a decimal number (01, 02, ..., 12)")),
+            ("%m", _("Month as a decimal number (01, 02, ..., 12)")),
+            ("%M", _("Minute as a decimal number (00, 01, ..., 59)")),
+            ("%p", _("AM or PM")),
+            ("%S", _("Second as a decimal number (00, 01, ..., 59)")),
+            ("%x", _("Appropriate date representation")),
+            ("%X", _("Appropriate time representation")),
+            ("%y", _("Year without century (00, 01, ..., 99)")),
+            ("%Y", _("Year with century")),
+            ("%%", _("A literal '%' character")),
         ]
 
         self._createGui()
-        self.Bind(wx.EVT_TEXT,
-                  handler=self._onChange,
-                  source=self.dateTimeFormatCtrl.formatCtrl)
+        self.Bind(
+            wx.EVT_TEXT,
+            handler=self._onChange,
+            source=self.dateTimeFormatCtrl.formatCtrl,
+        )
         self._updateExample()
 
     def _onChange(self, event):
         self._updateExample()
 
     def _createGui(self):
-        self.dateTimeFormatCtrl = FormatCtrl(self,
-                                             self._initial,
-                                             self._hints,
-                                             self._buttonBitmap)
+        self.dateTimeFormatCtrl = FormatCtrl(
+            self, self._initial, self._hints, self._buttonBitmap
+        )
         self.exampleText = wx.TextCtrl(self, style=wx.TE_READONLY)
         self.exampleText.Enable(False)
 
         dateTimeSizer = wx.BoxSizer(wx.VERTICAL)
-        dateTimeSizer.Add(self.dateTimeFormatCtrl,
-                          flag=wx.ALL | wx.EXPAND,
-                          border=2)
+        dateTimeSizer.Add(self.dateTimeFormatCtrl, flag=wx.ALL | wx.EXPAND, border=2)
         dateTimeSizer.Add(self.exampleText, flag=wx.ALL | wx.EXPAND, border=2)
 
         self.SetSizer(dateTimeSizer)
