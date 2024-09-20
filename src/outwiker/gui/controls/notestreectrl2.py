b490e35ded1923a6ad71a2407d55f9d4b4654d6e
Eugeniy Ilin <jenyay.ilin@gmail.com>
2024-09-20 17:09:32 +0300

    Generate tree from NotesTreeItems

--- src/outwiker/gui/controls/notestreectrl2.py
+++ src/outwiker/gui/controls/notestreectrl2.py
@@ -2,8 +2,9 @@
 
 import logging
 import os
-from typing import Any, Optional, List
+from typing import Dict, Optional, List
 
+from outwiker.core.tree import BasePage, WikiDocument, WikiPage
 import wx
 
 from outwiker.core.defines import ICON_HEIGHT
@@ -12,20 +13,56 @@ from outwiker.gui.imagelistcache import ImageListCache
 
 logger = logging.getLogger("outwiker.gui.controls.notestreectrl2")
 
-class _NotesTreeItem:
-    def __init__(self) -> None:
-        self._children: List["_NotesTreeItem"] = []
-        self._title = ""
-        self._data: Any = None
-        self._icon = -1
+class NotesTreeItem:
+    def __init__(self, title: str, page: BasePage, depth) -> None:
+        self._title = title
+        self._page = page
+        self._depth = depth
+        self._line = 0
+        self._children: List["NotesTreeItem"] = []
+        self._imageId = -1
         self._bold = False
         self._italic = False
         self._fontColor = wx.Colour(0, 0, 0)
         self._backColor = wx.Colour(0, 0, 0)
         self._expanded = False
 
+    def getDepth(self) -> int:
+        return self._depth
 
-class NotesTreeCtrl2(wx.Control):
+    def getLine(self) -> int:
+        return self._line
+
+    def setLine(self, line: int) -> "NotesTreeItem":
+        self._line = line
+        return self
+
+    def addChild(self, child: "NotesTreeItem") -> "NotesTreeItem":
+        self._children.append(child)
+        return self
+
+    def setImageId(self, imageId) -> "NotesTreeItem":
+        self._imageId = imageId
+        return self
+
+    def isExpanded(self) -> bool:
+        return self._expanded
+
+    def expand(self, expanded=True) -> "NotesTreeItem":
+        self._expanded = expanded
+        return self
+
+    def insertChild(self, index: int, child: "NotesTreeItem") -> "NotesTreeItem":
+        if index < 0:
+            index = 0
+        if index >= len(self._children):
+            index = len(self._children)
+
+        self._children.insert(index, child)
+        return self
+
+
+class NotesTreeCtrl2(wx.ScrolledCanvas):
     def __init__(self, parent: wx.Window):
         super().__init__(parent)
         self._iconHeight = ICON_HEIGHT
@@ -37,51 +74,65 @@ class NotesTreeCtrl2(wx.Control):
         self._expandCtrlHeight = 8
         self._expandCtrlLeftGap = 3
         self._expandCtrlRightGap = 3
+        self.SetScrollRate(0, 0)
 
         self.defaultIcon = getBuiltinImagePath('page.svg')
 
         # Картинки для дерева
         self._iconsCache = ImageListCache(self.defaultIcon)
-        # self.AssignImageList(self._iconsCache.getImageList())
 
         # Кеш для страниц, чтобы было проще искать элемент дерева по странице
-        # Словарь. Ключ - страница, значение - элемент дерева wx.TreeItemId
-        self._pageCache = {}
+        # Словарь. Ключ - страница, значение - элемент дерева NotesTreeItem
+        self._pageCache: Dict[BasePage, NotesTreeItem] = {}
 
         # Имя опции для сохранения развернутости страницы
         self.pageOptionExpand = 'Expand'
 
-        self._items: List[_NotesTreeItem] = []
+        self._items: List[NotesTreeItem] = []
+        self._lineCount = 0
 
         self.Bind(wx.EVT_CLOSE, self._onClose)
+        self.Bind(wx.EVT_PAINT, handler=self._onPaint)
+
+    def _onPaint(self, event):
+        with wx.PaintDC(self) as dc:
+            back_color = self.GetBackgroundColour()
+            dc.SetBrush(wx.Brush(back_color))
+            dc.SetPen(wx.Pen(back_color))
+            width, height = self.GetClientSize()
+            dc.DrawRectangle(0, 0, width, height)
 
     def _onClose(self, event):
         self._iconsCache.clear()
 
-    def _addRoot(self, title, data, imageId: int):
-        pass
+    def _createRootNotesTreeItem(self, rootPage: WikiDocument) -> "NotesTreeItem":
+        rootname = os.path.basename(rootPage.path)
+        return (NotesTreeItem(rootname, rootPage, 0)
+                .setImageId(self._iconsCache.getDefaultImageId())
+        )
 
-    def _deleteAllItems(self):
-        pass
+    def _createNotesTreeItem(self, page: WikiPage) -> "NotesTreeItem":
+        title = page.display_title
+        parent_page = page.parent
+        assert parent_page is not None
+
+        parent_item = self._pageCache[parent_page]
+
+        return (NotesTreeItem(title, page, parent_item.getDepth() + 1)
+                .setImageId(self._loadIcon(page))
+                )
 
     def treeUpdate(self, rootPage):
         """
         Обновить дерево
         """
-        self._deleteAllItems()
         self._iconsCache.clear()
-
-        # Ключ - страница, значение - экземпляр класса TreeItemId
-        self._pageCache = {}
+        self._pageCache.clear()
+        self._items.clear()
 
         if rootPage is not None:
-            rootname = os.path.basename(rootPage.path)
-            rootItem = self._addRoot(
-                rootname,
-                data=rootPage,
-                imageId=self._iconsCache.getDefaultImageId())
-
-            self._pageCache[rootPage] = rootItem
+            root_item = self._createRootNotesTreeItem(rootPage)
+            self._pageCache[rootPage] = root_item
             self.appendChildren(rootPage)
 
             self.selectedPage = rootPage.selectedPage
@@ -92,35 +143,30 @@ class NotesTreeCtrl2(wx.Control):
         Добавить детей в дерево
         parentPage - родительская страница, куда добавляем дочерние страницы
         """
-        # grandParentExpanded = self._getItemExpandState(parentPage.parent)
+        grandParentExpanded = self._getItemExpandState(parentPage.parent)
 
-        # if grandParentExpanded:
-        #     for child in parentPage.children:
-        #         if child not in self._pageCache:
-        #             self.insertChild(child)
+        if grandParentExpanded:
+            for child in parentPage.children:
+                if child not in self._pageCache:
+                    self.insertChild(child)
 
-        # if self._getPageExpandState(parentPage):
-        #     self.expand(parentPage)
+        if self._getPageExpandState(parentPage):
+            self.expand(parentPage)
 
     def insertChild(self, childPage):
         """
         Вставить одну дочерниюю страницу (childPage) в ветвь
         """
-        # parentItem = self.getTreeItem(childPage.parent)
-        # assert parentItem is not None
+        parentItem = self.getTreeItem(childPage.parent)
+        assert parentItem is not None
 
-        # item = self.InsertItem(parentItem,
-        #                        childPage.order,
-        #                        childPage.display_title,
-        #                        data=childPage)
+        childItem = self._createNotesTreeItem(childPage)
+        childItem.insertChild(childPage.order, childItem)
 
-        # self.SetItemImage(item, self._loadIcon(childPage))
+        self._pageCache[childPage] = childItem
+        self.appendChildren(childPage)
 
-        # self._pageCache[childPage] = item
-        # self._mountItem(item, childPage)
-        # self.appendChildren(childPage)
-
-        # return item
+        return childItem
 
     def removePageItem(self, page):
         """
@@ -167,34 +213,34 @@ class NotesTreeCtrl2(wx.Control):
         # self.SetItemImage(self._pageCache[page], icon_id)
         pass
 
-    # def _getItemExpandState(self, page):
-    #     """
-    #     Проверить, раскрыт ли элемент в дереве для страницы page
-    #     """
-    #     if page is None:
-    #         return True
+    def _getItemExpandState(self, page):
+        """
+        Проверить, раскрыт ли элемент в дереве для страницы page
+        """
+        if page is None:
+            return True
 
-    #     if page.parent is None:
-    #         return True
+        if page.parent is None:
+            return True
 
-    #     return self.IsExpanded(self._pageCache[page])
+        return self._pageCache[page].isExpanded()
 
-    # def _getPageExpandState(self, page):
-    #     """
-    #     Проверить состояние "раскрытости" страницы
-    #     """
-    #     if page is None:
-    #         return True
+    def _getPageExpandState(self, page):
+        """
+        Проверить состояние "раскрытости" страницы
+        """
+        if page is None:
+            return True
 
-    #     if page.parent is None:
-    #         return True
+        if page.parent is None:
+            return True
 
-    #     page_registry = page.root.registry.get_page_registry(page)
-    #     expanded = page_registry.getbool(self.pageOptionExpand, default=False)
+        page_registry = page.root.registry.get_page_registry(page)
+        expanded = page_registry.getbool(self.pageOptionExpand, default=False)
 
-    #     return expanded
+        return expanded
 
-    def getTreeItem(self, page: 'outwiker.core.tree.WikiPage') -> Optional[wx.TreeItemId]:
+    def getTreeItem(self, page: BasePage) -> Optional[NotesTreeItem]:
         """
         Получить элемент дерева по странице.
         Если для страницы не создан элемент дерева, возвращается None
@@ -217,10 +263,9 @@ class NotesTreeCtrl2(wx.Control):
         #     self.expand(page)
 
     def expand(self, page):
-        # item = self.getTreeItem(page)
-        # if item is not None:
-        #     self.Expand(item)
-        pass
+        item = self.getTreeItem(page)
+        if item is not None:
+            item.expand()
 
     def createPage(self, newpage):
         # if newpage.parent in self._pageCache:
