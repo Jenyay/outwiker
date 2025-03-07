172f19ab228d324d634f758259a0e8519dbe63b1
Eugeniy Ilin <jenyay.ilin@gmail.com>
2025-03-07 12:25:17 +0300

    Improve package build. Version in pyproject.toml is dynamic now

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -2,6 +2,8 @@ __version__ = (4, 0, 0, 951)
 __status__ = 'dev'
 __api_version__ = (4, 951)
 
+__version_str__ = ".".join([str(n) for n in __version__])
+
 
 def getVersionStr() -> str:
     return '.'.join([str(item) for item in __version__]) + ' ' + __status__
