619a7debd34c980542eaf2526394747fea61f777
Eugeniy Ilin <jenyay.ilin@gmail.com>
2025-06-03 15:34:55 +0300

    Change tests runner params

--- runtests.py
+++ runtests.py
@@ -3,6 +3,7 @@
 import cProfile
 import sys
 import os
+import os.path
 
 import pytest
 
@@ -11,18 +12,22 @@ from outwiker.tests.utils import print_memory
 if __name__ == '__main__':
     args = sys.argv[1:]
     args.append('-s')
+    # args.append('--durations=20')
+    # args.append('--durations-min=1.0')
 
     profile_dir = 'tmp'
     profile_name = 'test_profile'
     if not os.path.exists(profile_dir):
         os.mkdir(profile_dir)
 
-    profile_path = os.path.join(profile_dir, profile_name)
 
     profile = cProfile.Profile()
     profile.enable()
+
     result = pytest.main(args)
+
     profile.create_stats()
+    profile_path = os.path.join(profile_dir, profile_name)
     profile.dump_stats(profile_path)
 
     # print_memory()
