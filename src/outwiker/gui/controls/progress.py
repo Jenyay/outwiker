564e03dab5a8f99eadc7ab86f102debb9fcb0a98
Eugeniy Ilin <jenyay.ilin@gmail.com>
2025-07-28 14:59:18 +0300

    Clear code

--- src/outwiker/gui/controls/progress.py
+++ src/outwiker/gui/controls/progress.py
@@ -4,11 +4,8 @@ import wx
 
 
 class ProgressWindow(wx.Frame):
-    def __init__(self, parent: wx.Window,
-                 title: str,
-                 text: str):
-        super().__init__(parent,
-                         style=wx.FRAME_FLOAT_ON_PARENT | wx.FRAME_NO_TASKBAR)
+    def __init__(self, parent: wx.Window, title: str, text: str):
+        super().__init__(parent, style=wx.FRAME_FLOAT_ON_PARENT | wx.FRAME_NO_TASKBAR)
         self._title = title
         self._text = text
         self._parentTop = wx.GetTopLevelParent(parent)
@@ -25,24 +22,20 @@ class ProgressWindow(wx.Frame):
         sizer.AddGrowableRow(1)
 
         self._titleLabel = wx.StaticText(self)
-        self._titleLabel.SetLabelMarkup('<b>' + self._title + '</b>')
+        self._titleLabel.SetLabelMarkup("<b>" + self._title + "</b>")
 
         self._textLabel = wx.StaticText(
-            self,
-            label=self._text + self._getTextSuffix(self._maxDotCount))
+            self, label=self._text + self._getTextSuffix(self._maxDotCount)
+        )
 
-        sizer.Add(self._titleLabel,
-                  flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER | wx.ALL,
-                  border=10)
-        sizer.Add(self._textLabel,
-                  flag=wx.EXPAND | wx.ALL,
-                  border=20)
+        sizer.Add(
+            self._titleLabel,
+            flag=wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER | wx.ALL,
+            border=10,
+        )
+        sizer.Add(self._textLabel, flag=wx.EXPAND | wx.ALL, border=20)
 
-        self.SetSizer(sizer)
-        self.Fit()
-        self.Layout()
-        self.Update()
-
+        self.SetSizerAndFit(sizer)
         self.Bind(wx.EVT_WINDOW_DESTROY, self._onDestroy)
 
     def ShowWindow(self):
@@ -54,12 +47,11 @@ class ProgressWindow(wx.Frame):
         self.Raise()
 
     def _getTextSuffix(self, updateCount):
-        return '.' * (updateCount % (self._maxDotCount + 1))
+        return "." * (updateCount % (self._maxDotCount + 1))
 
     def UpdatePulse(self):
         self._updateCount += 1
-        self._textLabel.SetLabel(
-            self._text + self._getTextSuffix(self._updateCount))
+        self._textLabel.SetLabel(self._text + self._getTextSuffix(self._updateCount))
         self.Update()
         wx.SafeYield()
 
