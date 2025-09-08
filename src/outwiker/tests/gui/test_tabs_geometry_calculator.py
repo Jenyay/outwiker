37fa2beef96da876493908943206d5f1dfde1b2b
Eugene Ilin <jenyay.ilin@gmail.com>
2025-09-08 09:32:09 +0300

    Fix tests

--- src/outwiker/tests/gui/test_tabs_geometry_calculator.py
+++ src/outwiker/tests/gui/test_tabs_geometry_calculator.py
@@ -8,20 +8,22 @@ from outwiker.gui.theme import Theme
 
 @pytest.fixture
 def theme() -> Theme:
-    return Theme()
+    theme = Theme()
+    calculator = TabsGeometryCalculator(theme)
+    theme.set(Theme.SECTION_TABS, Theme.TABS_MIN_WIDTH, 50 - calculator.horizontal_gap_after_tab)
+    theme.set(Theme.SECTION_TABS, Theme.TABS_MAX_WIDTH, 150 - calculator.horizontal_gap_after_tab)
+    return theme
 
 
 @pytest.fixture
-def calculator(theme) -> TabsGeometryCalculator:
+def calculator(theme: Theme) -> TabsGeometryCalculator:
     calculator = TabsGeometryCalculator(theme)
-    calculator.min_width = 25 - calculator.horizontal_gap_after_tab
-    calculator.max_width = 100 - calculator.horizontal_gap_after_tab
     return calculator
 
 
-def test_empty(calculator: TabsGeometryCalculator):
+def test_empty(calculator: TabsGeometryCalculator, theme: Theme):
     text_height = 12
-    parent_width = 5 * calculator.max_width
+    parent_width = 5 * theme.get(Theme.SECTION_TABS, Theme.TABS_MAX_WIDTH)
     parent_height = 10
     tabs: List[TabInfo] = []
     result = calculator.calc(tabs, parent_width, parent_height, text_height)
@@ -33,9 +35,10 @@ def test_empty(calculator: TabsGeometryCalculator):
 def test_single_line_max_width(
     calculator: TabsGeometryCalculator, theme: Theme, tabs_count: int
 ):
+    theme_max_width = theme.get(Theme.SECTION_TABS, Theme.TABS_MAX_WIDTH)
     text_height = 12
     parent_width = (
-        4 * calculator.max_width
+        4 * theme_max_width
         + calculator.horizontal_gap_after_tab
         + theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
     )
@@ -46,16 +49,17 @@ def test_single_line_max_width(
 
     result = calculator.calc(tabs, parent_width, parent_height, text_height)
 
-    min_width = calculator.max_width - calculator.horizontal_gap_after_tab
+    min_width = theme_max_width - calculator.horizontal_gap_after_tab
     for n in range(tabs_count):
-        assert result[n].width <= calculator.max_width and result[n].width >= min_width
+        assert result[n].width <= theme_max_width and result[n].width >= min_width
         assert result[n].top == result[0].top
 
 
-def test_single_line_small_width(calculator: TabsGeometryCalculator):
+def test_single_line_small_width(calculator: TabsGeometryCalculator, theme: Theme):
     tabs_count = 5
     text_height = 12
-    parent_width = (tabs_count - 1) * calculator.max_width
+    theme_max_width = theme.get(Theme.SECTION_TABS, Theme.TABS_MAX_WIDTH)
+    parent_width = (tabs_count - 1) * theme_max_width
     parent_height = 10
     tabs: List[TabInfo] = []
     for _ in range(tabs_count):
@@ -64,14 +68,14 @@ def test_single_line_small_width(calculator: TabsGeometryCalculator):
     result = calculator.calc(tabs, parent_width, parent_height, text_height)
 
     for n in range(tabs_count):
-        assert (result[n].right - result[n].left) < calculator.max_width
+        assert (result[n].right - result[n].left) < theme_max_width
         assert result[n].top == result[0].top
 
 
-def test_right_border(calculator: TabsGeometryCalculator):
+def test_right_border(calculator: TabsGeometryCalculator, theme: Theme):
     tabs_count = 8
     text_height = 12
-    parent_width = 5 * calculator.max_width
+    parent_width = 7 * theme.get(Theme.SECTION_TABS, Theme.TABS_MAX_WIDTH)
     parent_height = 10
     tabs: List[TabInfo] = []
     for _ in range(tabs_count):
@@ -86,7 +90,7 @@ def test_right_border(calculator: TabsGeometryCalculator):
 def test_two_rows(calculator: TabsGeometryCalculator, theme: Theme):
     tabs_count = 3
     text_height = 12
-    parent_width = 55 + calculator.horizontal_gap_after_tab + theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
+    parent_width = 155 + calculator.horizontal_gap_after_tab + theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
     parent_height = 10
     tabs: List[TabInfo] = []
     for _ in range(tabs_count):
@@ -105,7 +109,7 @@ def test_single_line_full_height(
     calculator: TabsGeometryCalculator, theme: Theme):
     text_height = 12
     parent_width = (
-        4 * calculator.max_width
+        4 * theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
         + calculator.horizontal_gap_after_tab
         + theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
     )
@@ -121,7 +125,7 @@ def test_single_line_bottom_align(
     calculator: TabsGeometryCalculator, theme: Theme):
     text_height = 50
     parent_width = (
-        4 * calculator.max_width
+        4 * theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
         + calculator.horizontal_gap_after_tab
         + theme.get(Theme.SECTION_TABS, Theme.TABS_ADD_BUTTON_SIZE)
     )
