c25a746e149671d1e90327cabacf280c8cc938f4
Eugene Ilin <jenyay.ilin@gmail.com>
2025-11-08 18:50:05 +0300

    Start new version

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -1,5 +1,5 @@
-__version__ = (4, 0, 0, 962)
-__status__ = 'beta'
+__version__ = (4, 0, 0, 963)
+__status__ = 'dev'
 __api_version__ = (4, 962)
 
 __version_str__ = ".".join([str(n) for n in __version__])
