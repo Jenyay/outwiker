6b932b9d8190361981f3bed002f2513cde0cc37a
Eugene Ilin <jenyay.ilin@gmail.com>
2026-02-14 17:15:36 +0300

    Update changelog

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -1,5 +1,5 @@
-__version__ = (4, 0, 0, 967)
-__status__ = 'dev'
+__version__ = (4, 0, 0, 968)
+__status__ = 'beta'
 __api_version__ = (4, 966)
 
 __version_str__ = ".".join([str(n) for n in __version__])
