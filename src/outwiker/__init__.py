2defe71579b26e7a23c805e2c794d588a8584be5
Eugene Ilin <jenyay.ilin@gmail.com>
2026-05-01 12:56:18 +0300

    Update chengelog

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -1,5 +1,5 @@
-__version__ = (4, 0, 0, 971)
-__status__ = 'dev'
+__version__ = (4, 0, 0, 972)
+__status__ = 'beta'
 __api_version__ = (4, 972)
 
 __version_str__ = ".".join([str(n) for n in __version__])
