244be55b1d2958c748636a06215134482939b487
Eugene Ilin <jenyay.ilin@gmail.com>
2026-06-13 19:30:57 +0300

    Begin OutWiker 5.0

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -1,5 +1,5 @@
-__version__ = (4, 1, 0, 975)
-__status__ = 'dev'
+__version__ = (5, 0, 0, 975)
+__status__ = ''
 __api_version__ = (4, 972)
 
 __version_str__ = ".".join([str(n) for n in __version__])
