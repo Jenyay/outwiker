458dd505e48c55eee084ffdb10d3056ab63db4f1
Eugene Ilin <jenyay.ilin@gmail.com>
2026-06-13 18:51:08 +0300

    Update version number

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -1,5 +1,5 @@
-__version__ = (4, 0, 0, 974)
-__status__ = ''
+__version__ = (4, 1, 0, 975)
+__status__ = 'dev'
 __api_version__ = (4, 972)
 
 __version_str__ = ".".join([str(n) for n in __version__])
