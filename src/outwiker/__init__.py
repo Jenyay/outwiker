9dd86a90a12341dfb713ce7ad257e3db93e84e8e
Eugene Ilin <jenyay.ilin@gmail.com>
2026-07-28 13:47:34 +0300

    Next dev version

--- src/outwiker/__init__.py
+++ src/outwiker/__init__.py
@@ -1,5 +1,5 @@
-__version__ = (4, 1, 0, 978)
-__status__ = 'beta'
+__version__ = (4, 1, 0, 979)
+__status__ = 'dev'
 __api_version__ = (4, 972)
 
 __version_str__ = ".".join([str(n) for n in __version__])
